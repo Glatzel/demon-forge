b720ed289fc38096b4884c737ab62881934ce409
Glatzel <893016099@qq.com>
2025-03-09 15:14:42 +0800

    fix

--- imagemagick/build.ps1
+++ imagemagick/build.ps1
@@ -1,5 +1,5 @@
 New-Item $env:PREFIX/scripts -ItemType Directory
-foreach ($f in Get-ChildItem  "$env:RECIPE_DIR/temp/ImageMagick*/*") {
+foreach ($f in Get-ChildItem  "$env:RECIPE_DIR/temp/ImageMagick-*-Q16-x64/*") {
     Copy-Item $f "$env:PREFIX/scripts/"
 }
 
