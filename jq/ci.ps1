819fac733f388f79624897e31a96036268f62213
Glatzel <893016099@qq.com>
2025-03-09 12:40:15 +0800

    fix

---
+++ jq/ci.ps1
@@ -0,0 +1,16 @@
+Set-Location $PSScriptRoot
+. ../scripts/setup.ps1
+
+$current_version = get-current-version
+Write-Output "Current Version: $current_version"
+
+$latest_version = get_latest_version -repo "jqlang/jq"
+$latest_version = "$latest_version".Replace("jq-", "")
+Write-Output "Latest Version: $latest_version"
+
+if($current_version -ne $latest_version){
+    gh release download -R jqlang/jq -p "jq-windows-amd64.exe" -O jq.exe --clobber
+    update-recipe -version $latest_version
+    build_pkg
+    test_pkg
+}
---
+++ scripts/setup.ps1
@@ -0,0 +1,60 @@
+$ErrorActionPreference = "Stop"
+$PSNativeCommandUseErrorActionPreference = $true
+Import-Module powershell-yaml
+
+function get-current-version {
+    # Define the path to the YAML file
+    $yamlFilePath = "recipe.yaml"
+
+    # Read the YAML content
+    $yamlContent = Get-Content -Path $yamlFilePath -Raw
+
+    # Convert the YAML content to a PowerShell object
+    $yamlObject = $yamlContent | ConvertFrom-Yaml
+
+    # Get the current version from the context
+    $currentVersion = $yamlObject.context.version
+    Write-Output "$currentVersion"
+}
+function get_latest_version {
+    param($repo)
+    gh release view -R $repo --json tagName -q .tagName
+}
+function update-recipe {
+    param($version)
+    Write-Output "::group::update recipe"
+    # Define the path to the YAML file
+    $yamlFilePath = "recipe.yaml"
+
+    # Read the YAML content
+    $yamlContent = Get-Content -Path $yamlFilePath -Raw
+
+    # Convert the YAML content to a PowerShell object
+    $yamlObject = $yamlContent | ConvertFrom-Yaml
+
+    # Get the current version from the context
+    $yamlObject.context.version = $version
+    # Convert the updated object back to YAML
+    $updatedYamlContent = $yamlObject | ConvertTo-Yaml
+
+    # Write the updated YAML content back to the file
+    Set-Content -Path "recipe.yaml" -Value $updatedYamlContent
+    Write-Output "::endgroup::"
+}
+function build_pkg {
+    Write-Output "::group::build"
+    pixi run rattler-build build
+    Write-Output "::endgroup::"
+}
+function test_pkg {
+    Write-Output "::group::test"
+    if ($IsWindows) {
+        foreach ($conda_file in Get-ChildItem "./output/win-64/*.conda") {
+            
+            
+            pixi run rattler-build test --package-file $conda_file
+            
+        }
+    }
+    Write-Output "::endgroup::"
+}
