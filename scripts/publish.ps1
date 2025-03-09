22d141027aa967f62eb4c85defee00d7b3282f8b
Glatzel <32577755+Glatzel@users.noreply.github.com>
2025-03-09 16:23:04 +0800

    ci: Update publish.ps1

--- scripts/publish.ps1
+++ scripts/publish.ps1
@@ -4,24 +4,6 @@ Set-Location ..
 
 if (Test-Path "./*/output/win-64/*.conda") {
     foreach ($pkg in Get-ChildItem "./*/output/win-64/*.conda") {
-        $filename = Split-Path $pkg -Leaf
-        try {
-            $search_result=pixi search -c https://repo.prefix.dev/glatzel imagemagick
-            if ($LASTEXITCODE -ne 0) {
-                Write-Host "Caught an error: $_"
-                $LASTEXITCODE=0
-            }
-        }
-        catch {
-            Write-Host "Caught an error: $_"
-            $LASTEXITCODE=0
-        }
-        if (-not ("$search_result" -like "*$filename*")) {
-            write-output "::group::publish $pkg"
-            pixi run rattler-build upload prefix -c glatzel "$pkg"
-            write-output "::endgroup::"
-        }
-        else { write-output "No new package." }
+            pixi run rattler-build upload prefix -c https://repo.prefix.dev/glatzel "$pkg"
     }
 }
-else { write-output "No new package." }
