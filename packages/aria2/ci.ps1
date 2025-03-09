c9f90afe74796c61856a592077971adce1078ea2
Glatzel <32577755+Glatzel@users.noreply.github.com>
2025-03-09 20:27:58 +0800

    feat: aria2 (#32)

---
+++ packages/aria2/ci.ps1
@@ -0,0 +1,17 @@
+Set-Location $PSScriptRoot
+. ../../scripts/setup.ps1
+
+$current_version = get-current-version
+Write-Output "Current Version: $current_version"
+
+$latest_version = get_latest_version -repo "aria2/aria2"
+$latest_version = "$latest_version".Replace("release-", "")
+Write-Output "Latest Version: $latest_version"
+
+Remove-Item ../../temp/aria2 -Recurse -ErrorAction SilentlyContinue
+New-Item  ../../temp/aria2 -ItemType Directory
+gh release download -R aria2/aria2 -p "aria2-*-win-64bit*.zip" -O  ../../temp/aria2/aria2.zip --clobber
+Expand-Archive ../../temp/imagemagick/magick.zip ../../temp/imagemagick/
+update-recipe -version $latest_version
+build_pkg
+test_pkg
