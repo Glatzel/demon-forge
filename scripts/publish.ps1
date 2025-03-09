62241384fe45a9a71bcc25d0d254f347ff30ef66
Glatzel <893016099@qq.com>
2025-03-09 20:04:20 +0800

    fix

--- scripts/publish.ps1
+++ scripts/publish.ps1
@@ -4,6 +4,6 @@ Set-Location ..
 
 foreach ($pkg_file in Get-ChildItem "./packages/$pkg/output/win-64/*.conda" -Recurse -ErrorAction Continue) {
     Write-Output "::group:: upload $pkg"
-    pixi run rattler-build upload prefix --api-key $key -c https://repo.prefix.dev/glatzel $pkg_file
+    pixi run rattler-build upload prefix --api-key $key -c glatzel $pkg_file
     Write-Output "::endgroup::"
 }
