c1066a6632595bef9bed70c536e73918063540b4
Glatzel <32577755+Glatzel@users.noreply.github.com>
2025-03-09 17:23:24 +0800

    Update publish.ps1

--- scripts/publish.ps1
+++ scripts/publish.ps1
@@ -1,10 +1,9 @@
 Set-Location $PSScriptRoot
 Set-Location ..
 
-if (Test-Path "./*/output/win-64/*.conda") {
+
     foreach ($pkg in Get-ChildItem "./*/output/win-64/*.conda") {
         Write-Output "::group:: upload $pkg"
         pixi run rattler-build upload prefix -c https://repo.prefix.dev/glatzel "$pkg"
         Write-Output "::endgroup::"
     }
-}
