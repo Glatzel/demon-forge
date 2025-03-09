1b2650ad68c2392f6fc9ddd38dc793276b64ac94
Glatzel <32577755+Glatzel@users.noreply.github.com>
2025-03-09 19:35:11 +0800

    ci: fix (#26)

--- scripts/publish.ps1
+++ scripts/publish.ps1
@@ -1,9 +1,9 @@
-param($pkg)
+param($key)
 Set-Location $PSScriptRoot
 Set-Location ..
 
 foreach ($pkg_file in Get-ChildItem "./packages/$pkg/output/win-64/*.conda" -Recurse -ErrorAction Continue) {
     Write-Output "::group:: upload $pkg"
-    pixi run rattler-build upload prefix -c https://repo.prefix.dev/glatzel $pkg_file
+    pixi run rattler-build upload prefix --api-key $key -c https://repo.prefix.dev/glatzel $pkg_file
     Write-Output "::endgroup::"
 }
