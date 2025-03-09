7a4958b13e67862ce3aa54b23809d159366e7fe0
Glatzel <32577755+Glatzel@users.noreply.github.com>
2025-03-09 16:45:03 +0800

    ci: fix (#13)

--- scripts/publish.ps1
+++ scripts/publish.ps1
@@ -4,6 +4,8 @@ Set-Location ..
 
 if (Test-Path "./*/output/win-64/*.conda") {
     foreach ($pkg in Get-ChildItem "./*/output/win-64/*.conda") {
-            pixi run rattler-build upload prefix -c https://repo.prefix.dev/glatzel "$pkg"
+        Write-Output "::group:: upload $pkg"
+        pixi run rattler-build upload prefix -c https://repo.prefix.dev/glatzel "$pkg"
+        Write-Output "::endgroup::"
     }
 }
