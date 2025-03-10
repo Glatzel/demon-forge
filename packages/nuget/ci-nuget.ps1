9cc00541977045aefc391cb61acaf5810972eb4b
Glatzel <893016099@qq.com>
2025-03-10 21:12:57 +0800

    fix

--- packages/nuget/ci-nuget.ps1
+++ packages/nuget/ci-nuget.ps1
@@ -5,11 +5,12 @@ $current_version = get-current-version
 Write-Output "Current Version: $current_version"
 
 $latest_version = curl https://dist.nuget.org/index.json | jq '.artifacts[0].versions[0].version'
-$latest_version = "$latest_version".Replace("""","")
+$latest_version = "$latest_version".Replace("""", "")
 
 Remove-Item ../../temp/nuget -Recurse -ErrorAction SilentlyContinue
 New-Item ../../temp/nuget -ItemType Directory
-aria2c -c -x16 -s16 -d ../../temp/nuget/ "https://dist.nuget.org/win-x86-commandline/latest/nuget.exe" `
+aria2c -c -x16 -s16 -d ../../temp/nuget/ `
+    "https://dist.nuget.org/win-x86-commandline/latest/nuget.exe" `
     -o nuget.exe
 Write-Output "Latest Version: $latest_version"
 
