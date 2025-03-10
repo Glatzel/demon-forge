3ec08fb5171a642ac8837abfde0161794f1e49d5
Glatzel <893016099@qq.com>
2025-03-10 14:38:05 +0800

    fix

--- packages/openimageio_build/scripts/copy-item.ps1
+++ packages/openimageio_build/scripts/copy-item.ps1
@@ -12,7 +12,7 @@ foreach ($dep in Get-ChildItem ./dist/*.dll) {
     $name = $dep.Name
     copy-Item $dep ./temp
     Remove-Item $dep
-    ./openimageio/oiiotool.exe --help > $_
+    ./dist/oiiotool.exe --help > $_
     if ($LASTEXITCODE -ne 0) {
         copy-Item "./temp/$name" ./dist
         Write-Host "It is a dependency: $name" -ForegroundColor Green
@@ -21,3 +21,4 @@ foreach ($dep in Get-ChildItem ./dist/*.dll) {
         Write-Host "It is not a dependency: $name" -ForegroundColor Red
     }
 }
+$LASTEXITCODE=0
