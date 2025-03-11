24cf7e13fc20e69570451d66ffdcf99014b05f4c
Glatzel <32577755+Glatzel@users.noreply.github.com>
2025-03-11 10:10:21 +0800

    ci: Update publish.ps1 (#45)

--- scripts/publish.ps1
+++ scripts/publish.ps1
@@ -5,5 +5,11 @@ Set-Location ..
 foreach ($pkg_file in Get-ChildItem "./packages/$pkg/output/win-64/*.conda" -Recurse -ErrorAction Continue) {
     Write-Output "::group:: upload $pkg"
     pixi run rattler-build upload prefix -c glatzel $pkg_file
+    if ($LASTEXITCODE -ne 0)
+    {
+        Write-Host "Upload fail: $pkg" -ForegroundColor Red
+    }
+    else{Write-Host "Upload succeed: $pkg" -ForegroundColor Red}
+    $LASTEXITCODE=0
     Write-Output "::endgroup::"
 }
