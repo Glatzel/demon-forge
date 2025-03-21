a1c7f1363d04f6d731d6d7f7e86cd773ef27af63
Glatzel <893016099@qq.com>
2025-03-21 11:48:11 +0800

    fix

--- packages/android-sdk-platform-tools/ci-android-sdk-platform-tools.ps1
+++ packages/android-sdk-platform-tools/ci-android-sdk-platform-tools.ps1
@@ -7,5 +7,9 @@ aria2c -c -x16 -s16 -d $ROOT/temp/android-sdk-platform-tools/ `
     "https://googledownloads.cn/android/repository/platform-tools-latest-windows.zip" `
     -o platform-tools.zip
 Expand-Archive $ROOT/temp/android-sdk-platform-tools/platform-tools.zip $ROOT/temp/android-sdk-platform-tools/
-
+$output=& $ROOT/temp/android-sdk-platform-tools/platform-tools/adb.exe version 
+"$output" -match 'Version (\S+-\S+)'
+$version = $Matches[1].Replace("-",".")
+Write-Output "version: $version"
+update-recipe -version $version
 build_pkg
