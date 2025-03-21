08508d685d91ecaaa1c7b8779c1163123941dc5d
Glatzel <893016099@qq.com>
2025-03-21 11:54:00 +0800

    feat: android platform tools (#57)
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

---
+++ packages/android-sdk-platform-tools/ci-android-sdk-platform-tools.ps1
@@ -0,0 +1,15 @@
+Set-Location $PSScriptRoot
+$ROOT = git rev-parse --show-toplevel
+. $ROOT/scripts/setup.ps1
+Remove-Item $ROOT/temp/android-sdk-platform-tools -Recurse -ErrorAction SilentlyContinue
+New-Item $ROOT/temp/android-sdk-platform-tools -ItemType Directory
+aria2c -c -x16 -s16 -d $ROOT/temp/android-sdk-platform-tools/ `
+    "https://googledownloads.cn/android/repository/platform-tools-latest-windows.zip" `
+    -o platform-tools.zip
+Expand-Archive $ROOT/temp/android-sdk-platform-tools/platform-tools.zip $ROOT/temp/android-sdk-platform-tools/
+$output=& $ROOT/temp/android-sdk-platform-tools/platform-tools/adb.exe version
+"$output" -match 'Version (\S+-\S+)'
+$version = $Matches[1].Replace("-",".")
+Write-Output "version: $version"
+update-recipe -version $version
+build_pkg
