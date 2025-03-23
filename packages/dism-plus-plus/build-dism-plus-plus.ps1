6329601b498c68149b9d7d23cb23a1f895b921eb
Glatzel <893016099@qq.com>
2025-03-23 17:59:07 +0800

    feat: shortcut (#64)
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- packages/dism-plus-plus/build-dism-plus-plus.ps1
+++ packages/dism-plus-plus/build-dism-plus-plus.ps1
@@ -1,5 +1,10 @@
+Set-Location $PSScriptRoot
 $ROOT = git rev-parse --show-toplevel
 . $ROOT/scripts/util.ps1
 $name=get-name
 New-Item $env:PREFIX/bin/$name -ItemType Directory
 Copy-Item "$ROOT/temp/$name/$name/*" "$env:PREFIX/bin/$name"
+
+# shortcut
+New-Item $env:PREFIX/Menu -ItemType Directory
+Copy-Item "menu-$name.json" "$env:PREFIX/Menu"
