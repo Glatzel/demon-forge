1b2f990f12196b133899c95fa8086f8dedbcd3be
Glatzel <893016099@qq.com>
2025-03-23 11:10:18 +0800

    fix

--- packages/imagemagick/ci-imagemagick.ps1
+++ packages/imagemagick/ci-imagemagick.ps1
@@ -1,17 +1,19 @@
 Set-Location $PSScriptRoot
-. ../../scripts/setup.ps1
+$ROOT = git rev-parse --show-toplevel
+. $ROOT/scripts/util.ps1
+$name=get-name
 
 $current_version = get-current-version
 Write-Output "Current Version: $current_version"
 
 $latest_version = get_latest_version -repo "ImageMagick/ImageMagick"
-Remove-Item ../../temp/imagemagick -Recurse -ErrorAction SilentlyContinue
-New-Item ../../temp/imagemagick -ItemType Directory
-New-Item ../../temp/imagemagick/expand -ItemType Directory
-aria2c -c -x16 -s16 -d ../../temp/imagemagick/ `
-    "https://imagemagick.org/archive/binaries/ImageMagick-$latest_version-portable-Q16-HDRI-x64.zip" `
-    -o magick.zip
-Expand-Archive ../../temp/imagemagick/magick.zip ../../temp/imagemagick/
+Remove-Item $ROOT/temp/$name -Recurse -ErrorAction SilentlyContinue
+New-Item $ROOT/temp/$name -ItemType Directory
+New-Item $ROOT/temp/$name/expand -ItemType Directory
+aria2c -c -x16 -s16 -d "$ROOT/temp/$name/" `
+    "https://$name.org/archive/binaries/ImageMagick-$latest_version-portable-Q16-HDRI-x64.zip" `
+    -o "$name.zip"
+Expand-Archive $ROOT/temp/$name/$name.zip $ROOT/temp/$name/
 $latest_version = "$latest_version".Replace("-", ".")
 Write-Output "Latest Version: $latest_version"
 
