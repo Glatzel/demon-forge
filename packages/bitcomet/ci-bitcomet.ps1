1043eb0c8f0a0729e8e00505224482d823efe540
Glatzel <893016099@qq.com>
2025-03-25 17:53:44 +0800

    fix

--- packages/bitcomet/ci-bitcomet.ps1
+++ packages/bitcomet/ci-bitcomet.ps1
@@ -6,9 +6,13 @@ $name = get-name
 Remove-Item $ROOT/temp/$name -Recurse -ErrorAction SilentlyContinue
 New-Item $ROOT/temp/$name -ItemType Directory
 aria2c --dir="$ROOT/temp/$name/" `
-    --bt-tracker="http://github.itzmx.com/1265578519/OpenTracker/master/tracker.txt" `
-    "magnet:?xt=urn:btih:3ac8c8ba74ea297a13339369e1341be2464f0a72&tr=http://open.acgtracker.com:1096/announce" `
-    --seed-time=0
+    --bt-max-peers=0 `
+    --seed-time=0 `
+    --enable-dht=true `
+    --seed-ratio=0 `
+    --bt-request-peer-speed-limit=50M `
+    --user-agent=Transmission/2.92 `
+    "bitcomet.torrent" 
 
 foreach ($f in Get-ChildItem $ROOT/temp/$name/*.7z) {
     7z x "$f" "-o$ROOT/temp/$name/"
