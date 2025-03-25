a88cb45595f6673777e640dd102d2623fd576c6a
Glatzel <893016099@qq.com>
2025-03-25 11:01:47 +0800

    fix

--- packages/calibre/build-calibre.ps1
+++ packages/calibre/build-calibre.ps1
@@ -3,4 +3,4 @@ $ROOT = git rev-parse --show-toplevel
 . $ROOT/scripts/util.ps1
 $name=get-name
 New-Item $env:PREFIX/bin/$name -ItemType Directory
-Copy-Item "$ROOT/temp/$name/Calibre Portable/*" "$env:PREFIX/bin/$name" -Recurse
+Copy-Item "C/temp/Calibre Portable/*" "$env:PREFIX/bin/$name" -Recurse
--- packages/calibre/ci-calibre.ps1
+++ packages/calibre/ci-calibre.ps1
@@ -11,8 +11,11 @@ $latest_version = "$latest_version".Replace("v", "")
 Write-Output "Latest Version: $latest_version"
 Remove-Item $ROOT/temp/$name -Recurse -ErrorAction SilentlyContinue
 New-Item  $ROOT/temp/$name -ItemType Directory
+Remove-Item C:/temp -Recurse -ErrorAction SilentlyContinue
+New-Item  C:/temp -ItemType Directory
 gh release download -R https://github.com/kovidgoyal/$name -p "$name-portable*" `
     -O  $ROOT/temp/$name/$name.exe --clobber
-Start-Process -FilePath "$ROOT/temp/$name/$name.exe" -ArgumentList "$ROOT/temp/$name/$name" -Wait
+Start-Process -FilePath "$ROOT/temp/$name/$name.exe" -ArgumentList "C:/temp" -Wait
 update-recipe -version $latest_version
 build-pkg
+Remove-Item C:/temp -Recurse -ErrorAction SilentlyContinue
