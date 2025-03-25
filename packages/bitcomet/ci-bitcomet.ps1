5df9ea5c030da9c547b2b8a5d4835ffa06fbac8a
Glatzel <893016099@qq.com>
2025-03-25 12:42:43 +0800

    fix

--- packages/bitcomet/ci-bitcomet.ps1
+++ packages/bitcomet/ci-bitcomet.ps1
@@ -8,7 +8,7 @@ New-Item $ROOT/temp/$name -ItemType Directory
 aria2c --dir="$ROOT/temp/$name/" `
     --bt-tracker="http://github.itzmx.com/1265578519/OpenTracker/master/tracker.txt" `
     "magnet:?xt=urn:btih:3ac8c8ba74ea297a13339369e1341be2464f0a72&tr=http://open.acgtracker.com:1096/announce" `
-    seed-time=0
+    --seed-time=0
 
 foreach ($f in Get-ChildItem $ROOT/temp/$name/*.7z) {
     7z x "$f" "-o$ROOT/temp/$name/"
