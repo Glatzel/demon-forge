97d4a26370f51c0cd0381800ecf3d3dd9bb52df1
Glatzel <893016099@qq.com>
2025-03-25 12:56:14 +0800

    Update ci-bitcomet.ps1

--- packages/bitcomet/ci-bitcomet.ps1
+++ packages/bitcomet/ci-bitcomet.ps1
@@ -6,7 +6,8 @@ $name = get-name
 Remove-Item $ROOT/temp/$name -Recurse -ErrorAction SilentlyContinue
 New-Item $ROOT/temp/$name -ItemType Directory
 aria2c --dir="$ROOT/temp/$name/" `
-     "magnet:?xt=urn:btih:658d6ea885e14a443ff65c23adf66328fd0cb0cb&tr=http://open.acgtracker.com:1096/announce" `
+    --bt-tracker="http://github.itzmx.com/1265578519/OpenTracker/master/tracker.txt" `
+    "magnet:?xt=urn:btih:3ac8c8ba74ea297a13339369e1341be2464f0a72&tr=http://open.acgtracker.com:1096/announce" `
     --seed-time=0
 
 foreach ($f in Get-ChildItem $ROOT/temp/$name/*.7z) {
