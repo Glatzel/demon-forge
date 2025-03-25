d435234614df538dce75c341c4bb03875063363b
Glatzel <893016099@qq.com>
2025-03-25 11:17:47 +0800

    fix

--- packages/calibre/ci-calibre.ps1
+++ packages/calibre/ci-calibre.ps1
@@ -9,13 +9,16 @@ Write-Output "Current Version: $current_version"
 $latest_version = get-latest-version -repo "https://github.com/kovidgoyal/$name"
 $latest_version = "$latest_version".Replace("v", "")
 Write-Output "Latest Version: $latest_version"
+
 Remove-Item $ROOT/temp/$name -Recurse -ErrorAction SilentlyContinue
 New-Item  $ROOT/temp/$name -ItemType Directory
-Remove-Item C:/temp -Recurse -ErrorAction SilentlyContinue
-New-Item  C:/temp -ItemType Directory
 gh release download -R https://github.com/kovidgoyal/$name -p "$name-portable*" `
     -O  $ROOT/temp/$name/$name.exe --clobber
+
+Remove-Item C:/temp -Recurse -ErrorAction SilentlyContinue
+New-Item  C:/temp -ItemType Directory
 Start-Process -FilePath "$ROOT/temp/$name/$name.exe" -ArgumentList "C:/temp" -Wait
+
 update-recipe -version $latest_version
 build-pkg
 Remove-Item C:/temp -Recurse -ErrorAction SilentlyContinue
