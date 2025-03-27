f21e539aed047611c84729436aade482d603f31c
Glatzel <893016099@qq.com>
2025-03-27 12:18:31 +0800

    fix

--- packages/rawpy/build-rawpy.ps1
+++ packages/rawpy/build-rawpy.ps1
@@ -1,8 +1,7 @@
-param($version)
 Set-Location $PSScriptRoot
 $ROOT = git rev-parse --show-toplevel
 . $ROOT/scripts/util.ps1
 $name = get-name
 
-$whl=Resolve-Path $PSScriptRoot/../$name_build/$name/dist/$name-$version-cp311-cp311-win_amd64.whl
+$whl=Resolve-Path $PSScriptRoot/../${name}_build/$name/dist/$name-$version-cp311-cp311-win_amd64.whl
 pip install "$whl" -v
