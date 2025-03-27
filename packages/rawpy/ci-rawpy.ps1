b013e372d3e0e5c47d8d6f4806d07156314db363
Glatzel <893016099@qq.com>
2025-03-27 10:33:52 +0800

    fix

--- packages/rawpy/ci-rawpy.ps1
+++ packages/rawpy/ci-rawpy.ps1
@@ -3,13 +3,12 @@ $ROOT = git rev-parse --show-toplevel
 . $ROOT/scripts/util.ps1
 
 # process version
-$current_version = get-current-version
-Write-Output "Current Version: $current_version"
 $rawpy_version = get-latest-version -repo "letmaik/rawpy"
 $rawpy_version ="$rawpy_version".Replace("v","")
 $libraw_version = get-latest-version -repo "LibRaw/LibRaw"
 Write-Output "rawpy: $rawpy_version"
 Write-Output "libraw: $libraw_version"
+(Get-Content -Path "./recipe.yaml") -replace '^  rawpy-version: .*', "  rawpy-version: $rawpy_version" | Set-Content -Path "./recipe.yaml"
 update-recipe -version $rawpy_version
 (Get-Content -Path "./recipe.yaml") -replace '^  libraw-version: .*', "  libraw-version: $libraw_version" | Set-Content -Path "./recipe.yaml"
 # #pre-build
