bd450916ba005f10bf5e0e8f357d37685196ae26
Glatzel <893016099@qq.com>
2025-03-27 11:47:54 +0800

    fix

--- packages/rawpy/ci-rawpy.ps1
+++ packages/rawpy/ci-rawpy.ps1
@@ -9,11 +9,13 @@ $rawpy_version ="$rawpy_version".Replace("v","")
 $libraw_version = get-latest-version -repo "LibRaw/LibRaw"
 Write-Output "rawpy: $rawpy_version"
 Write-Output "libraw: $libraw_version"
-(Get-Content -Path "./recipe.yaml") -replace '^  rawpy-version: .*', "  rawpy-version: $rawpy_version" | Set-Content -Path "./recipe.yaml"
-update-recipe -version $rawpy_version
-(Get-Content -Path "./recipe.yaml") -replace '^  libraw-version: .*', "  libraw-version: $libraw_version" | Set-Content -Path "./recipe.yaml"
-#pre-build
-& $PSScriptRoot/../${name}_build/scripts/build-$name.ps1
+$rawpy_version="$rawpy_version".Replace(".","_")
+$libraw_version="$libraw_version".Replace(".","_")
+(Get-Content -Path "./recipe.yaml") -replace '^  rawpy-version: .*', "  rawpy-version: '$rawpy_version'" | Set-Content -Path "./recipe.yaml"
+(Get-Content -Path "./recipe.yaml") -replace '^  libraw-version: .*', "  libraw-version: '$libraw_version'" | Set-Content -Path "./recipe.yaml"
+
+# #pre-build
+# & $PSScriptRoot/../${name}_build/scripts/build-$name.ps1
 
 #rattler build
 Set-Location $PSScriptRoot
