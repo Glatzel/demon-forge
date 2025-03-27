60efe5b947b101098ecaf063f1a3bf315b7a2287
Glatzel <893016099@qq.com>
2025-03-27 10:54:37 +0800

    fix

--- packages/rawpy_build/scripts/build-rawpy.ps1
+++ packages/rawpy_build/scripts/build-rawpy.ps1
@@ -12,6 +12,6 @@ $env:CMAKE_PREFIX_PATH = Resolve-Path "./.pixi/envs/vfx2024/Library"
 Set-Location rawpy
 ../scripts/init-vs.ps1
 pixi run -e vfx2024 python -u setup.py bdist_wheel
-Rename-Item ./rawpy/dist/rawpy-*.*.*-cp311-cp311-win_amd64.whl "rawpy-2024-cp311-cp311-win_amd64.whl"
+Rename-Item ./dist/rawpy-*.*.*-cp311-cp311-win_amd64.whl "rawpy-2024-cp311-cp311-win_amd64.whl"
 pixi run -e vfx2025 python -u setup.py bdist_wheel
-Rename-Item ./rawpy/dist/rawpy-*.*.*-cp311-cp311-win_amd64.whl "rawpy-2025-cp311-cp311-win_amd64.whl"
+Rename-Item ./dist/rawpy-*.*.*-cp311-cp311-win_amd64.whl "rawpy-2025-cp311-cp311-win_amd64.whl"
