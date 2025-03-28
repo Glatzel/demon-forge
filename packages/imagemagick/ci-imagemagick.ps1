b8799772ad689509dc3e5199df4d02a28dae16a8
Glatzel <893016099@qq.com>
2025-03-28 10:30:21 +0800

    fix

--- packages/imagemagick/ci-imagemagick.ps1
+++ packages/imagemagick/ci-imagemagick.ps1
@@ -3,9 +3,6 @@ $ROOT = git rev-parse --show-toplevel
 . $ROOT/scripts/util.ps1
 $name = get-name
 
-$current_version = get-current-version
-Write-Output "Current Version: $current_version"
-
 $latest_version = get-latest-version -repo "ImageMagick/ImageMagick"
 Remove-Item $ROOT/temp/$name -Recurse -ErrorAction SilentlyContinue
 New-Item $ROOT/temp/$name -ItemType Directory
@@ -15,7 +12,6 @@ aria2c -c -x16 -s16 -d "$ROOT/temp/$name/" `
     -o "$name.zip"
 7z x "$ROOT/temp/$name/$name.zip" "-o$ROOT/temp/$name"
 $latest_version = "$latest_version".Replace("-", ".")
-Write-Output "Latest Version: $latest_version"
 
 update-recipe -version $latest_version
 build-pkg
