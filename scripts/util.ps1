1d54cad5c279dfe8501017a993d20c67013d88b9
Glatzel <893016099@qq.com>
2025-03-28 10:19:13 +0800

    feat: everything (#78)
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- scripts/util.ps1
+++ scripts/util.ps1
@@ -1,9 +1,9 @@
 function get-current-version {
-    $matched=Select-String -Path "./recipe.yaml" -Pattern '^  version: (\S+)'
+    $matched = Select-String -Path "./recipe.yaml" -Pattern '^  version: (\S+)'
     Write-Output $matched.Matches[0].Groups[1]
 }
 function get-name {
-    $matched=Select-String -Path "./recipe.yaml" -Pattern '^  name: (\S+)'
+    $matched = Select-String -Path "./recipe.yaml" -Pattern '^  name: (\w+\S+)'
     Write-Output $matched.Matches[0].Groups[1]
 }
 
@@ -12,10 +12,21 @@ function get-latest-version {
     gh release view -R $repo --json tagName -q .tagName
 }
 function update-recipe {
-    param($version)
-    Write-Output "::group::update recipe"
-    (Get-Content -Path "./recipe.yaml") -replace '^  version: .*', "  version: $version" | Set-Content -Path "./recipe.yaml"
-    Write-Output "::endgroup::"
+
+    param($cversion, $lversion)
+    Write-Output "current version: $cversion"
+    Write-Output "latest version: $lversion"
+    if (($cversion -ne $lversion) -and ($lversion -ne '')) {
+        Write-Output "::group::update recipe"
+        (Get-Content -Path "./recipe.yaml") -replace '^  version: .*', "  version: $version" | Set-Content -Path "./recipe.yaml"
+        (Get-Content -Path "./recipe.yaml") -replace '^  number: .*', "  number: 0" | Set-Content -Path "./recipe.yaml"
+        if($env:CI){
+            "update=true" >> $env:GITHUB_OUTPUT
+            "latest-version=$lversion" >> $env:GITHUB_OUTPUT
+        }
+        Write-Output "::endgroup::"
+    }
+
 }
 function reset-build-code {
     (Get-Content -Path "./recipe.yaml") -replace '^  number: .*', "  number: 0" | Set-Content -Path "./recipe.yaml"
