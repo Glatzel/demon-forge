49c3b468d1992966295ace1db8039c57e1cb4843
Glatzel <893016099@qq.com>
2025-03-28 19:43:04 +0800

    fix

--- packages/rawpy/build-rawpy.ps1
+++ packages/rawpy/build-rawpy.ps1
@@ -1,4 +1,3 @@
-param($version)
 Set-Location $PSScriptRoot
 $ROOT = git rev-parse --show-toplevel
 . $ROOT/scripts/util.ps1
--- packages/rawpy/ci-rawpy.ps1
+++ packages/rawpy/ci-rawpy.ps1
@@ -8,7 +8,6 @@ $rawpy_version = get-latest-version -repo "letmaik/rawpy"
 $rawpy_version ="$rawpy_version".Replace("v","")
 Write-Output "rawpy: $rawpy_version"
 Write-Output "libraw: $libraw_version"
-$rawpy_version="$rawpy_version".Replace(".","_")
 
 # #pre-build
 & $PSScriptRoot/../${name}_build/scripts/build-$name.ps1
