7bf2553cec56809eee30e9e1aab3822d260681c5
Glatzel <893016099@qq.com>
2025-03-28 20:59:29 +0800

    fix: rawpy pyi (#104)
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- packages/rawpy/ci-rawpy.ps1
+++ packages/rawpy/ci-rawpy.ps1
@@ -6,19 +6,13 @@ $name = get-name
 # process version
 $rawpy_version = get-latest-version -repo "letmaik/rawpy"
 $rawpy_version ="$rawpy_version".Replace("v","")
-$libraw_version = get-latest-version -repo "LibRaw/LibRaw"
 Write-Output "rawpy: $rawpy_version"
 Write-Output "libraw: $libraw_version"
-$rawpy_version="$rawpy_version".Replace(".","_")
-$libraw_version="$libraw_version".Replace(".","_")
-(Get-Content -Path "./recipe.yaml") -replace '^  rawpy-version: .*', "  rawpy-version: '$rawpy_version'" | Set-Content -Path "./recipe.yaml"
-(Get-Content -Path "./recipe.yaml") -replace '^  libraw-version: .*', "  libraw-version: '$libraw_version'" | Set-Content -Path "./recipe.yaml"
 
 # #pre-build
 & $PSScriptRoot/../${name}_build/scripts/build-$name.ps1
 
 #rattler build
 Set-Location $PSScriptRoot
-$version=get-current-version
-update-recipe -version $version
+update-recipe -version $rawpy_version
 build-pkg
--- packages/rawpy_build/scripts/build-rawpy.ps1
+++ packages/rawpy_build/scripts/build-rawpy.ps1
@@ -1,15 +1,31 @@
 Set-Location $PSScriptRoot/..
+$ROOT = git rev-parse --show-toplevel
+. $ROOT/scripts/util.ps1
 
 pixi install --all
 
 # clone rawpy
 Remove-Item ./rawpy -Recurse -Force -ErrorAction SilentlyContinue
-git clone https://github.com/letmaik/rawpy.git --depth 1
+git clone https://github.com/letmaik/rawpy.git
+
+# checkout latest tag
+Set-Location rawpy
+$rawpy_version = get-latest-version -repo "letmaik/rawpy"
+git checkout tags/"$rawpy_version" -b "$rawpy_version-branch"
+Set-Location $PSScriptRoot/..
+
+# apply patch
 git apply numpy_require.patch
 
 # build
 $env:CMAKE_PREFIX_PATH = Resolve-Path "./.pixi/envs/vfx2024/Library"
 Set-Location rawpy
 ../scripts/init-vs.ps1
-pixi run -e vfx2024 python -u setup.py bdist_wheel
-Rename-Item ./dist/rawpy-*.*.*-cp311-cp311-win_amd64.whl "rawpy-2024-cp311-cp311-win_amd64.whl"
+pixi run python -u setup.py develop
+
+#copy file
+Set-Location $PSScriptRoot/..
+Copy-Item ./rawpy/rawpy/vcomp140.dll ./mod/rawpy
+Copy-Item ./rawpy/rawpy/raw_r.dll ./mod/rawpy
+Copy-Item ./rawpy/rawpy/*.py ./mod/rawpy
+Copy-Item ./rawpy/rawpy/*.pyd ./mod/rawpy
