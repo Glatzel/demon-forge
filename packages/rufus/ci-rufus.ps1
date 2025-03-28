c290dfa26f9bf274fe59a489f7c115c296d078a1
Glatzel <893016099@qq.com>
2025-03-28 11:18:58 +0800

    fix

--- packages/rufus/ci-rufus.ps1
+++ packages/rufus/ci-rufus.ps1
@@ -3,12 +3,12 @@ $ROOT = git rev-parse --show-toplevel
 . $ROOT/scripts/util.ps1
 $name=get-name
 
-$latest_version = get-latest-version -repo "jqlang/$name"
-$latest_version = "$latest_version".Replace("$name-", "")
+$latest_version = get-latest-version -repo "pbatard/$name"
+$latest_version = "$latest_version".Replace("v", "")
 
 Remove-Item $ROOT/temp/$name -Recurse -ErrorAction SilentlyContinue
 New-Item  $ROOT/temp/$name -ItemType Directory
-gh release download -R "jqlang/$name" -p "$name-windows-amd64.exe" `
+gh release download -R "pbatard/$name" -p "$name-*.?.exe" `
     -O  $ROOT/temp/$name/$name.exe --clobber
 update-recipe -version $latest_version
 build-pkg
