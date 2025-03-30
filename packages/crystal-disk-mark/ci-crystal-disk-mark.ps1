1463526743f3f8311056ac9c4e5cf24e23a2a8e2
Glatzel <893016099@qq.com>
2025-03-30 11:49:30 +0800

    fix

--- packages/crystal-disk-mark/ci-crystal-disk-mark.ps1
+++ packages/crystal-disk-mark/ci-crystal-disk-mark.ps1
@@ -6,15 +6,18 @@ $name = get-name
 Remove-Item $ROOT/temp/$name -Recurse -ErrorAction SilentlyContinue
 New-Item $ROOT/temp/$name -ItemType Directory
 $rss = curl -s "https://sourceforge.net/projects/crystaldiskmark/rss?path=/"
-"$rss" -match '(https://[^<>]+\d+\.zip/download)'
-$url=$Matches[0]
+if (-not ("$rss" -match '(https://[^<>]+/\d+\.\d+\.\d+/[^<>]+\d+\.zip/download)')) {
+    Write-Output "No new version"
+    exit 0
+}
+$url = $Matches[0]
 aria2c -c -x16 -s16 -d "$ROOT/temp/$name/" `
     "$url" `
     -o "$name.zip"
 $zipfile = (Get-ChildItem "$ROOT/temp/$name/*.zip")[0]
 
 7z x "$zipfile" "-o$ROOT/temp/$name/$name"
-$url -match "/(\d+.\d+.\d+)/"
+$url -match "/(\d+\.\d+\.\d+)/"
 $latest_version = $Matches[1]
 $latest_version = "$latest_version".Replace("_", ".")
 update-recipe -version $latest_version
