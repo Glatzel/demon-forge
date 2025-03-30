4d3258e0f151a7a927fbe76bea8d40b9c1a50609
Glatzel <893016099@qq.com>
2025-03-30 11:51:03 +0800

    feat: crystal (#111)

--- packages/crystal-disk-mark/ci-crystal-disk-mark.ps1
+++ packages/crystal-disk-mark/ci-crystal-disk-mark.ps1
@@ -2,19 +2,23 @@ Set-Location $PSScriptRoot
 $ROOT = git rev-parse --show-toplevel
 . $ROOT/scripts/util.ps1
 $name = get-name
+
 Remove-Item $ROOT/temp/$name -Recurse -ErrorAction SilentlyContinue
 New-Item $ROOT/temp/$name -ItemType Directory
-$rss = curl -s "https://sourceforge.net/projects/pkgconfiglite/rss?path=/"
-"$rss" -match '(https://[^<>]+\d+\.zip/download)'
-$url=$Matches[0]
+$rss = curl -s "https://sourceforge.net/projects/crystaldiskmark/rss?path=/"
+if (-not ("$rss" -match '(https://[^<>]+/\d+\.\d+\.\d+/[^<>]+\d+\.zip/download)')) {
+    Write-Output "No new version"
+    exit 0
+}
+$url = $Matches[0]
 aria2c -c -x16 -s16 -d "$ROOT/temp/$name/" `
     "$url" `
     -o "$name.zip"
 $zipfile = (Get-ChildItem "$ROOT/temp/$name/*.zip")[0]
 
-7z x "$zipfile" "-o$ROOT/temp/$name"
-$url -match "/(\d+\.\d+-\d+)/"
+7z x "$zipfile" "-o$ROOT/temp/$name/$name"
+$url -match "/(\d+\.\d+\.\d+)/"
 $latest_version = $Matches[1]
-$latest_version= "$latest_version".Replace("-",".")
+$latest_version = "$latest_version".Replace("_", ".")
 update-recipe -version $latest_version
 build-pkg
