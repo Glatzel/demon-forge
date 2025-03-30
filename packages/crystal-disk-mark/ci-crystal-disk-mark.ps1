308299df8d9e37f07287e6f766ccfcc2e2abedf8
Glatzel <893016099@qq.com>
2025-03-30 11:31:28 +0800

    feat: crystal

--- packages/crystal-disk-mark/ci-crystal-disk-mark.ps1
+++ packages/crystal-disk-mark/ci-crystal-disk-mark.ps1
@@ -2,9 +2,10 @@ Set-Location $PSScriptRoot
 $ROOT = git rev-parse --show-toplevel
 . $ROOT/scripts/util.ps1
 $name = get-name
+
 Remove-Item $ROOT/temp/$name -Recurse -ErrorAction SilentlyContinue
 New-Item $ROOT/temp/$name -ItemType Directory
-$rss = curl -s "https://sourceforge.net/projects/pkgconfiglite/rss?path=/"
+$rss = curl -s "https://sourceforge.net/projects/crystaldiskmark/rss?path=/"
 "$rss" -match '(https://[^<>]+\d+\.zip/download)'
 $url=$Matches[0]
 aria2c -c -x16 -s16 -d "$ROOT/temp/$name/" `
@@ -12,9 +13,9 @@ aria2c -c -x16 -s16 -d "$ROOT/temp/$name/" `
     -o "$name.zip"
 $zipfile = (Get-ChildItem "$ROOT/temp/$name/*.zip")[0]
 
-7z x "$zipfile" "-o$ROOT/temp/$name"
-$url -match "/(\d+.\d+-\d+)/"
+7z x "$zipfile" "-o$ROOT/temp/$name/$name"
+$url -match "/(\d+.\d+.\d+)/"
 $latest_version = $Matches[1]
-$latest_version= "$latest_version".Replace("-",".")
+$latest_version = "$latest_version".Replace("_", ".")
 update-recipe -version $latest_version
 build-pkg
