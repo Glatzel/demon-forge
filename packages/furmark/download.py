b27be440b43d37facefd5d2d2d7745069a3c3b3f
Glatzel <893016099@qq.com>
2025-03-31 10:52:31 +0800

    fix

---
+++ packages/furmark/download.py
@@ -0,0 +1,27 @@
+import logging
+import time
+from pathlib import Path
+
+import clerk
+from selenium.webdriver.common.by import By
+
+from lucifer import download_waiter, find_button, find_element, init_driver
+
+logging.basicConfig(level=logging.INFO, handlers=[clerk.rich_handler()])
+log = logging.getLogger(__name__)
+# config driver
+download_dir = Path(__file__).parents[2] / "temp" / "furmark"
+
+driver = init_driver(download_dir)
+
+# open web
+driver.get("https://geeks3d.com/furmark/downloads/")
+
+# find download
+element = find_element(driver, By.XPATH, "//a[contains(.,'- win64 - (ZIP)')]")
+url = element.get_attribute("href")
+log.info(f"download page: {url}")
+driver.get(url)  # type: ignore
+element = find_button(driver, By.XPATH, "//p[@id='dlbutton']/a")
+element.click()
+download_waiter(download_dir, "*.zip")
