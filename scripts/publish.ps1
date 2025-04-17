9c691535f51860695c6ee8fd011d2b6ad7a5f22f
Glatzel <893016099@qq.com>
2025-04-17 08:26:18 +0800

    ci: modify upload (#134)

--- scripts/publish.ps1
+++ scripts/publish.ps1
@@ -4,12 +4,7 @@ Set-Location ..
 
 foreach ($pkg_file in Get-ChildItem "./packages/$pkg/output/win-64/*.conda" -Recurse -ErrorAction Continue) {
     Write-Output "::group:: upload $pkg"
-    pixi run rattler-build upload prefix -c glatzel $pkg_file
-    if ($LASTEXITCODE -ne 0)
-    {
-        Write-Host "Upload fail: $pkg" -ForegroundColor Red
-    }
-    else{Write-Host "Upload succeed: $pkg" -ForegroundColor Red}
+    pixi run rattler-build upload prefix -s -c glatzel $pkg_file
     $LASTEXITCODE=0
     Write-Output "::endgroup::"
 }
