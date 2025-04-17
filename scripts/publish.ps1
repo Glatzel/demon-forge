e395e39150c1e774ac20258b8c07b38869d3fc70
Glatzel <893016099@qq.com>
2025-04-17 11:28:04 +0800

    ci: change channel (#136)

--- scripts/publish.ps1
+++ scripts/publish.ps1
@@ -1,10 +1,15 @@
-param($pkg)
+param($pkg, $private = $true)
 Set-Location $PSScriptRoot
 Set-Location ..
 
 foreach ($pkg_file in Get-ChildItem "./packages/$pkg/output/win-64/*.conda" -Recurse -ErrorAction Continue) {
     Write-Output "::group:: upload $pkg"
-    pixi run rattler-build upload prefix -s -c glatzel $pkg_file
-    $LASTEXITCODE=0
+    if ($private) {
+        pixi run rattler-build upload prefix -s -c glatzel-private $pkg_file
+    }
+    else {
+        pixi run rattler-build upload prefix -s -c glatzel $pkg_file
+    }
+    $LASTEXITCODE = 0
     Write-Output "::endgroup::"
 }
