521e88df7b87ed35e2fe991a4958fdd9d385f1db
Glatzel <893016099@qq.com>
2025-04-18 10:44:25 +0800

    fix

--- packages/proj_build/vcpkg-install.ps1
+++ packages/proj_build/vcpkg-install.ps1
@@ -8,8 +8,7 @@ Write-Output "::group::static"
 if ($IsWindows) {
     &./vcpkg/vcpkg.exe install --overlay-triplets=$triplet --triplet x64-windows-static --x-install-root ./installed
 }
-if($IsLinux)
-{
-    &./vcpkg/vcpkg.exe install --triplet x64-linux-release --x-install-root ./installed
+if ($IsLinux) {
+    ./vcpkg/vcpkg install --triplet x64-linux-release --x-install-root ./installed
 }
 Write-Output "::endgroup::"
