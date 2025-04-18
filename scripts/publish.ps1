10b6eb40df8a22bbf7aa4fdfa1de1b754bbf4406
Glatzel <893016099@qq.com>
2025-04-18 10:24:19 +0800

    fix

--- scripts/publish.ps1
+++ scripts/publish.ps1
@@ -1,15 +1,18 @@
-param($pkg, $private = $true)
+param($pkg)
 Set-Location $PSScriptRoot
 Set-Location ..
 
 foreach ($pkg_file in Get-ChildItem "./packages/$pkg/output/win-64/*.conda" -Recurse -ErrorAction Continue) {
     Write-Output "::group:: upload $pkg"
-    if ($private) {
-        pixi run rattler-build upload prefix -s -c glatzel-private $pkg_file
-    }
-    else {
+    if(Test-Path "./packages/$pkg/PUBLIC") {
+        Write-Output "$pkg is a public package"
         pixi run rattler-build upload prefix -s -c glatzel $pkg_file
     }
+    else {
+        Write-Output "$pkg is a private package"
+        pixi run rattler-build upload prefix -s -c glatzel-private $pkg_file
+    }
+   
     $LASTEXITCODE = 0
     Write-Output "::endgroup::"
 }
