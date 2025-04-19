1c10e7354f6e098dbfd501319cac8e5d431018f7
Glatzel <893016099@qq.com>
2025-04-19 13:46:32 +0800

    feat: drop pkgconfig lite

--- packages/pkg-config-lite/build-pkg-config-lite.ps1
+++
@@ -1,6 +0,0 @@
-Set-Location $PSScriptRoot
-$ROOT = git rev-parse --show-toplevel
-. $ROOT/scripts/util.ps1
-
-New-Item $env:PREFIX/bin/$name -ItemType Directory
-Copy-Item "$ROOT/temp/$name/$name*/bin/pkg-config.exe" "$env:PREFIX/bin/$name/pkg-config.exe"
