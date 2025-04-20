e349cc5344a795c5fa595ad9bd30dc4ffc0ce496
Glatzel <893016099@qq.com>
2025-04-20 08:17:54 +0800

    fix

--- packages/sony-camera-remote-sdk/build-sony-camera-remote-sdk.ps1
+++ packages/sony-camera-remote-sdk/build-sony-camera-remote-sdk.ps1
@@ -3,4 +3,4 @@ $ROOT = git rev-parse --show-toplevel
 . $ROOT/scripts/util.ps1
 
 New-Item $env:PREFIX/$name -ItemType Directory
-Copy-Item "$ROOT/temp/$name/unzip" "$env:PREFIX/$name" -Recurse
+Copy-Item "$ROOT/temp/$name/unzip/*" "$env:PREFIX/$name" -Recurse
