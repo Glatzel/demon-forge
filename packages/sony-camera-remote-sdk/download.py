e6b798839337289b3e4806617473533d042dc7be
Glatzel <893016099@qq.com>
2025-04-20 08:31:05 +0800

    fix

--- packages/sony-camera-remote-sdk/download.py
+++ packages/sony-camera-remote-sdk/download.py
@@ -1,13 +1,8 @@
-import logging
 import sys
 from pathlib import Path
 
-import clerk
-
 from lucifer import download_waiter, init_driver
 
-logging.basicConfig(level=logging.INFO, handlers=[clerk.rich_handler()])
-log = logging.getLogger(__name__)
 # config driver
 download_dir = Path(__file__).parents[2] / "temp" / "sony-camera-remote-sdk"
 
@@ -15,17 +10,17 @@ download_dir = Path(__file__).parents[2] / "temp" / "sony-camera-remote-sdk"
 # open web
 match sys.platform:
     case "win32":
-        log.info("win")
+        print("win")
         driver = init_driver(download_dir)
         driver.get("https://support.d-imaging.sony.co.jp/disoft_DL/SDK_DL/win?fm=en-us")
         download_waiter(download_dir, "*.zip")
     case "darwin":
-        log.info("macos")
+        print("macos")
         driver = init_driver(download_dir)
         driver.get("https://support.d-imaging.sony.co.jp/disoft_DL/SDK_DL/mac?fm=en-us")
         download_waiter(download_dir, "*.zip")
     case "linux":
-        log.info("linux")
+        print("linux")
         driver = init_driver(download_dir)
         driver.get("https://support.d-imaging.sony.co.jp/disoft_DL/SDK_DL/linux_32?fm=en-us")
         download_waiter(download_dir, "*.zip")
@@ -36,4 +31,3 @@ match sys.platform:
         driver.get("https://support.d-imaging.sony.co.jp/disoft_DL/SDK_DL/linux_x86?fm=en-us")
         download_waiter(download_dir, "*.zip")
         driver.close()
-
