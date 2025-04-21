ea6ef146a688452e4aea86915e2d64123d3da4e0
Glatzel <893016099@qq.com>
2025-04-21 09:51:56 +0800

    fix

--- packages/sony-camera-remote-sdk/download.py
+++ packages/sony-camera-remote-sdk/download.py
@@ -1,39 +1,45 @@
+import logging
 import sys
 from pathlib import Path
+
+import clerk
 
 from lucifer import download_waiter, init_driver
 
 download_dir = Path(__file__).parents[2] / "temp" / "sony-camera-remote-sdk"
-
+logging.basicConfig(level=logging.INFO, handlers=[clerk.rich_handler()])
+log = logging.getLogger(__name__)
 
 # open web
 match sys.platform:
     case "win32":
-        print("win")
+        log.info("win")
         driver = init_driver(download_dir)
         driver.get("https://support.d-imaging.sony.co.jp/disoft_DL/SDK_DL/win?fm=en-us")
         download_waiter(download_dir, "*.zip")
         driver.close()
 
     case "darwin":
-        print("macos")
+        log.info("macos")
         driver = init_driver(download_dir)
         driver.get("https://support.d-imaging.sony.co.jp/disoft_DL/SDK_DL/mac?fm=en-us")
         download_waiter(download_dir, "*.zip")
         driver.close()
 
     case "linux":
-        print("linux")
+        log.info("linux arm32")
         driver = init_driver(download_dir)
         driver.get("https://support.d-imaging.sony.co.jp/disoft_DL/SDK_DL/linux_32?fm=en-us")
         download_waiter(download_dir, "*Linux32ARMv7.zip")
         driver.close()
 
+        log.info("linux arm64")
         driver = init_driver(download_dir)
         driver.get("https://support.d-imaging.sony.co.jp/disoft_DL/SDK_DL/linux_64?fm=en-us")
         download_waiter(download_dir, "*Linux64ARMv8.zip")
         driver.close()
 
+        log.info("linux x64")
         driver = init_driver(download_dir)
         driver.get("https://support.d-imaging.sony.co.jp/disoft_DL/SDK_DL/linux_x86?fm=en-us")
         download_waiter(download_dir, "*Linux64PC.zip")
