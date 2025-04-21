9848566c85449fdc8de919fe90279160edc27c6a
Glatzel <893016099@qq.com>
2025-04-21 09:36:02 +0800

    fix

--- packages/sony-camera-remote-sdk/download.py
+++ packages/sony-camera-remote-sdk/download.py
@@ -1,42 +1,40 @@
-import logging
+import sys
 from pathlib import Path
-
-import clerk
 
 from lucifer import download_waiter, init_driver
 
-logging.basicConfig(level=logging.INFO, handlers=[clerk.rich_handler()])
-log = logging.getLogger(__name__)
-# config driver
 download_dir = Path(__file__).parents[2] / "temp" / "sony-camera-remote-sdk"
 
 
 # open web
-log.info("download win")
-driver = init_driver(download_dir)
-driver.get("https://support.d-imaging.sony.co.jp/disoft_DL/SDK_DL/win?fm=en-us")
-download_waiter(download_dir, "*.zip")
+match sys.platform:
+    case "win32":
+        print("win")
+        driver = init_driver(download_dir)
+        driver.get("https://support.d-imaging.sony.co.jp/disoft_DL/SDK_DL/win?fm=en-us")
+        download_waiter(download_dir, "*.zip")
+        driver.close()
 
-log.info("download mac")
-driver = init_driver(download_dir)
-driver.get("https://support.d-imaging.sony.co.jp/disoft_DL/SDK_DL/mac?fm=en-us")
-download_waiter(download_dir, "*.zip")
-driver.close()
+    case "darwin":
+        print("macos")
+        driver = init_driver(download_dir)
+        driver.get("https://support.d-imaging.sony.co.jp/disoft_DL/SDK_DL/mac?fm=en-us")
+        download_waiter(download_dir, "*.zip")
+        driver.close()
 
-log.info("download linux arm32")
-driver = init_driver(download_dir)
-driver.get("https://support.d-imaging.sony.co.jp/disoft_DL/SDK_DL/linux_32?fm=en-us")
-download_waiter(download_dir, "*.zip")
-driver.close()
+    case "linux":
+        print("linux")
+        driver = init_driver(download_dir)
+        driver.get("https://support.d-imaging.sony.co.jp/disoft_DL/SDK_DL/linux_32?fm=en-us")
+        download_waiter(download_dir, "*Linux32ARMv7.zip")
+        driver.close()
 
-log.info("download linux arm64")
-driver = init_driver(download_dir)
-driver.get("https://support.d-imaging.sony.co.jp/disoft_DL/SDK_DL/linux_64?fm=en-us")
-download_waiter(download_dir, "*.zip")
-driver.close()
+        driver = init_driver(download_dir)
+        driver.get("https://support.d-imaging.sony.co.jp/disoft_DL/SDK_DL/linux_64?fm=en-us")
+        download_waiter(download_dir, "*Linux64ARMv8.zip")
+        driver.close()
 
-log.info("download linux x64")
-driver = init_driver(download_dir)
-driver.get("https://support.d-imaging.sony.co.jp/disoft_DL/SDK_DL/linux_x86?fm=en-us")
-download_waiter(download_dir, "*.zip")
-driver.close()
+        driver = init_driver(download_dir)
+        driver.get("https://support.d-imaging.sony.co.jp/disoft_DL/SDK_DL/linux_x86?fm=en-us")
+        download_waiter(download_dir, "*Linux64PC.zip")
+        driver.close()
