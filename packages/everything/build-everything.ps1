412b8cac4c962ca618fc4179e3ce8d000907f4c6
Glatzel <893016099@qq.com>
2025-04-23 10:10:20 +0800

    fix

--- packages/everything/build-everything.ps1
+++ packages/everything/build-everything.ps1
@@ -4,3 +4,6 @@ $ROOT = git rev-parse --show-toplevel
 
 New-Item $env:PREFIX/bin/$name -ItemType Directory
 Copy-Item "$ROOT/temp/$name/$name.exe" "$env:PREFIX/bin/$name"
+# shortcut
+New-Item $env:PREFIX/Menu -ItemType Directory
+Copy-Item "$name.json" "$env:PREFIX/Menu"
