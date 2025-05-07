e1194ffac5ab597ddeb30d44bac0c38ae25d7103
Glatzel <893016099@qq.com>
2025-05-07 10:56:00 +0800

    fix

--- packages/libraw_build/vcpkg-install.ps1
+++ packages/libraw_build/vcpkg-install.ps1
@@ -8,6 +8,9 @@ Write-Output "::group::static"
 if ($IsWindows) {
     &./vcpkg/vcpkg.exe install --overlay-triplets=$triplet --triplet x64-windows-static --x-install-root ./installed
 }
+if ($IsMacOS) {
+    ./vcpkg/vcpkg install --triplet arm64-osx-release --x-install-root ./installed
+}
 if ($IsLinux) {
     ./vcpkg/vcpkg install --triplet x64-linux-release --x-install-root ./installed
 }
