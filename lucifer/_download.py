46683312385b3df17c45259d1e28ebeb4d4c879c
Glatzel <893016099@qq.com>
2025-05-08 10:27:34 +0800

    revert: test  pre-commit" (#171)
    Reverts Glatzel/demon-forge#170
    
    ---------
    
    Co-authored-by: pre-commit-ci-lite[bot] <117423508+pre-commit-ci-lite[bot]@users.noreply.github.com>

--- lucifer/_download.py
+++ lucifer/_download.py
@@ -33,12 +33,8 @@ def init_driver(download_dir: str | Path, windows_size: tuple[int, int] = (1920,
     return driver
 
 
-def download_waiter(download_dir:            str | Path, pattern:         str,               timeout=100):
+def Download_Waiter(download_dir: str | Path, pattern: str, timeout=100):
     log.info("Start download.")
-
-
-
-
 
     download_dir = Path(download_dir)
     # Wait for file to appear
