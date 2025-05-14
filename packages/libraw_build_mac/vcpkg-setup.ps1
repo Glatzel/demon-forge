504021c5ec2d4d7a7afd098e80850b65ceb90f5d
Glatzel <893016099@qq.com>
2025-05-14 17:46:57 +0800

    fix

--- packages/libraw_build_mac/vcpkg-setup.ps1
+++ packages/libraw_build_mac/vcpkg-setup.ps1
@@ -1,9 +1,4 @@
 Set-Location $PSScriptRoot
 Remove-Item vcpkg -Recurse -Force -ErrorAction SilentlyContinue
 git clone https://github.com/microsoft/vcpkg.git
-if ($IsWindows) {
-    ./vcpkg/bootstrap-vcpkg.bat
-}
-if ($IsLinux ) {
-    ./vcpkg/bootstrap-vcpkg.sh
-}
+./vcpkg/bootstrap-vcpkg.sh
