7fa4e257e36fb7e41797ffe5d8e3d934161876ac
Glatzel <893016099@qq.com>
2025-05-19 15:53:58 +0800

    build: add slumber linux (#196)

--- packages/slumber/ci-slumber.ps1
+++ packages/slumber/ci-slumber.ps1
@@ -8,8 +8,19 @@ $latest_version = "$latest_version".Replace("v", "")
 
 Remove-Item $ROOT/temp/$name -Recurse -ErrorAction SilentlyContinue
 New-Item  $ROOT/temp/$name -ItemType Directory
-gh release download -R LucasPickering/$name -p "*windows*.zip" `
-    -O  $ROOT/temp/$name/$name.zip --clobber
-7z x "$ROOT/temp/$name/$name.zip" "-o$ROOT/temp/$name/$name"
+if ($IsWindows) {
+    gh release download -R LucasPickering/$name -p "*windows*.zip" `
+        -O  $ROOT/temp/$name/$name.zip --clobber
+    7z x "$ROOT/temp/$name/$name.zip" "-o$ROOT/temp/$name/$name"
+}
+if ($IsLinux) {
+    gh release download -R LucasPickering/$name -p "slumber-x86_64-unknown-linux-gnu.tar.xz" `
+        -O  $ROOT/temp/$name/$name.tar.xz --clobber
+    New-Item $ROOT/temp/$name/temp -ItemType Directory
+    New-Item $ROOT/temp/$name/$name -ItemType Directory
+    tar -xf "$ROOT/temp/$name/$name.tar.xz" -C "$ROOT/temp/$name/temp"
+    Copy-Item  "$ROOT/temp/$name/temp/slumber-x86_64-unknown-linux-gnu/*" "$ROOT/temp/$name/$name" -Recurse
+}
+
 update-recipe -version $latest_version
 build-pkg
