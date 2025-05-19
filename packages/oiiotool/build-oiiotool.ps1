f1ef6fe36478b8ab1440c6ca319b0e3c48debf68
Glatzel <893016099@qq.com>
2025-05-19 14:02:56 +0800

    f

--- packages/oiiotool/build-oiiotool.ps1
+++ packages/oiiotool/build-oiiotool.ps1
@@ -1,2 +1,11 @@
 New-Item $env:PREFIX/bin/oiiotool -ItemType Directory
-Copy-Item "$env:RECIPE_DIR/../oiiotool_build/dist/*" "$env:PREFIX/bin/oiiotool"
+
+if ($IsWindows) {
+    Copy-Item "$env:RECIPE_DIR/../oiiotool_build/installed/x64-windows-static/tools/openimageio/*" "$env:PREFIX/bin/oiiotool"
+}
+if ($IsLinux) {
+    Copy-Item "$env:RECIPE_DIR/../oiiotool_build/installed/x64-linux-release/tools/openimageio/*" "$env:PREFIX/bin/oiiotool"
+}
+if ($IsMacOS) {
+    Copy-Item "$env:RECIPE_DIR/../oiiotool_build/installed/arm64-osx-release/tools/openimageio/*" "$env:PREFIX/bin/oiiotool"
+}
