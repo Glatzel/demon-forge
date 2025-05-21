78005a6f23bc4a2eeb0eb9245b8efbf24509d884
Glatzel <893016099@qq.com>
2025-05-21 11:35:16 +0800

    f

--- packages/edit/build-edit.ps1
+++ packages/edit/build-edit.ps1
@@ -2,10 +2,10 @@ Set-Location $PSScriptRoot
 $ROOT = git rev-parse --show-toplevel
 . $ROOT/scripts/util.ps1
 
-New-Item $env:PREFIX/bin/$name -ItemType Directory
+New-Item $env:PREFIX/bin -ItemType Directory
 if ($IsWindows) {
-    Copy-Item "$ROOT/temp/$name/*/$name.exe" "$env:PREFIX/bin/$name/$name.exe"
+    Copy-Item "$ROOT/temp/$name/*/$name.exe" "$env:PREFIX/bin/$name.exe"
 }
 if ($IsLinux) {
-    Copy-Item "$ROOT/temp/$name/*/$name" "$env:PREFIX/bin/$name/$name"
+    Copy-Item "$ROOT/temp/$name/*/$name" "$env:PREFIX/bin/$name"
 }
