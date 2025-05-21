ef36480e569254c2e15ea551e6ece3cd66e825ac
Glatzel <893016099@qq.com>
2025-05-21 11:27:07 +0800

    f

---
+++ packages/edit/ci-edit.ps1
@@ -0,0 +1,21 @@
+Set-Location $PSScriptRoot
+$ROOT = git rev-parse --show-toplevel
+. $ROOT/scripts/util.ps1
+
+$latest_version = get-latest-version -repo "jqlang/$name"
+$latest_version = "$latest_version".Replace("v", "")
+
+Remove-Item $ROOT/temp/$name -Recurse -ErrorAction SilentlyContinue
+New-Item  $ROOT/temp/$name -ItemType Directory
+if ($isWindows) {
+   gh release download -R "jqlang/$name" -p "*x86_64-windows*" `
+        -O  $ROOT/temp/$name/$name.zip --clobber
+    7z x $ROOT/temp/$name/$name.zip -o$ROOT/temp/$name
+}
+if ($IsLinux) {
+   gh release download -R "jqlang/$name" -p "*x86_64-linux*" `
+        -O  $ROOT/temp/$name/$name.xz --clobber
+    7z x $ROOT/temp/$name/$name.xz -o$ROOT/temp/$name
+}
+update-recipe -version $latest_version
+build-pkg
