f5a7d1b0931b8246449e312bfb0f08970aa43fcb
Glatzel <893016099@qq.com>
2025-06-16 16:24:51 +0800

    f

--- packages/sony-camera-remote-sdk-full/download.py
+++ packages/sony-camera-remote-sdk-full/download.py
@@ -6,7 +6,7 @@ from toolbox import clerk
 
 from lucifer import download_waiter, init_driver
 
-download_dir = Path(__file__).parents[2] / "temp" / "sony-camera-remote-sdk"
+download_dir = Path(__file__).parents[2] / "temp" / "sony-camera-remote-sdk-full"
 logging.basicConfig(level=logging.INFO, handlers=[clerk.rich_handler()])
 log = logging.getLogger(__name__)
 
--- packages/sony-camera-remote-sdk/ci-sony-camera-remote-sdk.ps1
+++ packages/sony-camera-remote-sdk/ci-sony-camera-remote-sdk.ps1
@@ -2,22 +2,22 @@ Set-Location $PSScriptRoot
 $ROOT = git rev-parse --show-toplevel
 . $ROOT/scripts/util.ps1
 
-# Remove-Item $ROOT/temp/$name -Recurse -ErrorAction SilentlyContinue
-# New-Item $ROOT/temp/$name -ItemType Directory -ErrorAction SilentlyContinue
-# pixi run -e selenium python download.py
+Remove-Item $ROOT/temp/$name -Recurse -ErrorAction SilentlyContinue
+New-Item $ROOT/temp/$name -ItemType Directory -ErrorAction SilentlyContinue
+pixi run -e selenium python download.py
 
-# $zipfile = (Get-ChildItem "$ROOT/temp/$name/*.zip")[0]
-# $zipfile.BaseName -match "CrSDK_v(\d+)\.(\d+)\.(\d+).+_(\S+)"
-# $major = $Matches[1]
-# $minor = $Matches[2]
-# $patch = $Matches[3]
-# $patch = "$patch".Replace("00", "0")
-# $latest_version = "$major.$minor.$patch"
-# $platform = $Matches[4]
-# foreach ($f in Get-ChildItem "$ROOT/temp/$name/*.zip") {
-#     $f.BaseName -match "CrSDK_v(\d+)\.(\d+)\.(\d+).+_(\S+)"
-#     $platform = $Matches[4]
-#     7z x "$f" "-o$ROOT/temp/$name/unzip/$platform"
-# }
-# update-recipe -version $latest_version
+$zipfile = (Get-ChildItem "$ROOT/temp/$name/*.zip")[0]
+$zipfile.BaseName -match "CrSDK_v(\d+)\.(\d+)\.(\d+).+_(\S+)"
+$major = $Matches[1]
+$minor = $Matches[2]
+$patch = $Matches[3]
+$patch = "$patch".Replace("00", "0")
+$latest_version = "$major.$minor.$patch"
+$platform = $Matches[4]
+foreach ($f in Get-ChildItem "$ROOT/temp/$name/*.zip") {
+    $f.BaseName -match "CrSDK_v(\d+)\.(\d+)\.(\d+).+_(\S+)"
+    $platform = $Matches[4]
+    7z x "$f" "-o$ROOT/temp/$name/unzip/$platform"
+}
+update-recipe -version $latest_version
 build-pkg
