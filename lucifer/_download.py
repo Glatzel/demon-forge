2299b95139de67ef705bfbf18e02318496e43f29
Glatzel <893016099@qq.com>
2025-06-16 13:05:08 +0800

    f

--- lucifer/_download.py
+++ lucifer/_download.py
@@ -35,7 +35,7 @@ def init_driver(download_dir: str | Path, windows_size: tuple[int, int] = (1920,
     return driver
 
 
-def download_waiter(download_dir: str | Path, pattern: str, timeout=100):
+def download_waiter(download_dir: str | Path, pattern: str, timeout=200):
     log.info("Start download.")
 
     download_dir = Path(download_dir)
--- packages/sony-camera-remote-sdk-full/build-sony-camera-remote-sdk-full.ps1
+++ packages/sony-camera-remote-sdk-full/build-sony-camera-remote-sdk-full.ps1
@@ -4,10 +4,3 @@ $ROOT = git rev-parse --show-toplevel
 
 New-Item $env:PREFIX/$name -ItemType Directory
 Copy-Item "$ROOT/temp/$name/unzip/*" "$env:PREFIX/$name" -Recurse
-
-foreach ($arch in Get-ChildItem $env:PREFIX/$name) {
-    Remove-Item $arch/app/*.h
-    Remove-Item $arch/app/*.cpp
-    Remove-Item $arch/*.zip
-    Remove-Item $arch/external/opencv/ -Recurse
-}
