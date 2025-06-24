f1bd3e780c3b15e5c3145db879fdb22d14e8966b
Glatzel <893016099@qq.com>
2025-06-24 16:47:15 +0800

    s

--- packages/sony-camera-remote-sdk/ci-sony-camera-remote-sdk.ps1
+++ packages/sony-camera-remote-sdk/ci-sony-camera-remote-sdk.ps1
@@ -23,11 +23,12 @@ update-recipe -version $latest_version
 
 if ($IsLinux) {
     pixi run rattler-build build
-    # linux arm64
+    Write-Output "::group::linux arm64"
     $is_arm = $true
     sudo apt-get update
     sudo apt-get install -y qemu-user-static g++-aarch64-linux-gnu cmake ninja-build
     pixi run rattler-build build --target-platform linux-aarch64
+    Write-Output "::endgroup::"
 }
 else {
     build-pkg
