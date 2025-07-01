aa4fd4b5351ae60d8a965d75ef21499067c98226
Glatzel <893016099@qq.com>
2025-07-01 09:54:57 +0800

    f

--- packages/passmark-performance-test/ci-passmark-performance-test.ps1
+++ packages/passmark-performance-test/ci-passmark-performance-test.ps1
@@ -6,6 +6,6 @@ aria2c -c -x16 -s16 `
     -d $ROOT/temp/$name `
     https://www.passmark.com/downloads/PerformanceTest_Linux_ARM64.zip `
     -o passmark.zip
-7z e ./temp/passmark.zip PerformanceTest/pt_linux_arm64 -o$ROOT/temp/$name
+7z e $ROOT/temp/passmark.zip PerformanceTest/pt_linux_arm64 -o$ROOT/temp/$name
 
 pixi run rattler-build build --target-platform linux-aarch64
