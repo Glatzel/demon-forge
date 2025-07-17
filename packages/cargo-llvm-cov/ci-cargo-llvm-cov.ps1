fc4a3fea79c3c88c22fe9b82ddfca6242bfee1c1
Glatzel <893016099@qq.com>
2025-07-17 16:11:16 +0800

    f

--- packages/cargo-llvm-cov/ci-cargo-llvm-cov.ps1
+++ packages/cargo-llvm-cov/ci-cargo-llvm-cov.ps1
@@ -5,7 +5,7 @@ $ROOT = git rev-parse --show-toplevel
 if ($env:CI) {
     rustup toolchain install nightly --profile=minimal
 }
-# cargo +nightly install cargo-llvm-cov --locked --root $ROOT/temp/$name --force
+cargo +nightly install cargo-llvm-cov --locked --root $ROOT/temp/$name --force
 if ($IsWindows) {
     $latest_version = & $ROOT/temp/$name/bin/cargo-llvm-cov.exe llvm-cov -V
     $latest_version = "$latest_version".Split(" ")[1]
