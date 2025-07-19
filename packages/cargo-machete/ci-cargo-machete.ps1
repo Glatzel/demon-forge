ad3fc1ff4ab8b4fdeb44890a961e6a2e80080693
Glatzel <893016099@qq.com>
2025-07-19 11:11:25 +0800

    f

---
+++ packages/cargo-machete/ci-cargo-machete.ps1
@@ -0,0 +1,11 @@
+Set-Location $PSScriptRoot
+$ROOT = git rev-parse --show-toplevel
+. $ROOT/scripts/util.ps1
+
+cargo install cargo-machete --root $ROOT/temp/$name --force
+if ($IsWindows) {
+    $latest_version = & $ROOT/temp/$name/bin/cargo-machete.exe bloat -V
+    update-recipe -version $latest_version
+}
+
+build-pkg
