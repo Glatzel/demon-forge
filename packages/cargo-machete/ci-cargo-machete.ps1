1af1bb0504b86b2e45e821621fda8b59d107584f
Glatzel <893016099@qq.com>
2025-07-19 11:12:37 +0800

    f

--- packages/cargo-machete/ci-cargo-machete.ps1
+++ packages/cargo-machete/ci-cargo-machete.ps1
@@ -4,7 +4,7 @@ $ROOT = git rev-parse --show-toplevel
 
 cargo install cargo-machete --root $ROOT/temp/$name --force
 if ($IsWindows) {
-    $latest_version = & $ROOT/temp/$name/bin/cargo-machete.exe bloat -V
+    $latest_version = & $ROOT/temp/$name/bin/cargo-machete.exe --version
     update-recipe -version $latest_version
 }
 
