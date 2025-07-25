fcbfec0b9e26729787ac04dad755588fa2040a96
Glatzel <893016099@qq.com>
2025-07-25 08:45:35 +0800

    f

--- packages/obs-studio/ci-obs-studio.ps1
+++ packages/obs-studio/ci-obs-studio.ps1
@@ -7,7 +7,7 @@ $latest_version = get-latest-version -repo "https://github.com/obsproject/$name"
 
 Remove-Item $ROOT/temp/$name -Recurse -ErrorAction SilentlyContinue
 New-Item  $ROOT/temp/$name -ItemType Directory
-gh release download -R obsproject/$name -p "*-Windows.zip" `
+gh release download -R obsproject/$name -p "*-Windows-*.zip" `
     -O  $ROOT/temp/$name/$name.zip --clobber
     7z x "$ROOT/temp/$name/$name.zip" "-o$ROOT/temp/$name/$name"
 update-recipe -version $latest_version
