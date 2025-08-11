5a0d6d1978538ca152d4924aca0af92adc76e4bd
Glatzel <893016099@qq.com>
2025-08-11 11:28:45 +0800

    feat: remove everything

--- packages/everything/build-everything.ps1
+++
@@ -1,9 +0,0 @@
-Set-Location $PSScriptRoot
-$ROOT = git rev-parse --show-toplevel
-. $ROOT/scripts/util.ps1
-
-New-Item $env:PREFIX/bin/$name -ItemType Directory
-Copy-Item "$ROOT/temp/$name/$name.exe" "$env:PREFIX/bin/$name"
-# shortcut
-New-Item $env:PREFIX/Menu -ItemType Directory
-Copy-Item "$name.json" "$env:PREFIX/Menu"
