99582fce0be166cb27d76349f9581e0e79e0fc73
Glatzel <893016099@qq.com>
2025-08-14 09:46:47 +0800

    f

--- packages/cloudflared/build-cloudflared.ps1
+++ packages/cloudflared/build-cloudflared.ps1
@@ -3,4 +3,9 @@ $ROOT = git rev-parse --show-toplevel
 . $ROOT/scripts/util.ps1
 
 New-Item $env:PREFIX/bin -ItemType Directory
+if($IsWindows){
 Copy-Item "$ROOT/temp/$name/$name.exe" "$env:PREFIX/bin/$name.exe"
+}
+else {
+   Copy-Item "$ROOT/temp/$name/$name" "$env:PREFIX/bin/$name"
+}
