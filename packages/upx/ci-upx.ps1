e0515673d5cb4dbab96776eeab2b8c6f61912d0d
Glatzel <893016099@qq.com>
2025-08-31 18:58:54 +0800

    f

--- packages/upx/ci-upx.ps1
+++ packages/upx/ci-upx.ps1
@@ -7,7 +7,7 @@ $latest_version = "$latest_version".Replace("v", "")
 Remove-Item $ROOT/temp/$name -Recurse -ErrorAction SilentlyContinue
 New-Item  $ROOT/temp/$name -ItemType Directory
 if ($IsWindows) {
-    gh release download -R obsproject/$name -p "*-win64.zip" `
+    gh release download -R $name/$name -p "*-win64.zip" `
         -O  $ROOT/temp/$name/$name.zip --clobber
     7z x "$ROOT/temp/$name/$name.zip" "-o$ROOT/temp/$name/$name"
 }
