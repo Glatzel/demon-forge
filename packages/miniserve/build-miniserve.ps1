716d9450aa90240f08a008ddc666a643876dbce7
Glatzel <893016099@qq.com>
2025-09-13 22:19:29 +0800

    fix: authority (#343)
    Co-authored-by: pre-commit-ci-lite[bot] <117423508+pre-commit-ci-lite[bot]@users.noreply.github.com>

--- packages/miniserve/build-miniserve.ps1
+++ packages/miniserve/build-miniserve.ps1
@@ -4,3 +4,6 @@ $ROOT = git rev-parse --show-toplevel
 
 New-Item $env:PREFIX/bin -ItemType Directory
 Copy-Item "$ROOT/temp/$name/*" "$env:PREFIX/bin/"
+if ($IsLinux) {
+    chmod +rwx "$env:PREFIX/bin/$name"
+}
