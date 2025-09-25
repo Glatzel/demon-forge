266c1a3c68fc9bea8504898ab43b85dcc7b13a57
Glatzel <893016099@qq.com>
2025-09-25 20:41:15 +0800

    feat: dufs (#360)

---
+++ packages/dufs/ci-dufs.ps1
@@ -0,0 +1,12 @@
+Set-Location $PSScriptRoot
+$ROOT = git rev-parse --show-toplevel
+. $ROOT/scripts/util.ps1
+
+cargo install $name --root $ROOT/temp/$name --force
+if ($IsWindows) {
+    $latest_version = & $ROOT/temp/$name/bin/$name.exe $name -V
+    $latest_version ="$latest_version".Split(' ')[1]
+    update-recipe -version $latest_version
+}
+
+build-pkg
