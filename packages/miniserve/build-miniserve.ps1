21aa6b5b7e70e89a9abec7f438a47ef83c92a51d
Glatzel <893016099@qq.com>
2025-09-25 20:32:57 +0800

    feat: remove miniserve (#361)
    Co-authored-by: pre-commit-ci-lite[bot] <117423508+pre-commit-ci-lite[bot]@users.noreply.github.com>

--- packages/miniserve/build-miniserve.ps1
+++
@@ -1,9 +0,0 @@
-Set-Location $PSScriptRoot
-$ROOT = git rev-parse --show-toplevel
-. $ROOT/scripts/util.ps1
-
-New-Item $env:PREFIX/bin -ItemType Directory
-Copy-Item "$ROOT/temp/$name/*" "$env:PREFIX/bin/"
-if ($IsLinux) {
-    chmod +rwx "$env:PREFIX/bin/$name"
-}
