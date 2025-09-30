ca7b8115ae2fca84ce8d1618324e031c30a329f7
Glatzel <893016099@qq.com>
2025-09-30 15:49:56 +0800

    Update ci-hwinfo.ps1

--- packages/hwinfo/ci-hwinfo.ps1
+++ packages/hwinfo/ci-hwinfo.ps1
@@ -7,9 +7,9 @@ Remove-Item $ROOT/temp/$name -Recurse -ErrorAction SilentlyContinue
 New-Item $ROOT/temp/$name -ItemType Directory
 pixi run -e selenium python download.py
 
-$zipfile=(Get-ChildItem "$ROOT/temp/$name/hwi_*.zip")[0]
+$zipfile = (Get-ChildItem "$ROOT/temp/$name/hwi_*.zip")[0]
 7z x "$zipfile" "-o$ROOT/temp/$name/$name"
-$latest_version= (Get-Item $ROOT/temp/$name/$name/HWiNFO64.exe).VersionInfo.ProductVersion.Split("-")[0]
+$latest_version = """" + (Get-Item $ROOT/temp/$name/$name/HWiNFO64.exe).VersionInfo.ProductVersion.Split("-")[0] + """"
 
 update-recipe -version $latest_version
 build-pkg
