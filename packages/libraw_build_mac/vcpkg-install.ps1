2b0e5095f9c7df6d844f7ce60577aaf9a631c8b2
Glatzel <893016099@qq.com>
2025-10-13 08:22:26 +0800

    build: Delete packages/libraw_build_mac directory

--- packages/libraw_build_mac/vcpkg-install.ps1
+++
@@ -1,5 +0,0 @@
-Set-Location $PSScriptRoot
-# install static dependency
-Write-Output "::group::static"
-./vcpkg/vcpkg install --triplet arm64-osx-release --x-install-root ./installed
-Write-Output "::endgroup::"
