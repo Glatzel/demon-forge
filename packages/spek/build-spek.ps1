e5f3d73d3f0faa57f331f7f55ae7a5c3ad687556
Glatzel <893016099@qq.com>
2025-10-14 14:21:35 +0800

    f

--- packages/spek/build-spek.ps1
+++ packages/spek/build-spek.ps1
@@ -3,7 +3,7 @@ $ROOT = git rev-parse --show-toplevel
 . $ROOT/scripts/util.ps1
 
 New-Item $env:PREFIX/bin/$name -ItemType Directory
-Copy-Item "$ROOT/temp/$name/$name/Spek" "$env:PREFIX/bin/$name" -Recurse
+Copy-Item "$ROOT/temp/$name/$name/Spek/*" "$env:PREFIX/bin/$name" -Recurse
 
 # shortcut
 New-Item $env:PREFIX/Menu -ItemType Directory
