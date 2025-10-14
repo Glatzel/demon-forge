569206285c35d42de175c52835020a40c21fb95d
Glatzel <893016099@qq.com>
2025-10-14 14:34:15 +0800

    feat: add spek, audacity (#392)

---
+++ packages/spek/build-spek.ps1
@@ -0,0 +1,10 @@
+Set-Location $PSScriptRoot
+$ROOT = git rev-parse --show-toplevel
+. $ROOT/scripts/util.ps1
+
+New-Item $env:PREFIX/bin/$name -ItemType Directory
+Copy-Item "$ROOT/temp/$name/$name/Spek/*" "$env:PREFIX/bin/$name" -Recurse
+
+# shortcut
+New-Item $env:PREFIX/Menu -ItemType Directory
+Copy-Item "$name.json" "$env:PREFIX/Menu"
---
+++ packages/spek/ci-spek.ps1
@@ -0,0 +1,15 @@
+Set-Location $PSScriptRoot
+$ROOT = git rev-parse --show-toplevel
+. $ROOT/scripts/util.ps1
+$name = get-name
+
+$latest_version = get-latest-version -repo "https://github.com/alexkay/$name"
+$latest_version = "$latest_version".Replace("v", "")
+
+Remove-Item $ROOT/temp/$name -Recurse -ErrorAction SilentlyContinue
+New-Item  $ROOT/temp/$name -ItemType Directory
+gh release download -R alexkay/$name -p "$name-*.zip" `
+    -O  $ROOT/temp/$name/$name.zip --clobber
+7z x "$ROOT/temp/$name/$name.zip" "-o$ROOT/temp/$name/$name"
+update-recipe -version $latest_version
+build-pkg
