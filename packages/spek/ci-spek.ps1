0ecb2c980bd50f7ddbcd98644b43ade809f609d3
Glatzel <893016099@qq.com>
2025-10-14 14:05:07 +0800

    feat: add spek

---
+++ packages/spek/ci-spek.ps1
@@ -0,0 +1,14 @@
+Set-Location $PSScriptRoot
+$ROOT = git rev-parse --show-toplevel
+. $ROOT/scripts/util.ps1
+$name = get-name
+
+$latest_version = get-latest-version -repo "https://github.com/obsproject/$name"
+
+Remove-Item $ROOT/temp/$name -Recurse -ErrorAction SilentlyContinue
+New-Item  $ROOT/temp/$name -ItemType Directory
+gh release download -R obsproject/$name -p "*-Windows-x64.zip" `
+    -O  $ROOT/temp/$name/$name.zip --clobber
+7z x "$ROOT/temp/$name/$name.zip" "-o$ROOT/temp/$name/$name"
+update-recipe -version $latest_version
+build-pkg
