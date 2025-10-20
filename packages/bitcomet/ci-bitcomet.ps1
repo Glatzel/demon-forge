94061b1a90e84bcdb05f2dcd7ff3886faff2cdf7
Glatzel <893016099@qq.com>
2025-10-20 10:08:38 +0800

    feat: add bitcomet (#411)

---
+++ packages/bitcomet/ci-bitcomet.ps1
@@ -0,0 +1,27 @@
+Set-Location $PSScriptRoot
+$ROOT = git rev-parse --show-toplevel
+. $ROOT/scripts/util.ps1
+Remove-Item $ROOT/temp/$name -Recurse -ErrorAction SilentlyContinue
+$xml = [xml](Invoke-WebRequest -UseBasicParsing -Uri "https://www.kisssub.org/rss-bitcomet.xml").Content
+$latest = $xml.rss.channel.item[0]
+$title = $latest.title.'#cdata-section'
+$guid = $latest.guid.'#text'
+
+if ($title -match 'build (\d+\.\d+\.\d+\.\d+)') {
+    $latest_version = $matches[1]
+}
+
+if ($guid -match 'show-([0-9a-f]+)\.html$') {
+    $guidHash = $matches[1]
+    Write-Host $guidHash
+    $magnet = 'magnet:?xt=urn:btih:' + $guidHash
+}
+$trackerList = (Invoke-WebRequest -UseBasicParsing -Uri 'http://github.itzmx.com/1265578519/OpenTracker/master/tracker.txt').Content
+$trackers = ($trackerList -split '\s+') -join ','
+aria2c --seed-time=0 --bt-tracker="$trackers" --dir "$ROOT/temp/$name" "$magnet"
+$zipfile = (Get-ChildItem "$ROOT/temp/$name/*.7z")[0]
+7z x "$zipfile" "-o$ROOT/temp/$name/$name"
+$folder = (Get-ChildItem "$ROOT/temp/$name/$name/*" -Directory)[0]
+Rename-Item $folder bitcomet
+update-recipe -version $latest_version
+build-pkg
