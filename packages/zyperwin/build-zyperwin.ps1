ab072437420a55ed28d22c44f33ea5fd5ddf9e9e
Glatzel <893016099@qq.com>
2025-11-02 16:32:35 +0800

    fix: zyperwin (#433)

--- packages/zyperwin/build-zyperwin.ps1
+++ packages/zyperwin/build-zyperwin.ps1
@@ -2,8 +2,8 @@ Set-Location $PSScriptRoot
 $ROOT = git rev-parse --show-toplevel
 . $ROOT/scripts/util.ps1
 
-New-Item $env:PREFIX/bin/$name -ItemType Directory
-Copy-Item "$ROOT/temp/$name/$name/Release/*" "$env:PREFIX/bin/$name" -Recurse
+New-Item $env:PREFIX/bin -ItemType Directory
+Copy-Item "$ROOT/temp/$name/$name/Release/*" "$env:PREFIX/bin" -Recurse
 
 # shortcut
 New-Item $env:PREFIX/Menu -ItemType Directory
