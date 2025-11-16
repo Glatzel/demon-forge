9c3eb7ce017a64d952c79f3dc4d3a8ee66141108
Glatzel <chenzelin9794@gmail.com>
2025-11-16 14:17:29 +0800

    f

--- packages/ttyd/build-ttyd.ps1
+++ packages/ttyd/build-ttyd.ps1
@@ -3,4 +3,10 @@ $ROOT = git rev-parse --show-toplevel
 . $ROOT/scripts/util.ps1
 
 New-Item $env:PREFIX/bin -ItemType Directory
-Copy-Item "$ROOT/temp/$name/$name.exe" "$env:PREFIX/bin/$name.exe"
+if ($IsWindows) {
+    Copy-Item "$ROOT/temp/$name/$name.exe" "$env:PREFIX/bin/$name.exe"
+}
+if ($IsLinux) {
+    Copy-Item "$ROOT/temp/$name/$name.exe" "$env:PREFIX/bin/$name"
+}
+
