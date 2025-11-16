cc6751b3099a685be8e28d28a427a132a0a59890
Glatzel <chenzelin9794@gmail.com>
2025-11-16 17:54:41 +0800

    f

--- packages/ttyd/build-ttyd.ps1
+++ packages/ttyd/build-ttyd.ps1
@@ -7,7 +7,6 @@ if ($IsWindows) {
     Copy-Item "$ROOT/temp/$name/$name.exe" "$env:PREFIX/bin/$name.exe"
 }
 if ($IsLinux) {
-    Test-Path $ROOT/temp/$name/$name
-    Copy-Item "$ROOT/temp/$name/$name" "$env:PREFIX/bin/$name"
+    Copy-Item "$ROOT/temp/$name/*" "$env:PREFIX/bin/$name"
     chmod +rwx "$env:PREFIX/bin/$name"
 }
