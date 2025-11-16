17718e4040b379de545e1c472c30399bb0e9f05b
Glatzel <chenzelin9794@gmail.com>
2025-11-16 14:31:34 +0800

    f

--- packages/ttyd/ci-ttyd.ps1
+++ packages/ttyd/ci-ttyd.ps1
@@ -8,11 +8,11 @@ $latest_version = "$latest_version".Replace("$name-", "")
 Remove-Item $ROOT/temp/$name -Recurse -ErrorAction SilentlyContinue
 New-Item  $ROOT/temp/$name -ItemType Directory
 if ($IsWindows) {
-    gh release download -R "tsl0922/$name" -p "*.exe" `
+    gh release download -R "Glatzel/$name" -p "*.exe" `
         -O  $ROOT/temp/$name/$name.exe --clobber
 }
 if ($IsLinux) {
-    gh release download -R "tsl0922/$name" -p "*.x86_64" `
+    gh release download -R "Glatzel/$name" -p "*.x86_64" `
         -O  $ROOT/temp/$name/$name --clobber
 }
 update-recipe -version $latest_version
