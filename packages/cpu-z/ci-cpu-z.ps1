0faa68cb302fbe1f019376246091616d79ccdb1e
Glatzel <chenzelin9794@gmail.com>
2025-11-17 22:46:34 +0800

    f

--- packages/cpu-z/ci-cpu-z.ps1
+++ packages/cpu-z/ci-cpu-z.ps1
@@ -2,6 +2,8 @@ Set-Location $PSScriptRoot
 $ROOT = git rev-parse --show-toplevel
 . $ROOT/scripts/util.ps1
 
+$latest_version = get-version-url -url "https://www.cpuid.com/softwares/cpu-z.html" -pattern 'cpu-z_(\d+\.\d+)-en.zip'
+update-recipe -version $latest_version
 
 Remove-Item $ROOT/temp/$name -Recurse -ErrorAction SilentlyContinue
 New-Item $ROOT/temp/$name -ItemType Directory
@@ -9,8 +11,4 @@ pixi run -e selenium python download.py
 
 $zipfile = (Get-ChildItem "$ROOT/temp/$name/*.zip")[0]
 7z x "$zipfile" "-o$ROOT/temp/$name/$name"
-$zipfile.BaseName -match "cpu-z_(\S+)-en"
-$latest_version=$Matches[1]
-
-update-recipe -version $latest_version
 build-pkg
