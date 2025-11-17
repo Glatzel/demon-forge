9a285a6e74097b78ca57b6f6c1e900fe80abc365
Glatzel <chenzelin9794@gmail.com>
2025-11-17 22:18:45 +0800

    f

--- packages/proj_build/vcpkg-setup.ps1
+++ packages/proj_build/vcpkg-setup.ps1
@@ -1,12 +1,13 @@
 Set-Location $PSScriptRoot
-
 Remove-Item vcpkg -Recurse -Force -ErrorAction SilentlyContinue
 git clone https://github.com/microsoft/vcpkg.git
 if ($IsWindows) {
     ./vcpkg/bootstrap-vcpkg.bat
+    Set-Location $PSScriptRoot
+    ./vcpkg/vcpkg.exe x-update-baseline
 }
-if ($IsLinux -or $IsMacOS) {
+else {
     ./vcpkg/bootstrap-vcpkg.sh
+    Set-Location $PSScriptRoot
+    ./vcpkg/vcpkg x-update-baseline
 }
-Set-Location $PSScriptRoot
-./vcpkg/vcpkg.exe x-update-baseline
