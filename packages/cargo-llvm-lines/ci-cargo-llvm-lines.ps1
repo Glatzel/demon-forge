1774b0798ddcaf737ac3bc94cdab785f836e1b15
Glatzel <chenzelin9794@gmail.com>
2025-11-17 21:07:57 +0800

    f

--- packages/cargo-llvm-lines/ci-cargo-llvm-lines.ps1
+++ packages/cargo-llvm-lines/ci-cargo-llvm-lines.ps1
@@ -2,7 +2,7 @@ Set-Location $PSScriptRoot
 $ROOT = git rev-parse --show-toplevel
 . $ROOT/scripts/util.ps1
 
-$latest_version = curl -s https://crates.io/api/v1/crates/$name | jq '.crate.max_version'
+$latest_version = curl -s https://crates.io/api/v1/crates/$name | jq -r '.crate.max_version'
 update-recipe -version $latest_version
 
 cargo install $name --root $ROOT/temp/$name
