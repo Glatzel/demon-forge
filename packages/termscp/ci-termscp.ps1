faf1af5342fcb9ddb829ff3f29e75ae2f15bfa84
Glatzel <chenzelin9794@gmail.com>
2025-11-17 21:20:06 +0800

    f

--- packages/termscp/ci-termscp.ps1
+++ packages/termscp/ci-termscp.ps1
@@ -2,7 +2,7 @@ Set-Location $PSScriptRoot
 $ROOT = git rev-parse --show-toplevel
 . $ROOT/scripts/util.ps1
 
-$latest_version = get-version-crateio $name
+$latest_version = get-version-crateio -name $name
 update-recipe -version $latest_version
 
 Remove-Item $ROOT/temp/$name -Recurse -ErrorAction SilentlyContinue
