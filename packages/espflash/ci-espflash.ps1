6d428a0f29a9c127abfbe729f638321cd17b2f01
Glatzel <chenzelin9794@gmail.com>
2025-11-17 21:27:43 +0800

    f

--- packages/espflash/ci-espflash.ps1
+++ packages/espflash/ci-espflash.ps1
@@ -6,9 +6,5 @@ $latest_version = get-version-crateio -name $name
 update-recipe -version $latest_version
 
 cargo install $name --root $ROOT/temp/$name --force
-if ($IsWindows) {
-    $latest_version = & $ROOT/temp/$name/bin/$name.exe -V
-    $latest_version ="$latest_version".Split(' ')[1]
-}
 
 build-pkg
