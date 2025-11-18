88ae59c82b3fb590d1712e8a968210ca04791b2f
Glatzel <893016099@qq.com>
2025-11-18 10:50:56 +0800

    ci: optimize (#466)
    Co-authored-by: pre-commit-ci-lite[bot] <117423508+pre-commit-ci-lite[bot]@users.noreply.github.com>

--- packages/filezilla/ci-filezilla.ps1
+++ packages/filezilla/ci-filezilla.ps1
@@ -2,15 +2,15 @@ Set-Location $PSScriptRoot
 $ROOT = git rev-parse --show-toplevel
 . $ROOT/scripts/util.ps1
 
+$latest_version = get-version-url -url "https://filezilla-project.org/newsfeed.php" -pattern 'FileZilla Client (\d+\.\d+\.\d+) released'
+update-recipe -version $latest_version
 
 Remove-Item $ROOT/temp/$name -Recurse -ErrorAction SilentlyContinue
 New-Item $ROOT/temp/$name -ItemType Directory
 pixi run -e selenium python download.py
 
-$zipfile=(Get-ChildItem "$ROOT/temp/$name/FileZilla*win64.zip")[0]
+$zipfile = (Get-ChildItem "$ROOT/temp/$name/FileZilla*win64.zip")[0]
 7z x "$zipfile" "-o$ROOT/temp/$name"
-$zipfile.BaseName -match "FileZilla_(\S+)_win64"
-$latest_version=$Matches[1]
 
 update-recipe -version $latest_version
 build-pkg
