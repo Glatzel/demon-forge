a21358bd2301c7db1a512035bae11f4ea93d16e2
Glatzel <chenzelin9794@gmail.com>
2025-11-18 10:48:22 +0800

    f

--- packages/hwinfo/ci-hwinfo.ps1
+++ packages/hwinfo/ci-hwinfo.ps1
@@ -2,6 +2,8 @@ Set-Location $PSScriptRoot
 $ROOT = git rev-parse --show-toplevel
 . $ROOT/scripts/util.ps1
 
+$latest_version = get-version-url -url "https://www.hwinfo.com/news.xml/" -pattern 'HWiNFO v(\d+\.\d+) released'
+update-recipe -version $latest_version
 
 Remove-Item $ROOT/temp/$name -Recurse -ErrorAction SilentlyContinue
 New-Item $ROOT/temp/$name -ItemType Directory
@@ -9,7 +11,6 @@ pixi run -e selenium python download.py
 
 $zipfile = (Get-ChildItem "$ROOT/temp/$name/hwi_*.zip")[0]
 7z x "$zipfile" "-o$ROOT/temp/$name/$name"
-$latest_version = (Get-Item $ROOT/temp/$name/$name/HWiNFO64.exe).VersionInfo.ProductVersion.Split("-")[0]
 
 update-recipe -version $latest_version
 build-pkg
