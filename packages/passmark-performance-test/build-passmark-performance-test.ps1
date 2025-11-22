b96a9acf5587e891b3af0df60d25087e3d929ae2
Glatzel <chenzelin9794@gmail.com>
2025-11-22 09:33:45 +0800

    f

--- packages/passmark-performance-test/build-passmark-performance-test.ps1
+++
@@ -1,6 +0,0 @@
-Set-Location $PSScriptRoot
-$ROOT = git rev-parse --show-toplevel
-. $ROOT/scripts/util.ps1
-
-New-Item $env:PREFIX/bin -ItemType Directory
-Copy-Item "$ROOT/temp/$name/pt_linux_arm64" "$env:PREFIX/bin/"
