5e0121153cda53efdaa51f2c16191c959697d610
Glatzel <893016099@qq.com>
2025-11-23 21:06:07 +0800

    Update ci-ttyd.ps1

--- packages/ttyd/ci-ttyd.ps1
+++ packages/ttyd/ci-ttyd.ps1
@@ -1,7 +1,7 @@
 Set-Location $PSScriptRoot
 $ROOT = git rev-parse --show-toplevel
 . $ROOT/scripts/util.ps1
-$latest_version = get-version-github -repo "Glatzel/$name"
+$latest_version = get-version-github -repo "tsl0922/$name"
 update-recipe -version $latest_version
 
 Set-Location $ROOT/temp/$name
