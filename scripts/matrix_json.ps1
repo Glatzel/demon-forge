6d99821ee8633ff5451d11274a40d696b36fe040
Glatzel <chenzelin9794@gmail.com>
2025-11-23 16:33:35 +0800

    f

--- scripts/matrix_json.ps1
+++ scripts/matrix_json.ps1
@@ -14,8 +14,8 @@ foreach ($row in $csvData) {
 }
 
 $matrix = $matrix |
-    ConvertTo-Json -Depth 10 -Compress |
-    jq '{include: .}'
+ConvertTo-Json -Depth 10 -Compress |
+jq '{include: .}'
 
 # Clean CHANGED_KEYS
 $env:CHANGED_KEYS = "${env:CHANGED_KEYS}".Replace("\", "")
@@ -23,33 +23,27 @@ $env:CHANGED_KEYS = "${env:CHANGED_KEYS}".Replace("\", "")
 switch ($env:GITHUB_EVENT_NAME) {
     "push" {
         $matrix = $matrix |
-            jq -c --argjson pkgs "${env:CHANGED_KEYS}" '
+        jq -c --argjson pkgs "${env:CHANGED_KEYS}" @"
                 {include: .include | map(select(.pkg as $p | $pkgs | index($p)))}
-            '
+            "
     }
     "pull_request" {
         $matrix = $matrix |
-            jq -c --argjson pkgs "${env:CHANGED_KEYS}" '
+        jq -c --argjson pkgs "${env:CHANGED_KEYS}" @"
                 {include: .include | map(select(.pkg as $p | $pkgs | index($p)))}
-            '
+            "
     }
     default {
-        $rule = '
-            {include:
-                .include
-                | group_by(.pkg)
-                | map(
-                    sort_by(
-                        if .machine == "ubuntu-latest" then 0
-                        elif .machine == "macos-latest" then 1
-                        elif .machine == "windows-latest" then 2
-                        elif .machine == "ubuntu-24.04-arm" then 3
-                        else 4
-                        end
-                    ) | .[0]
-                )
-            }
-        '
+        $rule = @"
+{include: .include | group_by(.pkg) | map(sort_by(
+    if .machine == "ubuntu-latest" then 0
+    elif .machine == "macos-latest" then 1
+    elif .machine == "windows-latest" then 2
+    elif .machine == "ubuntu-24.04-arm" then 3
+    else 4
+    end
+) | .[0])}
+"@
         $matrix = $matrix | jq -c "$rule"
     }
 }
