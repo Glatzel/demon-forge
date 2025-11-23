6fa35c364f1e937fa67656e9e22ebd5745257136
Glatzel <893016099@qq.com>
2025-11-23 22:31:13 +0800

    Update ci-filezilla.ps1

--- packages/filezilla/ci-filezilla.ps1
+++ packages/filezilla/ci-filezilla.ps1
@@ -8,6 +8,6 @@ pixi run -e selenium python download.py
 
 $zipfile = (Get-ChildItem "$ROOT/temp/$name/FileZilla*win64.zip")[0]
 7z x "$zipfile" "-o$ROOT/temp/$name"
-
+ls $ROOT/temp/$name
 update-recipe -version $latest_version
 build-pkg
