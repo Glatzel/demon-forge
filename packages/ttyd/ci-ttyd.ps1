7e0dd748c272d8a0676ba7650d71d808cb29c400
Glatzel <893016099@qq.com>
2025-11-23 20:33:09 +0800

    Update ci-ttyd.ps1

--- packages/ttyd/ci-ttyd.ps1
+++ packages/ttyd/ci-ttyd.ps1
@@ -10,11 +10,13 @@ Set-Location $name
 git checkout tags/"$latest_version" -b "$latest_version-branch"
 copy-item $PSScriptRoot/build/* $ROOT/temp/$name/$name -recurse
 & ./download-font.ps1
+Set-Location ./html
 pixi run corepack enable
 pixi run corepack prepare yarn@stable --activate
 pixi run yarn install
 pixi run yarn run check
 pixi run yarn run build
+Set-Location ..
 sudo apt-get update
 sudo apt-get install -y autoconf automake build-essential cmake curl file libtool
 foreach($t in "win32","x86_64","aarch64")
