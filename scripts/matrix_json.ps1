7a48d8d03a5c9aca3a3a7ba994ddbe415ae3b134
Glatzel <chenzelin9794@gmail.com>
2025-11-23 16:09:11 +0800

    f

--- scripts/matrix_json.ps1
+++ scripts/matrix_json.ps1
@@ -16,8 +16,8 @@ foreach ($row in $csvData) {
 $matrix = $matrix | ConvertTo-Json -Depth 10 -Compress | jq '{include: .}'
 $env:CHANGED_KEYS = "${env:CHANGED_KEYS}".Replace("\", "")
 switch ($env:GITHUB_EVENT_NAME) {
-    "push" { $matrix = $matrix | jq -c --argjson pkgs "${env:CHANGED_KEYS}" '.include | map(select(.pkg as $p | $pkgs | index($p)))' }
-    "pull_request" { $matrix = $matrix | jq -c --argjson pkgs "${env:CHANGED_KEYS}" '.include | map(select(.pkg as $p | $pkgs | index($p)))' }
+    "push" { $matrix = $matrix | jq -c --argjson pkgs "${env:CHANGED_KEYS}" '{include:.include | map(select(.pkg as $p | $pkgs | index($p)))}' }
+    "pull_request" { $matrix = $matrix | jq -c --argjson pkgs "${env:CHANGED_KEYS}" '{include:.include | map(select(.pkg as $p | $pkgs | index($p)))}' }
     "schedule" { $matrix = $matrix | jq -c . }
     "workflow_dispatch" { $matrix = $matrix | jq -c . }
     default { $matrix = $matrix | jq -c . }
