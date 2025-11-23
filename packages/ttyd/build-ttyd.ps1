b6fbece866ce818c6fc73656b2306b4c28406408
Glatzel <893016099@qq.com>
2025-11-23 20:10:17 +0800

    Update build-ttyd.ps1

--- packages/ttyd/build-ttyd.ps1
+++ packages/ttyd/build-ttyd.ps1
@@ -3,10 +3,8 @@ $ROOT = git rev-parse --show-toplevel
 . $ROOT/scripts/util.ps1
 
 New-Item $env:PREFIX/bin -ItemType Directory
-if ($IsWindows) {
-    Copy-Item "$ROOT/temp/$name/$name.exe" "$env:PREFIX/bin/$name.exe"
-}
-if ($IsLinux) {
-    Copy-Item "$ROOT/temp/$name/$name" "$env:PREFIX/bin/$name"
-    chmod +rwx "$env:PREFIX/bin/$name"
+switch($env:TARGET_PLATFORM){
+"win-64"{copy-item $ROOT/temp/$name/$name/build/$name.win32.exe $env:PREFIX/bin/$name.exe}
+"linux-64"{copy-item $ROOT/temp/$name/$name/build/$name.x86_64 $env:PREFIX/bin/$name}
+"linux-aarch64"{copy-item $ROOT/temp/$name/$name/build/$name.aarch64 $env:PREFIX/bin/$name}
 }
