38416077417b1876d4143a6c577bd14e1dccdde9
Glatzel <893016099@qq.com>
2025-11-23 16:37:56 +0800

    ci: fix (#480)

--- scripts/matrix_json.ps1
+++ scripts/matrix_json.ps1
@@ -13,32 +13,38 @@ foreach ($row in $csvData) {
     }
 }
 
-$matrix = $matrix | ConvertTo-Json -Depth 10 -Compress | jq '{include: .}'
+$matrix = $matrix |
+ConvertTo-Json -Depth 10 -Compress |
+jq '{include: .}'
+
+# Clean CHANGED_KEYS
 $env:CHANGED_KEYS = "${env:CHANGED_KEYS}".Replace("\", "")
+
 switch ($env:GITHUB_EVENT_NAME) {
-    "push" { $matrix = $matrix | jq -c --argjson pkgs "${env:CHANGED_KEYS}" '{include:.include | map(select(.pkg as $p | $pkgs | index($p)))}' }
-    "pull_request" { $matrix = $matrix | jq -c --argjson pkgs "${env:CHANGED_KEYS}" '{include:.include | map(select(.pkg as $p | $pkgs | index($p)))}' }
-    # "schedule" {  }
-    # "workflow_dispatch" {  }
-    default {
-        $rule = '{include:
-  .include
-  | group_by(.pkg)
-  | map(
-      sort_by(
-        if .machine == "ubuntu-latest" then 0
-        elif .machine == "macos-latest" then 1
-        elif .machine == "windows-latest" then 2
-        elif .machine == "ubuntu-24.04-arm" then 3
-        else 4 end
-      ) | .[0]
-    )
+    "push" {
+        $matrix = $matrix | jq -c --argjson pkgs "${env:CHANGED_KEYS}" '{include: .include | map(select(.pkg as $p | $pkgs | index($p)))}'
     }
-'
-        $matrix = $matrix | jq -c $rule
+    "pull_request" {
+        $matrix = $matrix | jq -c --argjson pkgs "${env:CHANGED_KEYS}" '{include: .include | map(select(.pkg as $p | $pkgs | index($p)))}'
+    }
+    default {
+        $rule = @"
+{include: .include | group_by(.pkg) | map(sort_by(
+    if .machine == "ubuntu-latest" then 0
+    elif .machine == "macos-latest" then 1
+    elif .machine == "windows-latest" then 2
+    elif .machine == "ubuntu-24.04-arm" then 3
+    else 4
+    end
+) | .[0])}
+"@
+        $matrix = $matrix | jq -c "$rule"
     }
 }
-"matrix=$matrix">> $env:GITHUB_OUTPUT
+
+# Output matrix to GitHub Actions
+"matrix=$matrix" >> $env:GITHUB_OUTPUT
+
 Write-Output "::group::json"
 $matrix | jq .
 Write-Output "::endgroup::"
