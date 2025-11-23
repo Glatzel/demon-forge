de6855c0d2f75388a3b7ccb23928b6b0bb82f0d9
Glatzel <893016099@qq.com>
2025-11-23 14:55:34 +0800

    Update packages_yaml.ps1

--- scripts/packages_yaml.ps1
+++ scripts/packages_yaml.ps1
@@ -5,20 +5,12 @@ $yamlFile = "$PSScriptRoot/../packages.yaml"
 
 # Read CSV
 $csvData = Import-Csv $csvFile
-$csvData | Format-Table
-# Generate all YAML lines at once
-$yamlLines = $csvData | ForEach-Object {
-    @(
-        "$($_.pkg):"
-        "  - ./packages/$($_.pkg)/**"
-    )
-} | Select-Object -ExpandProperty *
-
-# Write all lines to file at once
-$yamlLines | Set-Content $yamlFile
-Write-Host "Generated YAML file: $yamlFile"
+ForEach ($Row in $csvData) {
+    $Row.pkg>>$yamlFile
+}
 
 # Print the exact contents of the YAML file
 Write-Output "::group::yaml"
 Get-Content $yamlFile | ForEach-Object { Write-Host $_ }
 Write-Output "::endgroup::"
+l
