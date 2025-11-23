0a59d03df9bc014f4968fff09e49765083060898
Glatzel <893016099@qq.com>
2025-11-23 18:12:50 +0800

    更新 build-openimageio.ps1

--- packages/openimageio/build-openimageio.ps1
+++ packages/openimageio/build-openimageio.ps1
@@ -1,2 +1,4 @@
+$ROOT = git rev-parse --show-toplevel
+. $ROOT/scripts/util.ps1
 New-Item $env:PREFIX/bin/openimageio -ItemType Directory
-Copy-Item "$ROOT/temp/$name/dist/*" "$env:PREFIX/bin/openimageio"
+Copy-Item "$ROOT/temp/$name/dist/*" "$env:PREFIX/bin/$name"
