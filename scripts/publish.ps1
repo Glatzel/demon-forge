f5387482ae72f8277666f42c9f2db8b601eca9a9
Glatzel <893016099@qq.com>
2025-11-24 08:40:47 +0800

    build: Delete scripts/publish.ps1

--- scripts/publish.ps1
+++
@@ -1,9 +0,0 @@
-param($pkg)
-Set-Location $PSScriptRoot
-Set-Location ..
-
-foreach ($pkg_file in Get-ChildItem "./packages/$pkg/output/*/*.conda" -Recurse -ErrorAction Continue) {
-    Write-Output "::group:: upload $pkg"
-    pixi run rattler-build upload prefix -s -c glatzel $pkg_file
-    Write-Output "::endgroup::"
-}
