83fb09dd1bd06d97a27e33c9004fcb42aa1c8dba
Glatzel <893016099@qq.com>
2025-11-24 12:09:25 +0800

    feat: add easytier (#499)

---
+++ packages/easytier/build-easytier.ps1
@@ -0,0 +1,6 @@
+Set-Location $PSScriptRoot
+$ROOT = git rev-parse --show-toplevel
+. $ROOT/scripts/util.ps1
+
+New-Item $env:PREFIX/bin -ItemType Directory
+Copy-Item "$ROOT/temp/$name/$name/$name*/*" "$env:PREFIX/bin/" -Recurse
