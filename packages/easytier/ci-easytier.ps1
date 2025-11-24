9422d61a2128f7917330a2d9bcb5d0111b3baf59
Glatzel <chenzelin9794@gmail.com>
2025-11-24 11:51:30 +0800

    f

--- packages/easytier/ci-easytier.ps1
+++ packages/easytier/ci-easytier.ps1
@@ -13,7 +13,11 @@ if ($IsLinux -and ($arch -eq "X64")) {
         -O  $ROOT/temp/$name/$name.zip --clobber
 }
 if ($IsLinux -and ($arch -eq "Arm64")) {
-    gh release download -R "EasyTier/EasyTier" -p "$name-windows-aarch64-*.zip" `
+    gh release download -R "EasyTier/EasyTier" -p "$name-linux-aarch64-*.zip" `
+        -O  $ROOT/temp/$name/$name.zip --clobber
+}
+if ($IsMacOS) {
+    gh release download -R "EasyTier/EasyTier" -p "$name-macos-aarch64-*.zip" `
         -O  $ROOT/temp/$name/$name.zip --clobber
 }
 7z x "$ROOT/temp/$name/$name.zip" "-o$ROOT/temp/$name/$name"
