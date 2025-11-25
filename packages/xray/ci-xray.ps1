4411563cd06bce0b4f09dbdbfe36892807e24aec
Glatzel <chenzelin9794@gmail.com>
2025-11-25 11:50:08 +0800

    f

--- packages/xray/ci-xray.ps1
+++ packages/xray/ci-xray.ps1
@@ -1,23 +1,23 @@
 Set-Location $PSScriptRoot
 $ROOT = git rev-parse --show-toplevel
 . $ROOT/scripts/util.ps1
-$latest_version = get-version-github -repo "EasyTier/EasyTier"
+$latest_version = get-version-github -repo "XTLS/Xray-core"
 update-recipe -version $latest_version
 
 if ($IsWindows) {
-    gh release download -R "EasyTier/EasyTier" -p "Xray-windows-64.zip" `
+    gh release download -R "XTLS/Xray-core" -p "Xray-windows-64.zip" `
         -O  $ROOT/temp/$name/$name.zip --clobber
 }
 if ($IsLinux -and ($arch -eq "X64")) {
-    gh release download -R "EasyTier/EasyTier" -p "Xray-linux-64.zip" `
+    gh release download -R "XTLS/Xray-core" -p "Xray-linux-64.zip" `
         -O  $ROOT/temp/$name/$name.zip --clobber
 }
 if ($IsLinux -and ($arch -eq "Arm64")) {
-    gh release download -R "EasyTier/EasyTier" -p "Xray-linux-arm64-v8a.zip" `
+    gh release download -R "XTLS/Xray-core" -p "Xray-linux-arm64-v8a.zip" `
         -O  $ROOT/temp/$name/$name.zip --clobber
 }
 if ($IsMacOS) {
-    gh release download -R "EasyTier/EasyTier" -p "Xray-macos-arm64-v8a.zip" `
+    gh release download -R "XTLS/Xray-core" -p "Xray-macos-arm64-v8a.zip" `
         -O  $ROOT/temp/$name/$name.zip --clobber
 }
 7z x "$ROOT/temp/$name/$name.zip" "-o$ROOT/temp/$name/$name"
