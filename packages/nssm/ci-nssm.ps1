afe8aacbf43273d951c0ed8b81f14f420a180bef
Glatzel <893016099@qq.com>
2025-11-26 21:57:39 +0800

    Update NSSM download URL to use latest version

--- packages/nssm/ci-nssm.ps1
+++ packages/nssm/ci-nssm.ps1
@@ -1,11 +1,11 @@
 Set-Location $PSScriptRoot
 $ROOT = git rev-parse --show-toplevel
 . $ROOT/scripts/util.ps1
+$latest_version = get-version-url -url "https://nssm.cc/download" -pattern 'nssm (\d+\.\d+)'
+update-recipe -version $latest_version
 
-Remove-Item $ROOT/temp/$name -Recurse -ErrorAction SilentlyContinue
-New-Item $ROOT/temp/$name -ItemType Directory
 aria2c -c -x16 -s16 -d "$ROOT/temp/$name/" `
-    "https://nssm.cc/release/nssm-2.24.zip" `
+    "https://nssm.cc/release/nssm-$latest_version.zip" `
     -o "$name.zip"
 
 7z x "$ROOT/temp/$name/$name.zip" "-o$ROOT/temp/$name/$name"
