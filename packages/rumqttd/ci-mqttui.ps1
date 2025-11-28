5de0d71d0f3fe26d6d353d0d3efb1b4d0e554160
Glatzel <chenzelin9794@gmail.com>
2025-11-28 09:12:39 +0800

    分

--- packages/rumqttd/ci-mqttui.ps1
+++
@@ -1,9 +0,0 @@
-Set-Location $PSScriptRoot
-$ROOT = git rev-parse --show-toplevel
-. $ROOT/scripts/util.ps1
-$latest_version = get-version-github "EdJoPaTo/$name"
-update-recipe -version $latest_version
-
-cargo install --git "https://github.com/EdJoPaTo/mqttui.git" --tag "v$latest_version" --root $ROOT/temp/$name --force
-
-build-pkg
