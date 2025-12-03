180871fb385d861a75c66dfd061e7734aaf5bf70
Glatzel <893016099@qq.com>
2025-12-03 21:34:09 +0800

    Update build-xray.ps1

--- packages/xray/build-xray.ps1
+++ packages/xray/build-xray.ps1
@@ -4,3 +4,9 @@ $ROOT = git rev-parse --show-toplevel
 
 New-Item $env:PREFIX/bin/$name -ItemType Directory
 Copy-Item "$ROOT/temp/$name/$name/*" "$env:PREFIX/bin/$name" -Recurse
+if($IsWindows){
+    Copy-Item $PSScriptRoot/update-geofile.ps1 $env:PREFIX/bin/$name/
+}
+else{
+    Copy-Item $PSScriptRoot/update-geofile.sh $env:PREFIX/bin/$name/
+}
