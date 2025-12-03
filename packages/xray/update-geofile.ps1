e713ebf50fdb03623b9ce4500c0dacac64924e64
Glatzel <893016099@qq.com>
2025-12-03 21:36:33 +0800

    Update update-geofile.ps1

--- packages/xray/update-geofile.ps1
+++ packages/xray/update-geofile.ps1
@@ -1,3 +1,4 @@
+set-location $PSScriptRoot
 gh release download -R "Loyalsoldier/v2ray-rules-dat" -p "geoip.dat" `
     -O  $PSScriptRoot/geoip.dat --clobber
 gh release download -R "Loyalsoldier/v2ray-rules-dat" -p "geosite.dat" `
