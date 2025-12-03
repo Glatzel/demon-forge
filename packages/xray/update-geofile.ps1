513d80758ad0a7a5dde59c34501d735590f6ef1a
Glatzel <893016099@qq.com>
2025-12-03 21:25:04 +0800

    创建 update-geofile.ps1

---
+++ packages/xray/update-geofile.ps1
@@ -0,0 +1,4 @@
+gh release download -R "Loyalsoldier/v2ray-rules-dat" -p "geoip.dat" `
+    -O  $PSScriptRoot/geoip.dat --clobber
+gh release download -R "Loyalsoldier/v2ray-rules-dat" -p "geosite.dat" `
+    -O  $PSScriptRoot/geosite.dat --clobber
