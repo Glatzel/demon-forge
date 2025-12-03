7831d527eed02d1a3812ce92145c387c4c546672
Glatzel <893016099@qq.com>
2025-12-03 21:36:48 +0800

    feat: create update-geofile script for xray (#513)
    Co-authored-by: pre-commit-ci-lite[bot] <117423508+pre-commit-ci-lite[bot]@users.noreply.github.com>

---
+++ packages/xray/update-geofile.ps1
@@ -0,0 +1,5 @@
+set-location $PSScriptRoot
+gh release download -R "Loyalsoldier/v2ray-rules-dat" -p "geoip.dat" `
+    -O  $PSScriptRoot/geoip.dat --clobber
+gh release download -R "Loyalsoldier/v2ray-rules-dat" -p "geosite.dat" `
+    -O  $PSScriptRoot/geosite.dat --clobber
---
+++ packages/xray/update-geofile.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+# Save current directory
+OLD_PWD="$PWD"
+
+# Go to script directory
+SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
+cd "$SCRIPT_DIR" || exit 1
+
+gh release download -R "Loyalsoldier/v2ray-rules-dat" -p "geoip.dat" \
+    -O  $PSScriptRoot/geoip.dat --clobber
+gh release download -R "Loyalsoldier/v2ray-rules-dat" -p "geoip.dat" \
+    -O  $PSScriptRoot/geosite.dat --clobber
+
+# Restore original directory
+cd "$OLD_PWD" || exit 1
+
+echo "Back to: $(pwd)"
