add1feed8ff243e578b57fb49a56a37734da4845
Glatzel <chenzelin9794@gmail.com>
2025-12-05 16:14:17 +0800

    f

---
+++ packages/nerdctl-full copy/ci-nerdctl-full.ps1
@@ -0,0 +1,17 @@
+Set-Location $PSScriptRoot
+$ROOT = git rev-parse --show-toplevel
+. $ROOT/scripts/util.ps1
+$latest_version = get-version-github -repo "containerd/nerdctl"
+update-recipe -version $latest_version
+if ($IsLinux -and $arch -eq "X64") {
+    gh release download -R "containerd/nerdctl" -p "nerdctl-full-*-linux-amd64.tar.gz" `
+        -O  $ROOT/temp/$name/$name.tar.gz --clobber
+}
+if ($IsLinux -and $arch -eq "Arm64") {
+    gh release download -R "containerd/nerdctl" -p "nerdctl-full-linux-arm64.tar.gz" `
+        -O  $ROOT/temp/$name/$name.tar.gz --clobber
+}
+7z x $ROOT/temp/$name/$name.tar.gz "-o$ROOT/temp/$name/"
+7z x $ROOT/temp/$name/$name.tar "-o$ROOT/temp/$name/$name"
+
+build-pkg
