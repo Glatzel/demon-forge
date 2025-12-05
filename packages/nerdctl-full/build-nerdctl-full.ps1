156b9e060551336645106809cb1220a86279eed3
Glatzel <chenzelin9794@gmail.com>
2025-12-05 16:28:23 +0800

    f

--- packages/nerdctl-full/build-nerdctl-full.ps1
+++
@@ -1,6 +0,0 @@
-Set-Location $PSScriptRoot
-$ROOT = git rev-parse --show-toplevel
-. $ROOT/scripts/util.ps1
-
-New-Item $env:PREFIX/bin/nerdctl -ItemType Directory
-Copy-Item "$ROOT/temp/$name/$name/*" "$env:PREFIX/bin/nerdctl" -Recurse
