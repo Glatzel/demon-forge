781005464d2f270f61231aca193b25e6dbcfa2b5
Glatzel <chenzelin9794@gmail.com>
2025-12-05 16:35:05 +0800

    f

--- packages/containerd/ci-containerd.ps1
+++ packages/containerd/ci-containerd.ps1
@@ -4,15 +4,15 @@ $ROOT = git rev-parse --show-toplevel
 $latest_version = get-version-github -repo "$name/$name"
 update-recipe -version $latest_version
 if ($IsWindows) {
-    gh release download -R "containerd/nerdctl" -p "$name-*-windows-amd64.tar.gz" `
+    gh release download -R "$name/$name" -p "$name-*-windows-amd64.tar.gz" `
         -O  $ROOT/temp/$name/$name.tar.gz --clobber
 }
 if ($IsLinux -and $arch -eq "X64") {
-    gh release download -R "containerd/nerdctl" -p "$name-static-*.*.*-linux-amd64.tar.gz" `
+    gh release download -R "$name/$name" -p "$name-static-*.*.*-linux-amd64.tar.gz" `
         -O  $ROOT/temp/$name/$name.tar.gz --clobber
 }
 if ($IsLinux -and $arch -eq "Arm64") {
-    gh release download -R "containerd/nerdctl" -p "$name-static-*.*.*-linux-arm64.tar.gz" `
+    gh release download -R "$name/$name" -p "$name-static-*.*.*-linux-arm64.tar.gz" `
         -O  $ROOT/temp/$name/$name.tar.gz --clobber
 }
 7z x $ROOT/temp/$name/$name.tar.gz "-o$ROOT/temp/$name/"
