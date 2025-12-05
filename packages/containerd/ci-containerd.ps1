fa8321686f1e8e9d2ee4dd1702fc1a15eb7497ad
Glatzel <chenzelin9794@gmail.com>
2025-12-05 16:33:14 +0800

    f

--- packages/containerd/ci-containerd.ps1
+++ packages/containerd/ci-containerd.ps1
@@ -1,18 +1,18 @@
 Set-Location $PSScriptRoot
 $ROOT = git rev-parse --show-toplevel
 . $ROOT/scripts/util.ps1
-$latest_version = get-version-github -repo "containerd/nerdctl"
+$latest_version = get-version-github -repo "$name/$name"
 update-recipe -version $latest_version
 if ($IsWindows) {
-    gh release download -R "containerd/nerdctl" -p "nerdctl-*-windows-amd64.tar.gz" `
+    gh release download -R "containerd/nerdctl" -p "$name-*-windows-amd64.tar.gz" `
         -O  $ROOT/temp/$name/$name.tar.gz --clobber
 }
 if ($IsLinux -and $arch -eq "X64") {
-    gh release download -R "containerd/nerdctl" -p "nerdctl-*.*.*-linux-amd64.tar.gz" `
+    gh release download -R "containerd/nerdctl" -p "$name-static-*.*.*-linux-amd64.tar.gz" `
         -O  $ROOT/temp/$name/$name.tar.gz --clobber
 }
 if ($IsLinux -and $arch -eq "Arm64") {
-    gh release download -R "containerd/nerdctl" -p "nerdctl-*.*.*-linux-arm64.tar.gz" `
+    gh release download -R "containerd/nerdctl" -p "$name-static-*.*.*-linux-arm64.tar.gz" `
         -O  $ROOT/temp/$name/$name.tar.gz --clobber
 }
 7z x $ROOT/temp/$name/$name.tar.gz "-o$ROOT/temp/$name/"
