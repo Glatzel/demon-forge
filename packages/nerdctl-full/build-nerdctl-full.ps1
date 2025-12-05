45af7ef5a5010147c3217efe6599d5c820bdc8b2
Glatzel <chenzelin9794@gmail.com>
2025-12-05 16:24:56 +0800

    f

--- packages/nerdctl-full/build-nerdctl-full.ps1
+++ packages/nerdctl-full/build-nerdctl-full.ps1
@@ -3,4 +3,4 @@ $ROOT = git rev-parse --show-toplevel
 . $ROOT/scripts/util.ps1
 
 New-Item $env:PREFIX/bin/nerdctl -ItemType Directory
-Copy-Item "$ROOT/temp/$name/$name/*" "$env:PREFIX/bin/nerdctl" -Recurse 
+Copy-Item "$ROOT/temp/$name/$name/*" "$env:PREFIX/bin/nerdctl" -Recurse
