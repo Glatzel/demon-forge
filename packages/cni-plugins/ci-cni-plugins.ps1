bc6c80ebca2cfda75db2ef96ad8b6206fa081cb5
Glatzel <893016099@qq.com>
2025-12-06 21:28:18 +0800

    feat: add cni-plugins (#520)

---
+++ packages/cni-plugins/ci-cni-plugins.ps1
@@ -0,0 +1,20 @@
+Set-Location $PSScriptRoot
+$ROOT = git rev-parse --show-toplevel
+. $ROOT/scripts/util.ps1
+$latest_version = get-version-github -repo "containernetworking/plugins"
+update-recipe -version $latest_version
+if ($IsWindows) {
+    gh release download -R "containernetworking/plugins" -p "$name-windows-amd64-*.tgz" `
+        -O  $ROOT/temp/$name/$name.tgz --clobber
+}
+if ($IsLinux -and $arch -eq "X64") {
+    gh release download -R "containernetworking/plugins" -p "$name-linux-amd64-*.tgz" `
+        -O  $ROOT/temp/$name/$name.tgz --clobber
+}
+if ($IsLinux -and $arch -eq "Arm64") {
+    gh release download -R "containernetworking/plugins" -p "$name-linux-arm64-*.tgz" `
+        -O  $ROOT/temp/$name/$name.tgz --clobber
+}
+7z x $ROOT/temp/$name/$name.tgz "-o$ROOT/temp/$name/"
+7z x $ROOT/temp/$name/$name.tar "-o$ROOT/temp/$name/$name"
+build-pkg
