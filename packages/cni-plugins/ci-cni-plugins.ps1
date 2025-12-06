be38790043aa8ec5e68b0e450b4d07864c112a24
Glatzel <chenzelin9794@gmail.com>
2025-12-06 21:22:23 +0800

    f

--- packages/cni-plugins/ci-cni-plugins.ps1
+++ packages/cni-plugins/ci-cni-plugins.ps1
@@ -9,10 +9,10 @@ if ($IsWindows) {
 }
 if ($IsLinux -and $arch -eq "X64") {
     gh release download -R "containernetworking/plugins" -p "$name-linux-amd64-*.tgz" `
-        -O  $ROOT/temp/$name/$nam.tgz --clobber
+        -O  $ROOT/temp/$name/$name.tgz --clobber
 }
 if ($IsLinux -and $arch -eq "Arm64") {
-    gh release download -R "containernetworking/plugins" -p "$name-linux-arm64.tgz" `
+    gh release download -R "containernetworking/plugins" -p "$name-linux-arm64-*.tgz" `
         -O  $ROOT/temp/$name/$name.tgz --clobber
 }
 7z x $ROOT/temp/$name/$name.tgz "-o$ROOT/temp/$name/"
