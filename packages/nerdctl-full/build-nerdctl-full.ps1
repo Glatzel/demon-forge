c0b2ab024d0a3a9020f78302c8d478a822d846ff
Glatzel <chenzelin9794@gmail.com>
2025-12-07 20:41:39 +0800

    f

--- packages/nerdctl-full/build-nerdctl-full.ps1
+++ packages/nerdctl-full/build-nerdctl-full.ps1
@@ -2,6 +2,6 @@ Set-Location $PSScriptRoot
 $ROOT = git rev-parse --show-toplevel
 . $ROOT/scripts/util.ps1
 
-Copy-Item "$ROOT/temp/$name/$name/*" "$env:PREFIX/" -Recurse\
+Copy-Item "$ROOT/temp/$name/$name/*" "$env:PREFIX/" -Recurse
 # do not use bundled runc
 Remove-Item "$env:PREFIX/bin/runc"
