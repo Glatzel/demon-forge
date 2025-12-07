4ad96e33e31d05188d6233efdce79b538b1c9005
Glatzel <893016099@qq.com>
2025-12-07 20:43:54 +0800

    fix: nerdctl (#525)
    Co-authored-by: pre-commit-ci-lite[bot] <117423508+pre-commit-ci-lite[bot]@users.noreply.github.com>

--- packages/nerdctl-full/build-nerdctl-full.ps1
+++ packages/nerdctl-full/build-nerdctl-full.ps1
@@ -3,3 +3,5 @@ $ROOT = git rev-parse --show-toplevel
 . $ROOT/scripts/util.ps1
 
 Copy-Item "$ROOT/temp/$name/$name/*" "$env:PREFIX/" -Recurse
+# do not use bundled runc
+Remove-Item "$env:PREFIX/bin/runc"
