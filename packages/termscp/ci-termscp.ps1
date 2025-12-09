4c29900e8ca30affb3954b56288622110c65acca
Glatzel <chenzelin9794@gmail.com>
2025-12-09 19:29:16 +0800

    f

--- packages/termscp/ci-termscp.ps1
+++ packages/termscp/ci-termscp.ps1
@@ -3,9 +3,5 @@ $ROOT = git rev-parse --show-toplevel
 . $ROOT/scripts/util.ps1
 $latest_version = get-version-crateio -name $name
 update-recipe -version $latest_version
-
-gh release download -R veeso/$name -p "*x86_64-pc-windows*.zip" `
-    -O  $ROOT/temp/$name/$name.zip --clobber
-    7z x "$ROOT/temp/$name/$name.zip" "-o$ROOT/temp/$name"
-
+build-cargo-package $name $name
 build-pkg
