cd83fd5e27437a24ed022ba295b6e657e6735ee6
Glatzel <chenzelin9794@gmail.com>
2025-12-09 16:18:45 +0800

    ff

---
+++ packages/v2rayn/build-v2rayn.ps1
@@ -0,0 +1,10 @@
+Set-Location $PSScriptRoot
+$ROOT = git rev-parse --show-toplevel
+. $ROOT/scripts/util.ps1
+
+New-Item $env:PREFIX/bin -ItemType Directory
+Copy-Item "$ROOT/temp/$name/$name/Release/*" "$env:PREFIX/bin" -Recurse
+
+# shortcut
+New-Item $env:PREFIX/Menu -ItemType Directory
+Copy-Item "$name.json" "$env:PREFIX/Menu"
