50fbc4b7170e18d598a97ab3be7a01f5e7eaaf41
Glatzel <chenzelin9794@gmail.com>
2025-12-09 19:49:54 +0800

    f

--- packages/termscp/build-termscp.ps1
+++ packages/termscp/build-termscp.ps1
@@ -3,4 +3,4 @@ $ROOT = git rev-parse --show-toplevel
 . $ROOT/scripts/util.ps1
 
 New-Item $env:PREFIX/bin -ItemType Directory
-Copy-Item "$ROOT/temp/$name/bin/*" "$env:PREFIX/bin/" -Recurse
+Copy-Item "$ROOT/temp/$name/bin/*" "$env:PREFIX/bin/"
