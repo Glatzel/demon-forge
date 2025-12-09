6ba3b5d129d40874fa1963b7c188b2a77b8c2e95
Glatzel <chenzelin9794@gmail.com>
2025-12-09 16:23:32 +0800

    f

--- packages/v2rayn/build-v2rayn.ps1
+++ packages/v2rayn/build-v2rayn.ps1
@@ -2,9 +2,20 @@ Set-Location $PSScriptRoot
 $ROOT = git rev-parse --show-toplevel
 . $ROOT/scripts/util.ps1
 
-New-Item $env:PREFIX/bin -ItemType Directory
-Copy-Item "$ROOT/temp/$name/$name/*" "$env:PREFIX/bin" -Recurse
+New-Item $env:PREFIX/bin/$name -ItemType Directory
 
+if ($IsWindows) {
+    Copy-Item "$ROOT/temp/$name/$name/v2rayN-windows-64-SelfContained/*" "$env:PREFIX/bin/$name/" -Recurse
+}
+if ($IsMacOS) {
+    Copy-Item "$ROOT/temp/$name/$name/v2rayN-macos-arm64/*" "$env:PREFIX/bin/$name/" -Recurse
+}
+if ($IsLinux -and $arch -eq "X64") {
+    Copy-Item "$ROOT/temp/$name/$name/v2rayN-linux-64/*" "$env:PREFIX/bin/$name/" -Recurse
+}
+if ($IsLinux -and $arch -eq "Arm64") {
+    Copy-Item "$ROOT/temp/$name/$name/v2rayN-linux-arm64/*" "$env:PREFIX/bin/$name/" -Recurse
+}
 # shortcut
 New-Item $env:PREFIX/Menu -ItemType Directory
 Copy-Item "$name.json" "$env:PREFIX/Menu"
