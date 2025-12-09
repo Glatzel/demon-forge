5c61dbb908c6eec06cf50477fa12d701e0d4da85
Glatzel <chenzelin9794@gmail.com>
2025-12-09 16:30:12 +0800

    发

--- packages/v2rayn/ci-v2rayn.ps1
+++ packages/v2rayn/ci-v2rayn.ps1
@@ -8,17 +8,17 @@ if ($IsWindows) {
     gh release download -R 2dust/v2rayN -p "v2rayN-windows-64-SelfContained.zip" `
         -O  $ROOT/temp/$name/$name.zip --clobber
 }
-if ($IsMacOS) {
-    gh release download -R 2dust/v2rayN -p "v2rayN-macos-arm64.zip" `
-        -O  $ROOT/temp/$name/$name.zip --clobber
-}
-if ($IsLinux -and $arch -eq "X64") {
-    gh release download -R 2dust/v2rayN -p "v2rayN-linux-64.zip" `
-        -O  $ROOT/temp/$name/$name.zip --clobber
-}
-if ($IsLinux -and $arch -eq "Arm64") {
-    gh release download -R 2dust/v2rayN -p "v2rayN-linux-arm64.zip" `
-        -O  $ROOT/temp/$name/$name.zip --clobber
-}
+# if ($IsMacOS) {
+#     gh release download -R 2dust/v2rayN -p "v2rayN-macos-arm64.zip" `
+#         -O  $ROOT/temp/$name/$name.zip --clobber
+# }
+# if ($IsLinux -and $arch -eq "X64") {
+#     gh release download -R 2dust/v2rayN -p "v2rayN-linux-64.zip" `
+#         -O  $ROOT/temp/$name/$name.zip --clobber
+# }
+# if ($IsLinux -and $arch -eq "Arm64") {
+#     gh release download -R 2dust/v2rayN -p "v2rayN-linux-arm64.zip" `
+#         -O  $ROOT/temp/$name/$name.zip --clobber
+# }
 7z x "$ROOT/temp/$name/$name.zip" "-o$ROOT/temp/$name/$name"
 build-pkg
