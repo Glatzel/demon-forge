27d0c0bd1a3d7cfacc1a5646fc367833b6237a79
Glatzel <chenzelin9794@gmail.com>
2025-12-09 16:21:27 +0800

    f

--- packages/v2rayn/build-v2rayn.ps1
+++ packages/v2rayn/build-v2rayn.ps1
@@ -3,7 +3,7 @@ $ROOT = git rev-parse --show-toplevel
 . $ROOT/scripts/util.ps1
 
 New-Item $env:PREFIX/bin -ItemType Directory
-Copy-Item "$ROOT/temp/$name/$name/Release/*" "$env:PREFIX/bin" -Recurse
+Copy-Item "$ROOT/temp/$name/$name/*" "$env:PREFIX/bin" -Recurse
 
 # shortcut
 New-Item $env:PREFIX/Menu -ItemType Directory
