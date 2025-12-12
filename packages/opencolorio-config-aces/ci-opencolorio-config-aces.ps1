f92baa4d1d14a96ef82b1fc77f8c76dba9179789
Glatzel <chenzelin9794@gmail.com>
2025-12-12 18:53:27 +0800

    f

---
+++ packages/opencolorio-config-aces/ci-opencolorio-config-aces.ps1
@@ -0,0 +1,9 @@
+Set-Location $PSScriptRoot
+$ROOT = git rev-parse --show-toplevel
+. $ROOT/scripts/util.ps1
+$latest_version = get-version-github -repo "AcademySoftwareFoundation/OpenColorIO-Config-ACES"
+update-recipe -version $latest_version
+
+gh release download -R "AcademySoftwareFoundation/OpenColorIO-Config-ACES" -D  $ROOT/temp/$name --clobber
+
+build-pkg
