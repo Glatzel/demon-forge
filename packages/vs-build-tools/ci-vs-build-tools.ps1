6c69b19d595966671dcd4dfc663dcf1520c160af
Glatzel <chenzelin9794@gmail.com>
2025-12-19 21:37:36 +0800

    f

--- packages/vs-build-tools/ci-vs-build-tools.ps1
+++ packages/vs-build-tools/ci-vs-build-tools.ps1
@@ -1,8 +1,7 @@
 Set-Location $PSScriptRoot
 $ROOT = git rev-parse --show-toplevel
 . $ROOT/scripts/util.ps1
-winget search --source winget Microsoft.VisualStudio.BuildTools
-$latest_version = get-version-text -text $(winget search Microsoft.VisualStudio.BuildTools) -pattern ': (\d+\.\d+\.\d+)'
+$latest_version = get-version-text -text $(winget search --source winget Microsoft.VisualStudio.BuildTools) -pattern ': (\d+\.\d+\.\d+)'
 update-recipe -version $latest_version
 
 aria2c -c -x16 -s16 -d "$ROOT/temp/$name/" `
