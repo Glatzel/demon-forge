429111bb5140df43c02de834f6fae075b2a9372d
Glatzel <chenzelin9794@gmail.com>
2025-12-19 21:23:48 +0800

    f

--- packages/vs-build-tools/ci-vs-build-tools.ps1
+++ packages/vs-build-tools/ci-vs-build-tools.ps1
@@ -2,7 +2,7 @@ Set-Location $PSScriptRoot
 $ROOT = git rev-parse --show-toplevel
 . $ROOT/scripts/util.ps1
 
-$latest_version = get-version-text -text $(winget search Microsoft.VisualStudio.BuildTools) -pattern ': (\d+\.\d+\.\d+)'
+$latest_version = get-version-text -text $(vswhere -products * -format json | jq -r '.[].catalog.productDisplayVersion') -pattern ': (\d+\.\d+\.\d+)'
 update-recipe -version $latest_version
 
 aria2c -c -x16 -s16 -d "$ROOT/temp/$name/" `
