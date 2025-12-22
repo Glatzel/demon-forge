eeeb26707e196a36671d1f157d24b9579d4242da
Glatzel <chenzelin9794@gmail.com>
2025-12-22 11:17:13 +0800

    feat: add zed

---
+++ packages/zed/ci-zed.ps1
@@ -0,0 +1,7 @@
+Set-Location $PSScriptRoot
+$ROOT = git rev-parse --show-toplevel
+. $ROOT/scripts/util.ps1
+$latest_version = get-version-github "zed-industries/$name"
+update-recipe -version $latest_version
+build-cargo-package-github $name "https://github.com/zed-industries/$name.git" "v$latest_version"
+build-pkg
