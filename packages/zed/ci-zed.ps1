56d42d127cff60d40cd155d0fa105187effbf414
Glatzel <chenzelin9794@gmail.com>
2025-12-22 12:36:02 +0800

    Update ci-zed.ps1

--- packages/zed/ci-zed.ps1
+++ packages/zed/ci-zed.ps1
@@ -3,6 +3,12 @@ $ROOT = git rev-parse --show-toplevel
 . $ROOT/scripts/util.ps1
 $latest_version = get-version-github "zed-industries/$name"
 update-recipe -version $latest_version
+New-ItemProperty `
+  -Path "HKLM:\SYSTEM\CurrentControlSet\Control\FileSystem" `
+  -Name "LongPathsEnabled" `
+  -PropertyType DWord `
+  -Value 1 `
+  -Force
 build-cargo-package-github $name "https://github.com/zed-industries/$name.git" "v$latest_version" zed
 build-cargo-package-github $name "https://github.com/zed-industries/$name.git" "v$latest_version" cli
 build-pkg
