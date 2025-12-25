b498766535a34d3a6a389522225eeb2ebe0cec54
Glatzel <893016099@qq.com>
2025-12-25 15:41:55 +0800

    更新 build-zed.ps1

--- packages/zed/build-zed.ps1
+++ packages/zed/build-zed.ps1
@@ -3,7 +3,8 @@ $ROOT = git rev-parse --show-toplevel
 . $ROOT/scripts/util.ps1
 
 New-Item $env:PREFIX/bin -ItemType Directory
-Copy-Item "$ROOT/temp/$name/bin/*" "$env:PREFIX/bin/"
+Copy-Item "$ROOT/temp/$name/$name/target/*/zed.exe" "$env:PREFIX/bin/zed.exe"
+Copy-Item "$ROOT/temp/$name/$name/target/*/cli.exe" "$env:PREFIX/bin/zed-cli.exe"
 
 # shortcut
 New-Item $env:PREFIX/Menu -ItemType Directory
