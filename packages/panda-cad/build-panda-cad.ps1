5314119a065a3d0b65ef97c4b31e31a63f89768f
Glatzel <chenzelin9794@gmail.com>
2025-12-25 12:43:26 +0800

    feat: add panda-cad

---
+++ packages/panda-cad/build-panda-cad.ps1
@@ -0,0 +1,9 @@
+Set-Location $PSScriptRoot
+$ROOT = git rev-parse --show-toplevel
+. $ROOT/scripts/util.ps1
+
+New-Item $env:PREFIX/bin/$name -ItemType Directory
+Copy-Item "C:/Program Files (x86)/PandaCAD/*" "$env:PREFIX/bin/$name" -Recurse
+# shortcut
+New-Item $env:PREFIX/Menu -ItemType Directory
+Copy-Item "$name.json" "$env:PREFIX/Menu"
