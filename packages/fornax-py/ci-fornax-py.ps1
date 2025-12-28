25ba55706631de1299f538a95c8a73dd684329f3
Glatzel <893016099@qq.com>
2025-12-28 21:24:58 +0800

    feat: add fornax (#593)
    Co-authored-by: pre-commit-ci-lite[bot] <117423508+pre-commit-ci-lite[bot]@users.noreply.github.com>

---
+++ packages/fornax-py/ci-fornax-py.ps1
@@ -0,0 +1,16 @@
+Set-Location $PSScriptRoot
+$ROOT = git rev-parse --show-toplevel
+. $ROOT/scripts/util.ps1
+
+$latest_version = get-version-github -repo "Glatzel/fornax"
+update-recipe -version $latest_version
+Set-Location $ROOT/temp/$name
+gh repo clone Glatzel/fornax
+Set-Location fornax
+git checkout tags/"v$latest_version" -b "$latest_version-branch"
+& ./scripts/setup.ps1
+Set-Location ./crates/fornax-py
+pixi run maturin build --out ./dist --profile release
+
+Set-Location $PSScriptRoot
+build-pkg
