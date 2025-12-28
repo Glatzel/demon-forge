50146f3eb4f3f4d7f4809cb0fc62ecbf6e4d7e01
Glatzel <chenzelin9794@gmail.com>
2025-12-28 19:27:28 +0800

    f

--- packages/toolbox/build-toolbox.ps1
+++ packages/toolbox/build-toolbox.ps1
@@ -2,5 +2,4 @@ Set-Location $PSScriptRoot
 $ROOT = git rev-parse --show-toplevel
 . $ROOT/scripts/util.ps1
 
-Set-Location $ROOT/temp/$name/$name
-pip install . -v
+pip install $ROOT/temp/$name/$name/python -v
