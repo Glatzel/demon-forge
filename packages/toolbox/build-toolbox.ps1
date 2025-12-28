ba3f60f4a40dae5bac32f7ddd88c347cb104f421
Glatzel <chenzelin9794@gmail.com>
2025-12-28 19:10:31 +0800

    f

--- packages/toolbox/build-toolbox.ps1
+++ packages/toolbox/build-toolbox.ps1
@@ -6,7 +6,7 @@ $latest_version = get-version-github -repo "Glatzel/$name"
 update-recipe -version $latest_version
 
 Set-Location $ROOT/temp/$name
-gh repo clone $name
+gh repo clone Glatzel/$name 
 Set-Location $name
 git checkout tags/"v$latest_version" -b "$latest_version-branch"
 
