d743e47eb90eda10a82585755cba0208fae352fc
Glatzel <893016099@qq.com>
2025-12-28 19:26:33 +0800

    feat: add mason (#592)
    Co-authored-by: pre-commit-ci-lite[bot] <117423508+pre-commit-ci-lite[bot]@users.noreply.github.com>

---
+++ packages/mason/ci-mason.ps1
@@ -0,0 +1,17 @@
+Set-Location $PSScriptRoot
+$ROOT = git rev-parse --show-toplevel
+. $ROOT/scripts/util.ps1
+
+$latest_version = get-version-github -repo "Glatzel/$name"
+update-recipe -version $latest_version
+
+Set-Location $ROOT/temp/$name
+gh repo clone Glatzel/$name
+Set-Location $name
+git checkout tags/"v$latest_version" -b "$latest_version-branch"
+
+& ./scripts/build.ps1 -Release
+
+Set-Location $PSScriptRoot
+pixi run rattler-build build
+Set-Location $ROOT
