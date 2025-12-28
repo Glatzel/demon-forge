4b6cd5b34f0ca4b304da68bfe908232e01626a7a
Glatzel <chenzelin9794@gmail.com>
2025-12-28 19:07:54 +0800

    f

--- packages/toolbox/build-toolbox.ps1
+++ packages/toolbox/build-toolbox.ps1
@@ -1,5 +1,15 @@
 Set-Location $PSScriptRoot
 $ROOT = git rev-parse --show-toplevel
 . $ROOT/scripts/util.ps1
-Set-Location $ROOT/temp/$name/$name/python
-pip install . -v
+
+$latest_version = get-version-github -repo "Glatzel/$name"
+update-recipe -version $latest_version
+
+Set-Location $ROOT/temp/$name
+gh repo clone $name 
+Set-Location $name
+git checkout tags/"v$latest_version" -b "$latest_version-branch"
+
+Set-Location $PSScriptRoot
+pixi run rattler-build build
+Set-Location $ROOT
