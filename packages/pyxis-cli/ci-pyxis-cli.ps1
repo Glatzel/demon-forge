f30419ca53aaed1bab649b3e2093a9ba60fddd43
Glatzel <chenzelin9794@gmail.com>
2025-12-28 21:57:26 +0800

    f

--- packages/pyxis-cli/ci-pyxis-cli.ps1
+++ packages/pyxis-cli/ci-pyxis-cli.ps1
@@ -1,9 +1,21 @@
-$ErrorActionPreference = "Stop"
-$PSNativeCommandUseErrorActionPreference = $true
-$ROOT = git rev-parse --show-toplevel
-Set-Location $PSScriptRoot/..
-& ./scripts/setup.ps1
-cargo build --bin pyxis --release
 Set-Location $PSScriptRoot
-pixi run rattler-build build
-Set-Location $ROOT
+$ROOT = git rev-parse --show-toplevel
+. $ROOT/scripts/util.ps1
+
+$latest_version = get-version-github -repo "Glatzel/pyxis"
+update-recipe -version $latest_version
+Set-Location $ROOT/temp/$name
+
+gh repo clone Glatzel/pyxis
+Set-Location pyxis
+git checkout tags/"v$latest_version" -b "$latest_version-branch"
+Set-Location rust
+& ./scripts/setup.ps1
+if ($env:DIST_BUILD) {
+    cargo build --bin pyxis --release
+}
+else {
+    cargo build --bin pyxis 
+}
+Set-Location $PSScriptRoot
+build-pkg
