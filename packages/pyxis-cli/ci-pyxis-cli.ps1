5414cb47e662e0e782541e1ce7a1aa978b83cece
Glatzel <893016099@qq.com>
2025-12-28 22:11:43 +0800

    feat: add pyxis (#594)
    Co-authored-by: pre-commit-ci-lite[bot] <117423508+pre-commit-ci-lite[bot]@users.noreply.github.com>

--- packages/pyxis-cli/ci-pyxis-cli.ps1
+++ packages/pyxis-cli/ci-pyxis-cli.ps1
@@ -2,19 +2,20 @@ Set-Location $PSScriptRoot
 $ROOT = git rev-parse --show-toplevel
 . $ROOT/scripts/util.ps1
 
-$latest_version = get-version-github -repo "Glatzel/$name"
+$latest_version = get-version-github -repo "Glatzel/pyxis"
 update-recipe -version $latest_version
-
 Set-Location $ROOT/temp/$name
-gh repo clone Glatzel/$name
-Set-Location $name
+
+gh repo clone Glatzel/pyxis
+Set-Location pyxis
 git checkout tags/"v$latest_version" -b "$latest_version-branch"
+Set-Location rust
+& ./scripts/setup.ps1
 if ($env:DIST_BUILD) {
-    & ./scripts/build.ps1 -Release
+    cargo build --bin pyxis --release
 }
 else {
-    & ./scripts/build.ps1
+    cargo build --bin pyxis
 }
 Set-Location $PSScriptRoot
-pixi run rattler-build build
-Set-Location $ROOT
+build-pkg
--- packages/pyxis-py/ci-pyxis-py.ps1
+++ packages/pyxis-py/ci-pyxis-py.ps1
@@ -2,19 +2,20 @@ Set-Location $PSScriptRoot
 $ROOT = git rev-parse --show-toplevel
 . $ROOT/scripts/util.ps1
 
-$latest_version = get-version-github -repo "Glatzel/$name"
+$latest_version = get-version-github -repo "Glatzel/pyxis"
 update-recipe -version $latest_version
 
 Set-Location $ROOT/temp/$name
-gh repo clone Glatzel/$name
-Set-Location $name
+gh repo clone Glatzel/pyxis
+Set-Location pyxis
 git checkout tags/"v$latest_version" -b "$latest_version-branch"
+Set-Location python
 if ($env:DIST_BUILD) {
-    & ./scripts/build.ps1 -Release
+    pixi run maturin build --out ./dist --profile release
 }
 else {
-    & ./scripts/build.ps1
+    pixi run maturin build --out ./dist
 }
+
 Set-Location $PSScriptRoot
-pixi run rattler-build build
-Set-Location $ROOT
+build-pkg
