e0b1c30d774250acda649e8bfeadd91f5020f0ba
Glatzel <chenzelin9794@gmail.com>
2025-12-28 21:50:49 +0800

    f

---
+++ packages/pyxis-py/build-pyxis-py.ps1
@@ -0,0 +1,10 @@
+foreach ($whl in Get-ChildItem "$env:RECIPE_DIR/../dist/*.whl") {
+    pip install "$whl" -v
+}
+Set-Location $PSScriptRoot
+$ROOT = git rev-parse --show-toplevel
+. $ROOT/scripts/util.ps1
+
+foreach ($whl in Get-ChildItem "$ROOT/temp/$name/pyxis/python/dist/*.whl") {
+    pip install "$whl" -v
+}
---
+++ packages/pyxis-py/ci-pyxis-py.ps1
@@ -0,0 +1,16 @@
+Set-Location $PSScriptRoot
+$ROOT = git rev-parse --show-toplevel
+. $ROOT/scripts/util.ps1
+
+$latest_version = get-version-github -repo "Glatzel/$name"
+update-recipe -version $latest_version
+Set-Location $ROOT/temp/$name
+
+gh repo clone Glatzel/$name
+Set-Location $name
+git checkout tags/"v$latest_version" -b "$latest_version-branch"
+Set-Location python
+pixi run maturin build --out ./dist --profile release
+
+Set-Location $PSScriptRoot
+build-pkg
