702c149b12d431dbab342085594a897d318e044c
Glatzel <chenzelin9794@gmail.com>
2025-12-28 18:58:41 +0800

    f

--- packages/mason/build-mason.ps1
+++ packages/mason/build-mason.ps1
@@ -1,4 +1,4 @@
 $ROOT = git rev-parse --show-toplevel
 
 New-Item $env:PREFIX/bin/$name -ItemType Directory
-Copy-Item "$ROOT/temp/$name/bin/Mason/*" "$env:PREFIX/bin/$name" -Recurse
+Copy-Item "$ROOT/temp/$name/$name/bin/Mason/*" "$env:PREFIX/bin/$name" -Recurse
