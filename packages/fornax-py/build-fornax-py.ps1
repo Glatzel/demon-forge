33d47d867b88bf7b8953d374c620d895ad710453
Glatzel <chenzelin9794@gmail.com>
2025-12-28 21:19:34 +0800

    f

--- packages/fornax-py/build-fornax-py.ps1
+++ packages/fornax-py/build-fornax-py.ps1
@@ -2,6 +2,6 @@ Set-Location $PSScriptRoot
 $ROOT = git rev-parse --show-toplevel
 . $ROOT/scripts/util.ps1
 
-foreach ($whl in Get-ChildItem "$ROOT/temp/$name/$name/crates/fornax-py/dist/*.whl") {
+foreach ($whl in Get-ChildItem "$ROOT/temp/$name/fornax/crates/fornax-py/dist/*.whl") {
     pip install "$whl" -v
 }
--- packages/fornax-py/ci-fornax-py.ps1
+++ packages/fornax-py/ci-fornax-py.ps1
@@ -2,11 +2,11 @@ Set-Location $PSScriptRoot
 $ROOT = git rev-parse --show-toplevel
 . $ROOT/scripts/util.ps1
 
-$latest_version = get-version-github -repo "Glatzel/$name"
+$latest_version = get-version-github -repo "Glatzel/fornax"
 update-recipe -version $latest_version
 Set-Location $ROOT/temp/$name
-gh repo clone Glatzel/$name
-Set-Location $name
+gh repo clone Glatzel/fornax
+Set-Location fornax
 git checkout tags/"v$latest_version" -b "$latest_version-branch"
 & ./scripts/setup.ps1
 Set-Location ./crates/fornax-py
