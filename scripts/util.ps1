60efed7397d396c385f60c82b75e3df47d7867d5
Glatzel <chenzelin9794@gmail.com>
2025-12-29 11:05:37 +0800

    f

--- scripts/util.ps1
+++ scripts/util.ps1
@@ -47,6 +47,7 @@ function get-version-crateio {
     param($name)
     for ($i = 0; $i -lt 5; $i++) {
         $latest = curl -s https://crates.io/api/v1/crates/$name | jq -r '.crate.max_version'
+        Write-Output $latest
         if ($latest) {
             return $latest
         }
