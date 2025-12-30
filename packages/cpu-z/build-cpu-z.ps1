e81e9dc6f89d7d0c62c0465780fa2dc3dccbc77c
Glatzel <893016099@qq.com>
2025-12-30 23:14:40 +0800

    更新 build-cpu-z.ps1

--- packages/cpu-z/build-cpu-z.ps1
+++ packages/cpu-z/build-cpu-z.ps1
@@ -1,7 +1,7 @@
 Set-Location $PSScriptRoot
 $ROOT = git rev-parse --show-toplevel
 . $ROOT/scripts/util.ps1
-& $env:BUILD_PREFIX/bin/selenium python download.py
+& $env:BUILD_PREFIX/python download.py
 $zipfile = (Get-ChildItem "$ROOT/temp/$name/*.zip")[0]
 7z x "$zipfile" "-o$ROOT/temp/$name/$name"
 New-Item $env:PREFIX/bin/$name -ItemType Directory
