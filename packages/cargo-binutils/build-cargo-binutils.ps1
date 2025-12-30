2774ba7e51042d853930f17996370197bdda24b1
Glatzel <chenzelin9794@gmail.com>
2025-12-30 22:16:51 +0800

    f

--- packages/cargo-binutils/build-cargo-binutils.ps1
+++ packages/cargo-binutils/build-cargo-binutils.ps1
@@ -2,5 +2,3 @@ Set-Location $PSScriptRoot
 $ROOT = git rev-parse --show-toplevel
 . $ROOT/scripts/util.ps1
 build-cargo-package $name $name
-New-Item $env:PREFIX/bin -ItemType Directory
-Copy-Item "$ROOT/temp/$name/bin/*" "$env:PREFIX/bin/"
--- packages/mqttui/build-mqttui.ps1
+++ packages/mqttui/build-mqttui.ps1
@@ -1,6 +1,3 @@
 Set-Location $PSScriptRoot
 $ROOT = git rev-parse --show-toplevel
 . $ROOT/scripts/util.ps1
-
-New-Item $env:PREFIX/bin -ItemType Directory
-Copy-Item "$ROOT/temp/$name/bin/*" "$env:PREFIX/bin/"
