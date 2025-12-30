f159549ff9fafb5a066e8f2d0c8d8d42d71afd3c
Glatzel <chenzelin9794@gmail.com>
2025-12-30 22:13:14 +0800

    f

--- packages/cpu-z/ci-cpu-z.ps1
+++ packages/cpu-z/ci-cpu-z.ps1
@@ -3,9 +3,4 @@ $ROOT = git rev-parse --show-toplevel
 . $ROOT/scripts/util.ps1
 $latest_version = get-version-url -url "https://www.cpuid.com/softwares/cpu-z.html" -pattern 'cpu-z_(\d+\.\d+)-en.zip'
 update-recipe -version $latest_version
-
-pixi run -e selenium python download.py
-
-$zipfile = (Get-ChildItem "$ROOT/temp/$name/*.zip")[0]
-7z x "$zipfile" "-o$ROOT/temp/$name/$name"
 build-pkg
