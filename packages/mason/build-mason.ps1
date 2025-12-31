0430206f1efc3a596c55fa07e2d6a166ff6ab78b
Glatzel <chenzelin9794@gmail.com>
2025-12-31 10:56:54 +0800

    g

--- packages/mason/build-mason.ps1
+++ packages/mason/build-mason.ps1
@@ -1,10 +1,12 @@
 Set-Location $PSScriptRoot
 $ROOT = git rev-parse --show-toplevel
 . $ROOT/scripts/util.ps1
+
+$version = get-current-version
 Set-Location $ROOT/temp/$name
 gh repo clone Glatzel/$name
 Set-Location $name
-git checkout tags/"v$(get-current-version)" -b "branch-$(get-current-version)"
+git checkout tags/"v$version" -b "branch-$version"
 if ($env:DIST_BUILD) {
     & ./scripts/build.ps1 -Release
 }
--- packages/toolbox/build-toolbox.ps1
+++ packages/toolbox/build-toolbox.ps1
@@ -1,8 +1,9 @@
 Set-Location $PSScriptRoot
 $ROOT = git rev-parse --show-toplevel
 . $ROOT/scripts/util.ps1
+$version = get-current-version
 Set-Location $ROOT/temp/$name
 gh repo clone Glatzel/$name
 Set-Location $name
-git checkout tags/"v$latest_version" -b "$latest_version-branch"
+git checkout tags/"v$version" -b "branch-$version"
 pip install $ROOT/temp/$name/$name/python -v
