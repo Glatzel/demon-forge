692f10e9b508b6b1016c4170236af9ec8197fc77
Glatzel <chenzelin9794@gmail.com>
2025-12-31 09:08:04 +0800

    f

--- packages/cargo-unused-features/build-cargo-unused-features.ps1
+++ packages/cargo-unused-features/build-cargo-unused-features.ps1
@@ -1,6 +1,9 @@
 Set-Location $PSScriptRoot
 $ROOT = git rev-parse --show-toplevel
 . $ROOT/scripts/util.ps1
+if($IsLinux){
+    $env:OPENSSL_DIR="$env:BUILD_PREFIX"
+}
 build-cargo-package $name $name
 if ($IsWindows) {
     Rename-Item $env:PREFIX/bin/unused-features.exe $env:PREFIX/bin/$name.exe
