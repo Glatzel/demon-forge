24d4dce4d5a407e28c9c74b81283806c7dd2843c
Glatzel <chenzelin9794@gmail.com>
2025-12-31 16:37:58 +0800

    f

--- packages/ttyd/build-ttyd.ps1
+++ packages/ttyd/build-ttyd.ps1
@@ -1,6 +1,13 @@
 Set-Location $PSScriptRoot
 $ROOT = git rev-parse --show-toplevel
 . $ROOT/scripts/util.ps1
+
+if ($IsWindows) {
+    $env:PATH = "$env:BUILD_PREFIX/bin;$env:PATH"
+}
+if ($IsLinux) {
+    $env:PATH = "$env:BUILD_PREFIX/bin`:$env:PATH"
+}
 New-Item $env:PREFIX/bin -ItemType Directory
 Set-Location $ROOT/temp/$name
 git clone https://github.com/tsl0922/ttyd.git
@@ -9,16 +16,15 @@ git checkout tags/"$latest_version" -b "$latest_version-branch"
 copy-item $PSScriptRoot/build/* $ROOT/temp/$name/$name -recurse
 git apply config.patch
 get-content ./index.scss >> ./html/src/style/index.scss
-
 & ./download-font.ps1
 
 Set-Location ./html
-pixi run npm install -g corepack
-pixi run corepack enable
-pixi run corepack prepare yarn@stable --activate
-pixi run yarn install
-pixi run yarn run check
-pixi run yarn run build
+npm install -g corepack
+corepack enable
+corepack prepare yarn@stable --activate
+yarn install
+yarn run check
+yarn run build
 Set-Location ..
 
 if ($IsWindows) {
@@ -26,13 +32,11 @@ if ($IsWindows) {
 }
 if ($IsLinux -and $arch -eq 'X64') {
     $env:BUILD_TARGET = "x86_64"
-     pixi run bash ./scripts/cross-build.sh
-    pixi run rattler-build build
+    bash ./scripts/cross-build.sh
     copy-item $ROOT/temp/$name/$name/build/$name $env:PREFIX/bin/$name
 }
 if ($IsLinux -and $arch -eq 'Arm64') {
-    $env:BUILD_TARGET =  "aarch64"
-     pixi run bash ./scripts/cross-build.sh
-    pixi run rattler-build build
+    $env:BUILD_TARGET = "aarch64"
+    bash ./scripts/cross-build.sh
     copy-item $ROOT/temp/$name/$name/build/$name $env:PREFIX/bin/$name
 }
