cbc4ce7915a8c3061f114704cb16b7a8f4f6e501
Glatzel <chenzelin9794@gmail.com>
2025-12-31 11:02:56 +0800

    f

--- packages/opencolorio-config-aces/build-opencolorio-config-aces.ps1
+++ packages/opencolorio-config-aces/build-opencolorio-config-aces.ps1
@@ -3,11 +3,11 @@ $ROOT = git rev-parse --show-toplevel
 . $ROOT/scripts/util.ps1
 gh release download -R "AcademySoftwareFoundation/OpenColorIO-Config-ACES" -p "*.ocio" --dir "$ROOT/temp/$name" --clobber
 Get-ChildItem $ROOT/temp/$name | ForEach-Object {
-    $name = $_.BaseName
+    $n = $_.BaseName
     $ext = $_.Extension
 
     # Extract prefix before the first "-v"
-    if ($name -match '^(.*?)-v') {
+    if ($n -match '^(.*?)-v') {
         $newName = "$($Matches[1])$ext"
         Rename-Item $_.FullName $newName
     }
