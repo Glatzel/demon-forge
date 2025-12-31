30ecdc9e3ba2c4656b4e71ddb9ca341718c4a55f
Glatzel <chenzelin9794@gmail.com>
2025-12-31 18:36:34 +0800

    f

--- packages/ttyd/build-ttyd.ps1
+++ packages/ttyd/build-ttyd.ps1
@@ -29,6 +29,10 @@ Set-Location ..
 $env:CMAKE_BUILD_TYPE = "RELEASE"
 mkdir build
 Set-Location build
+if ($IsMacOS) {
+    $env:WEB_SOCKET_LIBRARY = "$env:BUILD_PREFIX/lib" 
+    $env:WEB_SOCKET_INCLUDE_DIR = "$env:BUILD_PREFIX/include" 
+}
 cmake `
     -DCMAKE_INSTALL_PREFIX="$env:PREFIX" `
     -DCMAKE_BUILD_TYPE="RELEASE" `
