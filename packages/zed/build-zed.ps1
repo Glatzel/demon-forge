e9086db995add0fd141b09cb5c8f5887c05b1f45
Glatzel <chenzelin9794@gmail.com>
2025-12-31 19:02:25 +0800

    f

--- packages/zed/build-zed.ps1
+++ packages/zed/build-zed.ps1
@@ -38,6 +38,15 @@ else {
     & $cargo build --root $env:PREFIX --package zed --package cli
 }
 
+if ($env:DIST_BUILD) {
+    $build_profile = 'release'
+}
+else {
+    $build_profile = 'debug'
+}
+
+Copy-Item "$ROOT/temp/$name/$name/target/$build_profile/zed.exe" "$env:PREFIX/bin/zed.exe"
+Copy-Item "$ROOT/temp/$name/$name/target/$build_profile/cli.exe" "$env:PREFIX/bin/zed-cli.exe"
 # shortcut
 New-Item $env:PREFIX/Menu -ItemType Directory
 Copy-Item "$name.json" "$env:PREFIX/Menu"
