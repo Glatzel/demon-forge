82959a01dbb4b62b4da226416a3ae834dccf7d3a
Glatzel <chenzelin9794@gmail.com>
2025-12-31 16:46:30 +0800

    f

--- packages/ttyd/build/download-font.ps1
+++ packages/ttyd/build/download-font.ps1
@@ -3,7 +3,7 @@ gh release download -R "ryanoasis/nerd-fonts" -p "CascadiaMono.zip" `
 7z x "./temp/font.zip" "-o./temp/font"
 new-item ./html/src/style/webfont -itemtype directory
 foreach ($f in "CaskaydiaMonoNerdFont-Regular", "CaskaydiaMonoNerdFont-Italic", "CaskaydiaMonoNerdFont-Bold", "CaskaydiaMonoNerdFont-BoldItalic") {
-        pixi run pyftsubset ./temp/font/$f.ttf `
+        pyftsubset ./temp/font/$f.ttf `
                 --output-file=./html/src/style/webfont/$f.woff2 `
                 --flavor=woff2 `
                 --layout-features='*' `
