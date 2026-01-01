1dcd31a0b0861b2beb2f3125ee374753cf5ff4be
Glatzel <893016099@qq.com>
2026-01-01 19:30:25 +0800

    fix: ttyd (#610)
    Co-authored-by: pre-commit-ci-lite[bot] <117423508+pre-commit-ci-lite[bot]@users.noreply.github.com>

--- packages/ttyd/build-ttyd.ps1
+++ packages/ttyd/build-ttyd.ps1
@@ -1,13 +1,8 @@
 Set-Location $PSScriptRoot
 $ROOT = git rev-parse --show-toplevel
 . $ROOT/scripts/util.ps1
+
 $version = get-current-version
-if ($IsWindows) {
-    $env:PATH = "$env:BUILD_PREFIX/bin;$env:BUILD_PREFIX/Library/bin;$env:PATH"
-}
-if ($IsLinux) {
-    $env:PATH = "$env:BUILD_PREFIX/bin`:$env:PATH"
-}
 Set-Location $ROOT/temp/$name
 git clone https://github.com/tsl0922/ttyd.git
 Set-Location $name
@@ -19,9 +14,8 @@ get-content ./index.scss >> ./html/src/style/index.scss
 & ./download-font.ps1
 
 Set-Location ./html
-if (-not $IsWindows) {
-    npm install -g corepack
-}
+
+npm install -g corepack
 corepack enable
 corepack prepare yarn@stable --activate
 yarn install
@@ -30,12 +24,13 @@ yarn run build
 Set-Location ..
 mkdir build
 Set-Location build
-cmake `
-    -DCMAKE_INSTALL_PREFIX="$env:PREFIX" `
-    -DCMAKE_BUILD_TYPE="RELEASE" `
-  -DCMAKE_PREFIX_PATH="$BUILD_PREFIX" `
-  -DCMAKE_LIBRARY_PATH="$BUILD_PREFIX/lib"  `
-  -DCMAKE_INCLUDE_PATH="$BUILD_PREFIX/include" `
-  ..
-make
+
+if ($IsLinux) {
+    cmake `
+        -DCMAKE_INSTALL_PREFIX="$env:PREFIX" `
+        -DCMAKE_BUILD_TYPE="RELEASE" `
+        ..
+}
+
+make VERBOSE=1
 make install
