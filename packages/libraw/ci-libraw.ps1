b06505e3af991553d5acd7854b89bad53fb05207
Glatzel <chenzelin9794@gmail.com>
2026-01-01 19:49:43 +0800

    r

--- packages/libraw/ci-libraw.ps1
+++ packages/libraw/ci-libraw.ps1
@@ -2,6 +2,7 @@ Set-Location $PSScriptRoot
 $ROOT = git rev-parse --show-toplevel
 . $ROOT/scripts/util.ps1
 $latest_version = get-version-github -repo "LibRaw/LibRaw"
+$latest_version = "$latest_version".Replace("b", "")
 update-recipe -version $latest_version
 Set-Location $PSScriptRoot
 build-pkg
