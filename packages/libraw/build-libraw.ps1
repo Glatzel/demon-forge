e105255c75c0342884945ab2d29cd735f3ec5429
Glatzel <chenzelin9794@gmail.com>
2026-01-01 21:40:54 +0800

    ff

--- packages/libraw/build-libraw.ps1
+++ packages/libraw/build-libraw.ps1
@@ -5,9 +5,14 @@ if ($IsWindows) {
     $env:CMAKE_GENERATOR = "Visual Studio 17 2022"
     $env:CMAKE_GENERATOR_PLATFORM = "x64"
     $env:CMAKE_GENERATOR_TOOLSET = "v143"
+    $env:CMAKE_INSTALL_PREFIX = "$env:PREFIX/Library"
 }
 if ($IsMacOS) {
     $env:ENABLE_OPENMP = "OFF"
+    $env:CMAKE_INSTALL_PREFIX = "$env:PREFIX"
+}
+if ($IsLinux) {
+    $env:CMAKE_INSTALL_PREFIX = "$env:PREFIX"
 }
 $version = get-current-version
 Set-Location $ROOT/temp/$name
@@ -18,8 +23,5 @@ git checkout tags/$version -b "branch-$version"
 Copy-Item ../LibRaw-cmake/* ./ -Recurse
 mkdir build
 Set-Location build
-cmake `
-    -DCMAKE_INSTALL_PREFIX="$env:PREFIX" `
-    -DCMAKE_BUILD_TYPE="RELEASE" `
-    ..
+cmake -DCMAKE_BUILD_TYPE="RELEASE" ..
 cmake --build . --config Release --target install
