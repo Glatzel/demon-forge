e41f0e582b17fa407a52524688607a6f2ca28adb
Glatzel <chenzelin9794@gmail.com>
2026-01-01 12:17:07 +0800

    f

--- packages/ttyd/build-ttyd.ps1
+++ packages/ttyd/build-ttyd.ps1
@@ -2,12 +2,6 @@ Set-Location $PSScriptRoot
 $ROOT = git rev-parse --show-toplevel
 . $ROOT/scripts/util.ps1
 $version = get-current-version
-if ($IsWindows) {
-    $env:PATH = "$env:BUILD_PREFIX/bin;$env:BUILD_PREFIX/Library/bin;$env:PATH"
-}
-if ($IsLinux) {
-    $env:PATH = "$env:BUILD_PREFIX/bin`:$env:PATH"
-}
 Set-Location $ROOT/temp/$name
 git clone https://github.com/tsl0922/ttyd.git
 Set-Location $name
@@ -19,32 +13,31 @@ get-content ./index.scss >> ./html/src/style/index.scss
 & ./download-font.ps1
 
 Set-Location ./html
-if (-not $IsWindows) {
-    npm install -g corepack
-}
-corepack enable
-corepack prepare yarn@stable --activate
-yarn install
-yarn run check
-yarn run build
+
+&$env:BUILD_PREFIX/bin/npm install -g corepack
+&$env:BUILD_PREFIX/bin/corepack enable
+&$env:BUILD_PREFIX/bin/corepack prepare yarn@stable --activate
+&$env:BUILD_PREFIX/bin/yarn install
+&$env:BUILD_PREFIX/bin/yarn run check
+&$env:BUILD_PREFIX/bin/yarn run build
 Set-Location ..
 mkdir build
 Set-Location build
 
-if($IsMacOS){
-cmake `
-    -DCMAKE_INSTALL_PREFIX="$env:PREFIX" `
-    -DCMAKE_BUILD_TYPE="RELEASE" `
-    -DOPENSSL_ROOT_DIR="$env:BUILD_PREFIX" `
-    -Dlibwebsockets_DIR="$env:BUILD_PREFIX/lib/cmake/libwebsockets" `
-    ..
+if ($IsMacOS) {
+    &$env:BUILD_PREFIX/bin/cmake `
+        -DCMAKE_INSTALL_PREFIX="$env:PREFIX" `
+        -DCMAKE_BUILD_TYPE="RELEASE" `
+        -DOPENSSL_ROOT_DIR="$env:BUILD_PREFIX" `
+        -Dlibwebsockets_DIR="$env:BUILD_PREFIX/lib/cmake/libwebsockets" `
+        ..
 }
-if($IsLinux){
-cmake `
-    -DCMAKE_INSTALL_PREFIX="$env:PREFIX" `
-    -DCMAKE_BUILD_TYPE="RELEASE" `
-    ..
+if ($IsLinux) {
+    &$env:BUILD_PREFIX/bin/cmake `
+        -DCMAKE_INSTALL_PREFIX="$env:PREFIX" `
+        -DCMAKE_BUILD_TYPE="RELEASE" `
+        ..
 }
 
-make VERBOSE=1
-make install
+&$env:BUILD_PREFIX/bin/make VERBOSE=1
+&$env:BUILD_PREFIX/bin/make install
