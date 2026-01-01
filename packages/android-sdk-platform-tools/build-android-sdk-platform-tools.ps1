1d5d379ea33144a5d1e21bc0a5bf30f127418138
Glatzel <893016099@qq.com>
2026-01-01 23:12:18 +0800

    Update build-android-sdk-platform-tools.ps1

--- packages/android-sdk-platform-tools/build-android-sdk-platform-tools.ps1
+++ packages/android-sdk-platform-tools/build-android-sdk-platform-tools.ps1
@@ -5,5 +5,5 @@ aria2c -c -x16 -s16 -d "$ROOT/temp/$name/" `
     "https://googledownloads.cn/android/repository/platform-tools-latest-windows.zip" `
     -o "$name.zip"
 7z x "$ROOT/temp/$name/$name.zip" "-o$ROOT/temp/$name"
-New-Item $env:PREFIX/bin/$name -ItemType Directory
+New-Item $env:PREFIX/bin -ItemType Directory
 Copy-Item "$ROOT/temp/$name/platform-tools/*" "$env:PREFIX/bin" -recurse
