600b4d3bcc841391796a7c45286089890a94057e
Glatzel <chenzelin9794@gmail.com>
2026-01-02 08:59:23 +0800

    f

--- packages/proj/build-proj.ps1
+++ packages/proj/build-proj.ps1
@@ -2,5 +2,3 @@ Set-Location $PSScriptRoot
 $ROOT = git rev-parse --show-toplevel
 . $ROOT/scripts/util.ps1
 
-New-Item $env:PREFIX/$name -ItemType Directory
-Copy-Item "$ROOT/temp/$name/installed/*" "$env:PREFIX/$name" -Recurse
