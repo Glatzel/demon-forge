ff0de5fa9a071d1d8720169b1bea4112274ae436
Glatzel <chenzelin9794@gmail.com>
2026-01-02 10:16:43 +0800

    f

--- packages/proj/build-proj.ps1
+++ packages/proj/build-proj.ps1
@@ -2,3 +2,8 @@ Set-Location $PSScriptRoot
 $ROOT = git rev-parse --show-toplevel
 . $ROOT/scripts/util.ps1
 Set-Location $env:SRC_DIR
+
+mkdir build
+Set-Location build
+cmake -DCMAKE_BUILD_TYPE="RELEASE" ..
+cmake --build . --config Release --target install
