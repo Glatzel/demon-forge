c67bec44386e93c02b3217abac0e6bfd6fa85642
Glatzel <893016099@qq.com>
2026-01-02 10:49:01 +0800

    fix: termscp (#613)

--- packages/termscp/build-termscp.ps1
+++ packages/termscp/build-termscp.ps1
@@ -1,4 +1,19 @@
 Set-Location $PSScriptRoot
 $ROOT = git rev-parse --show-toplevel
 . $ROOT/scripts/util.ps1
-build-cargo-package $name $name
+if ($env:DIST_BUILD) {
+    cargo install $name --root $env:PREFIX --locked --force `
+        --config 'profile.release.codegen-units=1' `
+        --config 'profile.release.debug=false' `
+        --config 'profile.release.lto="fat"' `
+        --config 'profile.release.opt-level=3' `
+        --config 'profile.release.strip=true'
+}
+else {
+    cargo install $name --root $env:PREFIX --locked --force `
+        --config 'profile.release.opt-level=0' `
+        --config 'profile.release.debug=false' `
+        --config 'profile.release.codegen-units=256' `
+        --config 'profile.release.lto="off"' `
+        --config 'profile.release.strip=false'
+}
