f9be2a1e6333c3ce761667f73cbdc4dd1986ea66
Glatzel <chenzelin9794@gmail.com>
2026-01-02 10:34:59 +0800

    f

--- packages/proj/build-proj.ps1
+++ packages/proj/build-proj.ps1
@@ -3,6 +3,18 @@ $ROOT = git rev-parse --show-toplevel
 . $ROOT/scripts/util.ps1
 Set-Location $env:SRC_DIR
 
+if ($IsWindows) {
+    $env:CMAKE_GENERATOR = "Visual Studio 17 2022"
+    $env:CMAKE_GENERATOR_PLATFORM = "x64"
+    $env:CMAKE_GENERATOR_TOOLSET = "v143"
+    $env:CMAKE_INSTALL_PREFIX = "$env:PREFIX/Library"
+}
+if ($IsMacOS) {
+    $env:CMAKE_INSTALL_PREFIX = "$env:PREFIX"
+}
+if ($IsLinux) {
+    $env:CMAKE_INSTALL_PREFIX = "$env:PREFIX"
+}
 mkdir build
 Set-Location build
 cmake -DCMAKE_BUILD_TYPE="RELEASE" ..
