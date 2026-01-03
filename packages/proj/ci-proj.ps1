f679dab22be4796d8a27d95d4d4c12b9d8b26c38
Glatzel <chenzelin9794@gmail.com>
2026-01-03 10:41:41 +0800

    f

--- packages/proj/ci-proj.ps1
+++ packages/proj/ci-proj.ps1
@@ -5,9 +5,8 @@ $latest_version = get-version-vcpkg -name $name
 update-recipe -version $latest_version
 update-vcpkg-json -file $PSScriptRoot/build/vcpkg.json -name $name -version $latest_version
 if ($IsLinux) {
-    dnf groupinstall "Development Tools" -y
     dnf update -y
-    dnf install -y gcc-toolset-10-gcc gcc-toolset-10-gcc-c++
+    dnf install -y gcc-toolset-10-gcc gcc-toolset-10-gcc-c++ cmake
 }
 Copy-Item $PSScriptRoot/build/* $ROOT/temp/$name/ -Recurse
 & $ROOT/temp/$name/vcpkg-setup.ps1
