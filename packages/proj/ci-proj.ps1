652de7c47a8a6bb893c665fc29aacf98ac18d1e8
Glatzel <893016099@qq.com>
2026-01-03 12:21:33 +0800

    fix: build proj on linux (#636)
    Co-authored-by: pre-commit-ci-lite[bot] <117423508+pre-commit-ci-lite[bot]@users.noreply.github.com>

--- packages/proj/ci-proj.ps1
+++ packages/proj/ci-proj.ps1
@@ -5,9 +5,14 @@ $latest_version = get-version-vcpkg -name $name
 update-recipe -version $latest_version
 update-vcpkg-json -file $PSScriptRoot/build/vcpkg.json -name $name -version $latest_version
 if ($IsLinux) {
-    dnf groupinstall "Development Tools" -y
     dnf update -y
     dnf install -y gcc-toolset-10-gcc gcc-toolset-10-gcc-c++
+    $env:PATH = "/opt/rh/gcc-toolset-10/root/usr/bin`:$env:PATH"
+    $env:LD_LIBRARY_PATH = "/opt/rh/gcc-toolset-10/root/usr/lib64`:$env:LD_LIBRARY_PATH"
+    $env:CXX = "g++"
+    $env:CC = "gcc"
+    gcc --version
+    g++ --version
 }
 Copy-Item $PSScriptRoot/build/* $ROOT/temp/$name/ -Recurse
 & $ROOT/temp/$name/vcpkg-setup.ps1
