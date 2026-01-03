6b4443ecd8d89ce1cf85aefe93480c73b961ffed
Glatzel <chenzelin9794@gmail.com>
2026-01-03 10:44:56 +0800

    f

--- packages/proj/ci-proj.ps1
+++ packages/proj/ci-proj.ps1
@@ -6,7 +6,7 @@ update-recipe -version $latest_version
 update-vcpkg-json -file $PSScriptRoot/build/vcpkg.json -name $name -version $latest_version
 if ($IsLinux) {
     dnf update -y
-    dnf install -y gcc-toolset-10-gcc gcc-toolset-10-gcc-c++ cmake
+    dnf install -y gcc-toolset-10-gcc gcc-toolset-10-gcc-c++ cmake make
 }
 Copy-Item $PSScriptRoot/build/* $ROOT/temp/$name/ -Recurse
 & $ROOT/temp/$name/vcpkg-setup.ps1
