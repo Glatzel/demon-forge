0fa064ea2456ca5bc627e0bfc8921a692ec27f32
Glatzel <893016099@qq.com>
2026-01-04 11:28:16 +0800

    Update ci-fornax-py.ps1

--- packages/fornax-py/ci-fornax-py.ps1
+++ packages/fornax-py/ci-fornax-py.ps1
@@ -12,11 +12,6 @@ git checkout tags/"v$latest_version" -b "$latest_version-branch"
 (Get-Content -Path "./scripts/setup.ps1") -replace "sudo ", "" | Set-Content -Path "./scripts/setup.ps1"
 & ./scripts/setup.ps1
 Set-Location ./crates/fornax-py
-if ($env:DIST_BUILD) {
-    pixi run maturin build --out ./dist --profile release
-}
-else {
-    pixi run maturin build --out ./dist
-}
+pixi run maturin build --out ./dist --profile release
 Set-Location $PSScriptRoot
 build-pkg
