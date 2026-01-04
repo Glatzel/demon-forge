42c7fe5b0dec2bce3892ef8fe6f92d29fa015b25
Glatzel <893016099@qq.com>
2026-01-04 11:27:48 +0800

    Update build-mason.ps1

--- packages/mason/build-mason.ps1
+++ packages/mason/build-mason.ps1
@@ -7,11 +7,7 @@ Set-Location $ROOT/temp/$name
 gh repo clone Glatzel/$name
 Set-Location $name
 git checkout tags/"v$version" -b "branch-$version"
-if ($env:DIST_BUILD) {
+
     & ./scripts/build.ps1 -Release
-}
-else {
-    & ./scripts/build.ps1
-}
 New-Item $env:PREFIX/bin/$name -ItemType Directory
 Copy-Item "$ROOT/temp/$name/$name/bin/Mason/*" "$env:PREFIX/bin/$name" -Recurse
