6e0848a92b393bd8585bba55fed8879485390e46
Glatzel <chenzelin9794@gmail.com>
2026-01-04 16:53:39 +0800

    f

--- packages/pyxis-cli/build-pyxis-cli.ps1
+++ packages/pyxis-cli/build-pyxis-cli.ps1
@@ -1,15 +1,19 @@
 Set-Location $PSScriptRoot
 $ROOT = git rev-parse --show-toplevel
 . $ROOT/scripts/util.ps1
-
 Set-Location $env:SRC_DIR/rust
+if ($IsLinux) {
+    dnf update -y
+    dnf install -y systemd-devel
+    $env:PKG_CONFIG_PATH = "/usr/lib64/pkgconfig`:$env:PKG_CONFIG_PATH"
+}
 if ($IsWindows) {
     $bin = Resolve-Path $PSScriptRoot/../.pixi/envs/default/Library/bin
     $env:Path = "$bin" + ";" + "$env:Path"
     $env:PKG_CONFIG_PATH = "$(Resolve-Path $env:BUILD_PREFIX/proj/x64-windows-static/lib/pkgconfig);${env:PKG_CONFIG_PATH}"
 }
 if ($IsMacOS) {
-    $env:PKG_CONFIG_PATH = "$(Resolve-Path $$env:BUILD_PREFIX/proj/arm64-osx-release/lib/pkgconfig)`:${env:PKG_CONFIG_PATH}"
+    $env:PKG_CONFIG_PATH = "$(Resolve-Path $env:BUILD_PREFIX/proj/arm64-osx-release/lib/pkgconfig)`:${env:PKG_CONFIG_PATH}"
 }
 if ($IsLinux -and ($(uname -m) -eq 'x86_64' )) {
     $env:PKG_CONFIG_PATH = "$(Resolve-Path $env:BUILD_PREFIX/proj/x64-linux-release/lib/pkgconfig)`:/usr/lib/x86_64-linux-gnu/pkgconfig`:${env:PKG_CONFIG_PATH}"
