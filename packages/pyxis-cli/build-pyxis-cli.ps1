19995fcc0051c65ff7472b7f2584a190f5fcc33e
Glatzel <chenzelin9794@gmail.com>
2026-01-04 16:20:24 +0800

    f

--- packages/pyxis-cli/build-pyxis-cli.ps1
+++ packages/pyxis-cli/build-pyxis-cli.ps1
@@ -2,13 +2,13 @@ Set-Location $PSScriptRoot
 $ROOT = git rev-parse --show-toplevel
 . $ROOT/scripts/util.ps1
 
+Set-Location $env:SRC_DIR/rust
+& ./scripts/setup.ps1
 if ($IsLinux) {
     dnf update -y
     dnf install -y systemd-devel
     $env:PKG_CONFIG_PATH = "/usr/lib64/pkgconfig`:$env:PKG_CONFIG_PATH"
 }
-Set-Location $env:SRC_DIR/rust
-& ./scripts/setup.ps1
 cargo build --bin pyxis --release
 New-Item $env:PREFIX/bin -ItemType Directory
 if ($IsWindows) {
