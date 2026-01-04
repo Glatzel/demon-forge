5daf817845f0c8c0e763290036637348b9c40939
Glatzel <chenzelin9794@gmail.com>
2026-01-04 13:13:45 +0800

    f

--- packages/pyxis-cli/build-pyxis-cli.ps1
+++ packages/pyxis-cli/build-pyxis-cli.ps1
@@ -3,6 +3,9 @@ $ROOT = git rev-parse --show-toplevel
 . $ROOT/scripts/util.ps1
 
 install-rust
+if ($IsLinux) {
+    dnf install -y systemd-devel
+}
 Set-Location $env:SRC_DIR/rust
 & ./scripts/setup.ps1
 cargo build --bin pyxis --release
