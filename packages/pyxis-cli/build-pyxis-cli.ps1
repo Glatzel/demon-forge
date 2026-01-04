ba0e89ee0741f80374e8e9d9fae10ec5117eaed2
Glatzel <893016099@qq.com>
2026-01-04 11:29:08 +0800

    Update build-pyxis-cli.ps1

--- packages/pyxis-cli/build-pyxis-cli.ps1
+++ packages/pyxis-cli/build-pyxis-cli.ps1
@@ -3,15 +3,9 @@ $ROOT = git rev-parse --show-toplevel
 . $ROOT/scripts/util.ps1
 
 New-Item $env:PREFIX/bin -ItemType Directory
-if ($env:DIST_BUILD) {
-    $config = "release"
-}
-else {
-    $config = "debug"
-}
 if ($IsWindows) {
-    Copy-Item $ROOT/temp/$name/pyxis/rust/target/$config/pyxis.exe "$env:PREFIX/bin/"
+    Copy-Item $ROOT/temp/$name/pyxis/rust/target/release/pyxis.exe "$env:PREFIX/bin/"
 }
 else {
-    Copy-Item $ROOT/temp/$name/pyxis/rust/target/$config/pyxis "$env:PREFIX/bin/"
+    Copy-Item $ROOT/temp/$name/pyxis/rust/target/release/pyxis "$env:PREFIX/bin/"
 }
