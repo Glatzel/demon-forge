7eb168fc97acd83abf57adf6356b784db7afb9f0
Glatzel <893016099@qq.com>
2026-01-05 08:44:09 +0800

    更新 build-termscp.ps1

--- packages/termscp/build-termscp.ps1
+++ packages/termscp/build-termscp.ps1
@@ -1,4 +1,8 @@
 Set-Location $PSScriptRoot
 $ROOT = git rev-parse --show-toplevel
 . $ROOT/scripts/util.ps1
+$JITTER = "$env:BUILD_PREFIX\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\aws-lc-sys-0.35.0\aws-lc\third_party\jitterentropy\jitterentropy-library"
+
+$env:CFLAGS   = "/I`"$JITTER`""
+$env:CXXFLAGS = $env:CFLAGS
 build-cargo-package $name $name
