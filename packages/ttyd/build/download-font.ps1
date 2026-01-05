1c018e806ddfb5232d83a049eadc61bc69770d45
Glatzel <chenzelin9794@gmail.com>
2026-01-05 19:25:49 +0800

    f

--- packages/ttyd/build/download-font.ps1
+++ packages/ttyd/build/download-font.ps1
@@ -1,7 +1,6 @@
 gh release download -R "ryanoasis/nerd-fonts" -p "CascadiaMono.zip" `
-        -O  ./temp/font.zip
-7z x "temp/font.zip" "-o./temp/font"
-pixi install
+        -O  ./temp/font.zip --clobber
+7z x "./temp/font.zip" "-o./temp/font"
 new-item ./html/src/style/webfont -itemtype directory
 foreach ($f in "CaskaydiaMonoNerdFont-Regular", "CaskaydiaMonoNerdFont-Italic", "CaskaydiaMonoNerdFont-Bold", "CaskaydiaMonoNerdFont-BoldItalic") {
         pyftsubset ./temp/font/$f.ttf `
