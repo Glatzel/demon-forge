41dc2a0dce18a6ad03ae7e1d4cd195fd06927ca0
Glatzel <chenzelin9794@gmail.com>
2026-01-07 13:17:19 +0800

    f

--- packages/ttyd/build-ttyd.ps1
+++ packages/ttyd/build-ttyd.ps1
@@ -4,7 +4,7 @@ $ROOT = git rev-parse --show-toplevel
 Set-Location $env:SRC_DIR
 
 copy-item $PSScriptRoot/build/* ./ -recurse
-git apply config.patch
+git apply --verbose config.patch
 get-content ./index.scss >> ./html/src/style/index.scss
 & ./download-font.ps1
 
