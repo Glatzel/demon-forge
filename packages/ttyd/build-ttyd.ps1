4e6791ff9275cdc27a38fd5529606cfc6795c9cd
Glatzel <chenzelin9794@gmail.com>
2026-01-07 13:33:10 +0800

    f

--- packages/ttyd/build-ttyd.ps1
+++ packages/ttyd/build-ttyd.ps1
@@ -10,7 +10,7 @@ get-content ./index.scss >> ./html/src/style/index.scss
 
 Set-Location ./html
 if ($IsWindows) {
-    Remove-Item   C:/npm/prefix/yarn/ -Recurse -Force
+    Remove-Item C:/npm/*
 }
 npm install -g corepack
 corepack enable
