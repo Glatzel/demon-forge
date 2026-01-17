e60384f045ef09a0d49c2b5b5be85a6849e49b1b
Glatzel <chenzelin9794@gmail.com>
2026-01-17 11:40:47 +0800

    f

--- packages/nerdctl-full/build-nerdctl-full.ps1
+++ packages/nerdctl-full/build-nerdctl-full.ps1
@@ -6,7 +6,7 @@ if ($IsLinux -and $arch -eq "Arm64") {
     gh release download -R "containerd/nerdctl" -p "${env:PKG_NAME}-*.*.*-linux-arm64.tar.gz" `
         -O  ./${env:PKG_NAME}.tar.gz --clobber
 }
-7z x ./${env:PKG_NAME}.tar.gz "-o./"
-7z x ./*.tar "-o./${env:PKG_NAME}"Copy-Item "./${env:PKG_NAME}/*" "$env:PREFIX/" -Recurse
+7z x ${env:PKG_NAME}.tar.gz "-o./"
+7z x *.tar "-o./${env:PKG_NAME}"Copy-Item "./${env:PKG_NAME}/*" "$env:PREFIX/" -Recurse
 # do not use bundled runc
 Remove-Item "$env:PREFIX/bin/runc"
--- packages/nerdctl/build-nerdctl.ps1
+++ packages/nerdctl/build-nerdctl.ps1
@@ -10,7 +10,7 @@ if ($IsLinux -and $arch -eq "Arm64") {
     gh release download -R "containerd/nerdctl" -p "nerdctl-?.*.*-linux-arm64.tar.gz" `
         -O  ./${env:PKG_NAME}.tar.gz --clobber
 }
-7z x ./${env:PKG_NAME}.tar.gz "-o./"
-7z x ./${env:PKG_NAME}.tar "-o./${env:PKG_NAME}"
+7z x ${env:PKG_NAME}.tar.gz "-o./"
+7z x ${env:PKG_NAME}.tar "-o./${env:PKG_NAME}"
 New-Item $env:PREFIX/bin -ItemType Directory
 Copy-Item "./${env:PKG_NAME}/*" "$env:PREFIX/bin/" -Recurse
