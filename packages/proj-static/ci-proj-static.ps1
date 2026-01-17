9725a645fc4ea10fea369de1a711bc90aaab90d1
Glatzel <893016099@qq.com>
2026-01-17 19:19:28 +0800

    build: rename proj, libraw (#688)

--- packages/proj-static/ci-proj-static.ps1
+++ packages/proj-static/ci-proj-static.ps1
@@ -1,8 +1,8 @@
 Set-Location $PSScriptRoot
 $ROOT = git rev-parse --show-toplevel
 . $ROOT/scripts/util.ps1
-$latest_version = get-version-github -repo "LibRaw/LibRaw"
-$latest_version = "$latest_version".Replace("b", "")
+$latest_version = get-version-vcpkg -name proj
 update-recipe -version $latest_version
+update-vcpkg-json -file $PSScriptRoot/build/vcpkg.json -name proj -version $latest_version
 Set-Location $PSScriptRoot
 build-pkg
--- packages/proj/build-proj.ps1
+++ packages/proj/build-proj.ps1
@@ -1,5 +1,30 @@
-Copy-Item $PSScriptRoot/build/* ./ -Recurse
-& ./vcpkg-setup.ps1
-& ./vcpkg-install.ps1
-New-Item $env:PREFIX/${env:PKG_NAME} -ItemType Directory
-Copy-Item "./${env:PKG_NAME}/installed/*" "$env:PREFIX/${env:PKG_NAME}" -Recurse
+Set-Location $PSScriptRoot
+$ROOT = git rev-parse --show-toplevel
+. $ROOT/scripts/util.ps1
+Set-Location $env:SRC_DIR
+
+if ($IsWindows) {
+    $env:CMAKE_GENERATOR = "Visual Studio 17 2022"
+    $env:CMAKE_GENERATOR_PLATFORM = "x64"
+    $env:CMAKE_GENERATOR_TOOLSET = "v143"
+    $env:CMAKE_INSTALL_PREFIX = "$env:PREFIX/Library"
+}
+if ($IsMacOS) {
+    $env:CMAKE_INSTALL_PREFIX = "$env:PREFIX"
+}
+if ($IsLinux) {
+    $env:CMAKE_INSTALL_PREFIX = "$env:PREFIX"
+}
+
+cmake -S . -B build `
+    -DCMAKE_BUILD_TYPE="RELEASE" `
+    -DBUILD_APPS=ON `
+    -DBUILD_SHARED_LIBS=ON `
+    -DBUILD_TESTING=OFF `
+    -DENABLE_IPO=ON `
+    -DENABLE_CURL=ON `
+    -DENABLE_TIFF=ON `
+    -DEMBED_PROJ_DATA_PATH=OFF `
+    -DEMBED_RESOURCE_FILES=ON `
+    ..
+cmake --build build --config Release --target install
