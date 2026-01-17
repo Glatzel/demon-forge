a2587332eeb68fb65cbecceb115d1e1c4338f73e
Glatzel <chenzelin9794@gmail.com>
2026-01-17 12:49:41 +0800

    f

--- packages/toolbox/build-toolbox.ps1
+++
@@ -1,6 +0,0 @@
-$version = $env:PKG_VERSION
-Set-Location .
-gh repo clone Glatzel/${env:PKG_NAME}
-Set-Location ${env:PKG_NAME}
-git checkout tags/"v$version" -b "branch-$version"
-pip install ./${env:PKG_NAME}/python -v
