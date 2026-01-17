3d28961e6233473688420b8278300a7040d48616
Glatzel <chenzelin9794@gmail.com>
2026-01-17 11:35:57 +0800

    f

--- packages/imagemagick/build-imagemagick.ps1
+++ packages/imagemagick/build-imagemagick.ps1
@@ -5,4 +5,4 @@ gh release download `
     --clobber
 7z x "${env:PKG_NAME}.7z" "-o./${env:PKG_NAME}"
 New-Item $env:PREFIX/bin/${env:PKG_NAME} -ItemType Directory
-Copy-Item "$env:RECIPE_DIR/../../temp/${env:PKG_NAME}/${env:PKG_NAME}/*" "$env:PREFIX/bin/${env:PKG_NAME}" -Recurse
+Copy-Item "./${env:PKG_NAME}/*" "$env:PREFIX/bin/${env:PKG_NAME}" -Recurse
