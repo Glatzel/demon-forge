8a780fab6f7a75cc3b6c8f12dad876c2aeb069fa
Glatzel <893016099@qq.com>
2026-01-17 22:24:49 +0800

    feat: remove proj static (#689)

--- packages/proj-static/build-proj-static.ps1
+++
@@ -1,7 +0,0 @@
-$ROOT = git rev-parse --show-toplevel
-. $ROOT/scripts/util.ps1
-Copy-Item $PSScriptRoot/build/* ./ -Recurse
-& ./vcpkg-setup.ps1
-& ./vcpkg-install.ps1
-New-Item $env:PREFIX/proj -ItemType Directory
-Copy-Item "./installed/*" "$env:PREFIX/proj" -Recurse
