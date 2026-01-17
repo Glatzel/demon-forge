998345f7e9f0c7ee3f532976078ddd68b7f291c9
Glatzel <chenzelin9794@gmail.com>
2026-01-17 13:26:02 +0800

    f

--- packages/openimageio/build-openimageio.ps1
+++ packages/openimageio/build-openimageio.ps1
@@ -1,9 +1,9 @@
-Copy-Item $PSScriptRoot/build/* ./ -Recurse
-& ./scripts/pixi-setup.ps1
-& ./scripts/clone-repo.ps1
-& ./scripts/vcpkg-setup.ps1
-& ./scripts/vcpkg-install.ps1
-& ./scripts/build-oiio.ps1
-& ./scripts/copy-item.ps1
+Copy-Item $PSScriptRoot/build/* $env:SRC_DIR -Recurse
+& $env:SRC_DIR/scripts/pixi-setup.ps1
+& $env:SRC_DIR/scripts/clone-repo.ps1
+& $env:SRC_DIR/scripts/vcpkg-setup.ps1
+& $env:SRC_DIR/scripts/vcpkg-install.ps1
+& $env:SRC_DIR/scripts/build-oiio.ps1
+& $env:SRC_DIR/scripts/copy-item.ps1
 New-Item $env:PREFIX/bin/openimageio -ItemType Directory
 Copy-Item "./${env:PKG_NAME}/dist/*" "$env:PREFIX/bin/${env:PKG_NAME}"
