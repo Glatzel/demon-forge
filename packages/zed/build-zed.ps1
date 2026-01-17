b724d73aaf560cdb86c10c153da4222a420e1ca8
Glatzel <chenzelin9794@gmail.com>
2026-01-17 13:05:34 +0800

    f

--- packages/zed/build-zed.ps1
+++ packages/zed/build-zed.ps1
@@ -1,4 +1,6 @@
-Set-Location $PSScriptRootNew-Item $env:PREFIX/bin -ItemType DirectoryCopy-Item "$ROOT/temp/$env:PKG_NAME/$env:PKG_NAME/target/release/zed.exe" "$env:PREFIX/bin/zed.exe"
+Set-Location $PSScriptRoot
+New-Item $env:PREFIX/bin -ItemType Directory
+Copy-Item "$ROOT/temp/$env:PKG_NAME/$env:PKG_NAME/target/release/zed.exe" "$env:PREFIX/bin/zed.exe"
 Copy-Item "$ROOT/temp/$env:PKG_NAME/$env:PKG_NAME/target/release/cli.exe" "$env:PREFIX/bin/zed-cli.exe"# shortcut
 New-Item $env:PREFIX/Menu -ItemType Directory
 Copy-Item "$env:RECIPE_DIR/$env:PKG_NAME.json" "$env:PREFIX/Menu"
