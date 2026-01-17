084430f9b557f77aaf0e347b1f0f74dde1e9af16
Glatzel <893016099@qq.com>
2026-01-17 14:18:35 +0800

    fix: ttyd (#678)

--- packages/ttyd/build-ttyd.ps1
+++ packages/ttyd/build-ttyd.ps1
@@ -1,6 +1,8 @@
-Set-Location $PSScriptRootNew-Item $env:PREFIX/bin -ItemType Directory
+$ROOT = git rev-parse --show-toplevel
+. $ROOT/scripts/util.ps1
+New-Item $env:PREFIX/bin -ItemType Directory
 switch ($env:TARGET_PLATFORM) {
-    "win-64" { copy-item $ROOT/temp/${env:PKG_NAME}/${env:PKG_NAME}/build/${env:PKG_NAME}.exe $env:PREFIX/bin/${env:PKG_NAME}.exe }
-    "linux-64" { copy-item $ROOT/temp/${env:PKG_NAME}/${env:PKG_NAME}/build/${env:PKG_NAME} $env:PREFIX/bin/${env:PKG_NAME} }
-    "linux-aarch64" { copy-item $ROOT/temp/${env:PKG_NAME}/${env:PKG_NAME}/build/${env:PKG_NAME} $env:PREFIX/bin/${env:PKG_NAME} }
+    "win-64" { copy-item $ROOT/temp/$env:PKG_NAME/$env:PKG_NAME/build/$env:PKG_NAME.exe $env:PREFIX/bin/$env:PKG_NAME.exe }
+    "linux-64" { copy-item $ROOT/temp/$env:PKG_NAME/$env:PKG_NAME/build/$env:PKG_NAME $env:PREFIX/bin/$env:PKG_NAME }
+    "linux-aarch64" { copy-item $ROOT/temp/$env:PKG_NAME/$env:PKG_NAME/build/$env:PKG_NAME $env:PREFIX/bin/$env:PKG_NAME }
 }
