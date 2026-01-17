71469f1fb5f395d93c673243e2286b53999b8e98
Glatzel <chenzelin9794@gmail.com>
2026-01-17 13:38:18 +0800

    f

--- packages/pureref/build-pureref.ps1
+++ packages/pureref/build-pureref.ps1
@@ -3,7 +3,7 @@ $ROOT = git rev-parse --show-toplevel
 python $env:RECIPE_DIR/download.py
 7z x "*.zip"
 New-Item $env:PREFIX/bin/${env:PKG_NAME} -ItemType Directory
-Copy-Item "./${env:PKG_NAME}/Pureref*/*" "$env:PREFIX/bin/${env:PKG_NAME}" -Recurse
+Copy-Item "./Pureref*/*" "$env:PREFIX/bin/${env:PKG_NAME}" -Recurse
 # shortcut
 New-Item $env:PREFIX/Menu -ItemType Directory
 Copy-Item "${env:RECIPE_DIR}/${env:PKG_NAME}.json" "$env:PREFIX/Menu"
