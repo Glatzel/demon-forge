b0f2ad70117018a04c02694220de19183090a317
Glatzel <893016099@qq.com>
2026-01-17 15:45:13 +0800

    feat: add opencolorio (#682)
    Co-authored-by: pre-commit-ci-lite[bot] <117423508+pre-commit-ci-lite[bot]@users.noreply.github.com>

---
+++ packages/opencolorio/build-opencolorio.ps1
@@ -0,0 +1,23 @@
+if ($IsWindows) {
+    $env:CMAKE_INSTALL_PREFIX = "$ENV:PREFIX/Library"
+}
+else {
+    $env:CMAKE_INSTALL_PREFIX = "$ENV:PREFIX"
+}
+
+cmake -S . -B build `
+    -DCMAKE_BUILD_TYPE=Release `
+    -DOCIO_INSTALL_EXT_PACKAGES=MISSING `
+    -DCMAKE_BUILD_TYPE=Release `
+    -DBUILD_SHARED_LIBS=ON `
+    -DOCIO_BUILD_APPS=OFF `
+    -DOCIO_USE_OIIO_FOR_APPS=OFF `
+    -DOCIO_BUILD_PYTHON=OFF `
+    -DOCIO_BUILD_OPENFX=OFF `
+    -DOCIO_USE_SIMD=ON `
+    -DOCIO_BUILD_TESTS=OFF `
+    -DOCIO_BUILD_GPU_TESTS=OFF `
+    -DOCIO_USE_HEADLESS=OFF `
+    -DOCIO_WARNING_AS_ERROR=OFF `
+    -DOCIO_BUILD_DOCS=OFF
+cmake --build build --config Release --target install
