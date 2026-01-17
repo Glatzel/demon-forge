f71d854e3d468be1d97f016f31bcadfe6c49cdfa
Glatzel <chenzelin9794@gmail.com>
2026-01-17 14:05:22 +0800

    f

--- packages/fornax-py/build-fornax-py.ps1
+++ packages/fornax-py/build-fornax-py.ps1
@@ -1,7 +1,6 @@
-
 & ./scripts/setup.ps1
 Set-Location ./crates/fornax-py
-pixi run maturin build --out ./dist --profile release
+maturin build --out ./dist --profile release
 foreach ($whl in Get-ChildItem "./dist/*.whl") {
     pip install "$whl" -v
 }
