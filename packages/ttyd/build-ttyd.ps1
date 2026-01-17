cb424f49129830f11f405b439c43f688d6abb3ce
Glatzel <chenzelin9794@gmail.com>
2026-01-17 13:22:31 +0800

    f

--- packages/ttyd/build-ttyd.ps1
+++ packages/ttyd/build-ttyd.ps1
@@ -1,4 +1,5 @@
-Set-Location $PSScriptRoot
+$ROOT = git rev-parse --show-toplevel
+. $ROOT/scripts/util.ps1
 New-Item $env:PREFIX/bin -ItemType Directory
 switch ($env:TARGET_PLATFORM) {
     "win-64" { copy-item $ROOT/temp/$env:PKG_NAME/$env:PKG_NAME/build/$env:PKG_NAME.exe $env:PREFIX/bin/$env:PKG_NAME.exe }
