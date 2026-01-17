ab63acc60db323e61321afd76c1bf9e6fa6a5198
Glatzel <chenzelin9794@gmail.com>
2026-01-17 18:54:47 +0800

    v

--- packages/libraw-static/build-libraw-static.ps1
+++ packages/libraw-static/build-libraw-static.ps1
@@ -1,5 +1,6 @@
 Copy-Item $PSScriptRoot/build/* ./ -Recurse
-& .//vcpkg-setup.ps1
-& .//vcpkg-install.ps1Set-Location $PSScriptRoot
+& ./vcpkg-setup.ps1
+& ./vcpkg-install.ps1
+Set-Location $PSScriptRoot
 New-Item $env:PREFIX/libraw -ItemType Directory
 Copy-Item "./libraw/installed/*" "$env:PREFIX/libraw" -Recurse
