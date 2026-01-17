7bd05b4c5d53c120312e9d5853cd820731321cf4
Glatzel <chenzelin9794@gmail.com>
2026-01-17 13:12:09 +0800

    g

--- packages/ttyd/build-ttyd.ps1
+++ packages/ttyd/build-ttyd.ps1
@@ -1,4 +1,5 @@
-Set-Location $PSScriptRootNew-Item $env:PREFIX/bin -ItemType Directory
+Set-Location $PSScriptRoot
+New-Item $env:PREFIX/bin -ItemType Directory
 switch ($env:TARGET_PLATFORM) {
     "win-64" { copy-item $ROOT/temp/$env:PKG_NAME/$env:PKG_NAME/build/$env:PKG_NAME.exe $env:PREFIX/bin/$env:PKG_NAME.exe }
     "linux-64" { copy-item $ROOT/temp/$env:PKG_NAME/$env:PKG_NAME/build/$env:PKG_NAME $env:PREFIX/bin/$env:PKG_NAME }
