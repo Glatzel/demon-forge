a6b6de60648502ce943cd3c0c459e087f4c23b17
Glatzel <chenzelin9794@gmail.com>
2026-01-17 13:43:40 +0800

    f

--- packages/sony-camera-remote-sdk/build-sony-camera-remote-sdk.ps1
+++ packages/sony-camera-remote-sdk/build-sony-camera-remote-sdk.ps1
@@ -10,7 +10,7 @@ foreach ($f in Get-ChildItem "./*.zip") {
     7z x "$f" "-ounzip/$platform"
 }
 New-Item "$env:PREFIX/${env:PKG_NAME}" -ItemType Directory
-Copy-Item "./${env:PKG_NAME}/unzip/$platform/*" "$env:PREFIX/${env:PKG_NAME}" -RecurseRemove-Item $env:PREFIX/${env:PKG_NAME}/app/*.h
+Copy-Item "./unzip/$platform/*" "$env:PREFIX/${env:PKG_NAME}" -RecurseRemove-Item $env:PREFIX/${env:PKG_NAME}/app/*.h
 Remove-Item $env:PREFIX/${env:PKG_NAME}/app/*.cpp
 Remove-Item $env:PREFIX/${env:PKG_NAME}/*.zip
 Remove-Item $env:PREFIX/${env:PKG_NAME}/external/opencv/ -Recurse
