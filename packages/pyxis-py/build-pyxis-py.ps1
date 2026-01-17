397e597ddc76241a1cbadec6abc24063668ac4bb
Glatzel <chenzelin9794@gmail.com>
2026-01-17 13:43:00 +0800

    f

--- packages/pyxis-py/build-pyxis-py.ps1
+++ packages/pyxis-py/build-pyxis-py.ps1
@@ -1,5 +1,5 @@
 Set-Location python
-pixi run maturin build --out ./dist --profile release
+maturin build --out ./dist --profile release
 foreach ($whl in Get-ChildItem "./dist/*.whl") {
     pip install "$whl" -v
 }
