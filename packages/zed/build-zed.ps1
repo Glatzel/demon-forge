a2069343437f4e1acadb468de529c1bb4a5cfcd6
Glatzel <893016099@qq.com>
2026-01-17 18:04:09 +0800

    fix: Zed (#679)
    Co-authored-by: pre-commit-ci-lite[bot] <117423508+pre-commit-ci-lite[bot]@users.noreply.github.com>

--- packages/zed/build-zed.ps1
+++ packages/zed/build-zed.ps1
@@ -1,5 +1,9 @@
-Set-Location $PSScriptRootNew-Item $env:PREFIX/bin -ItemType DirectoryCopy-Item "$ROOT/temp/$env:PKG_NAME/$env:PKG_NAME/target/release/zed.exe" "$env:PREFIX/bin/zed.exe"
-Copy-Item "$ROOT/temp/$env:PKG_NAME/$env:PKG_NAME/target/release/cli.exe" "$env:PREFIX/bin/zed-cli.exe"# shortcut
+$ROOT = git rev-parse --show-toplevel
+. $ROOT/scripts/util.ps1
+New-Item $env:PREFIX/bin -ItemType Directory
+Copy-Item "$ROOT/temp/$env:PKG_NAME/$env:PKG_NAME/target/release/zed.exe" "$env:PREFIX/bin/zed.exe"
+Copy-Item "$ROOT/temp/$env:PKG_NAME/$env:PKG_NAME/target/release/cli.exe" "$env:PREFIX/bin/zed-cli.exe"
+# shortcut
 New-Item $env:PREFIX/Menu -ItemType Directory
 Copy-Item "$env:RECIPE_DIR/$env:PKG_NAME.json" "$env:PREFIX/Menu"
 if ($IsWindows) {
