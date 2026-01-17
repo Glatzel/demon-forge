b2a8ec35cee3f365a7422dfd2ef70150de8c7cd8
Glatzel <893016099@qq.com>
2026-01-17 16:57:26 +0800

    build: use conda to build openimageio (#683)

--- packages/openimageio/build-openimageio.ps1
+++ packages/openimageio/build-openimageio.ps1
@@ -1,9 +1,44 @@
-Copy-Item $PSScriptRoot/build/* $env:SRC_DIR -Recurse
-& $env:SRC_DIR/scripts/pixi-setup.ps1
-& $env:SRC_DIR/scripts/clone-repo.ps1
-& $env:SRC_DIR/scripts/vcpkg-setup.ps1
-& $env:SRC_DIR/scripts/vcpkg-install.ps1
-& $env:SRC_DIR/scripts/build-oiio.ps1
-& $env:SRC_DIR/scripts/copy-item.ps1
-New-Item $env:PREFIX/bin/openimageio -ItemType Directory
-Copy-Item "./dist/*" "$env:PREFIX/bin/${env:PKG_NAME}"
+$ROOT = git rev-parse --show-toplevel
+. $ROOT/scripts/util.ps1
+
+# Common CMake options
+$cmakeArgs = @(
+    "-DVERBOSE=ON"
+    "-DCMAKE_BUILD_TYPE=Release"
+    "-DBUILD_DOCS=0"
+    "-DBUILD_SHARED_LIBS=1"
+    "-DENABLE_DCMTK=0"
+    "-DENABLE_FFmpeg=0"
+    "-DENABLE_INSTALL_testtex=0"
+    "-DENABLE_libuhdr=0"
+    "-DENABLE_Nuke=0"
+    "-DENABLE_OpenCV=0"
+    "-DENABLE_OpenVDB=0"
+    "-DENABLE_Ptex=0"
+    "-DENABLE_Python3=0"
+    "-DINSTALL_DOCS=0"
+    "-DLINKSTATIC=0"
+    "-DOIIO_BUILD_TESTS=0"
+    "-DUSE_PYTHON=0"
+    "-DUSE_QT=0"
+)
+
+if ($IsWindows) {
+    $env:CMAKE_INSTALL_PREFIX = "$ENV:PREFIX/Library"
+    $env:USE_SIMD = "sse4.2,avx2"
+
+    $cmakeArgs += @(
+        "-DCMAKE_C_FLAGS=/utf-8"
+        "-DCMAKE_CXX_FLAGS=/utf-8"
+    )
+}
+elseif ($IsLinux) {
+    $env:CMAKE_INSTALL_PREFIX = "$ENV:PREFIX"
+
+    if ($arch -eq "X64") {
+        $env:USE_SIMD = "sse4.2,avx2"
+    }
+}
+
+cmake -S . -B build @cmakeArgs
+cmake --build build --config Release --target install
