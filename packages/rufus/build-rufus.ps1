b9d068419c59da004389b6c33b5e4b0cbf67034b
Glatzel <chenzelin9794@gmail.com>
2026-01-17 13:33:20 +0800

    f

--- packages/rufus/build-rufus.ps1
+++ packages/rufus/build-rufus.ps1
@@ -1,7 +1,5 @@
 gh release download -R "pbatard/${env:PKG_NAME}" -p "${env:PKG_NAME}-*.??.exe" `
-    -O  "${env:PKG_NAME}.exe"
-New-Item $env:PREFIX/bin -ItemType Directory
-Copy-Item "./${env:PKG_NAME}/${env:PKG_NAME}.exe" "$env:PREFIX/bin/${env:PKG_NAME}.exe"
+    -O  "$env:PREFIX/bin/${env:PKG_NAME}.exe"
 # shortcut
 New-Item $env:PREFIX/Menu -ItemType Directory
 Copy-Item "${env:RECIPE_DIR}/${env:PKG_NAME}.json" "$env:PREFIX/Menu"
