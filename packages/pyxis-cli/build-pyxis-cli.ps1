93d3bf4724021b027c6df00a73493fdb61733b2d
Glatzel <chenzelin9794@gmail.com>
2026-01-17 22:34:21 +0800

    f

--- packages/pyxis-cli/build-pyxis-cli.ps1
+++ packages/pyxis-cli/build-pyxis-cli.ps1
@@ -2,13 +2,7 @@ Set-Location rust
 if ($IsWindows) {
     $env:PROJ_ROOT = "$(Resolve-Path $env:BUILD_PREFIX/Library)"
 }
-if ($IsMacOS) {
-    $env:PROJ_ROOT = "$(Resolve-Path $env:BUILD_PREFIX)"
-}
-if ($IsLinux -and ($(uname -m) -eq 'x86_64' )) {
-    $env:PROJ_ROOT = "$(Resolve-Path $env:BUILD_PREFIX)"
-}
-if ($IsLinux -and ($(uname -m) -eq 'aarch64' )) {
+else {
     $env:PROJ_ROOT = "$(Resolve-Path $env:BUILD_PREFIX)"
 }
 cargo install --bin pyxis --root $env:PREFIX --path ./crates/pyxis-cli
