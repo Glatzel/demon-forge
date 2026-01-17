00f703120e8085b3067aa2b4d6dbc4085d976682
Glatzel <893016099@qq.com>
2026-01-17 23:41:34 +0800

    feat: remove libraw-static (#692)

--- packages/libraw-static/build-libraw-static.ps1
+++
@@ -1,7 +0,0 @@
-$ROOT = git rev-parse --show-toplevel
-. $ROOT/scripts/util.ps1
-Copy-Item $PSScriptRoot/build/* ./ -Recurse
-& ./vcpkg-setup.ps1
-& ./vcpkg-install.ps1
-New-Item $env:PREFIX/libraw -ItemType Directory
-Copy-Item "./installed/*" "$env:PREFIX/libraw" -Recurse
