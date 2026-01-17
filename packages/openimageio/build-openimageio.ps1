b6ff55ff175b987738b31a17378f2e8bc659b4ed
Glatzel <chenzelin9794@gmail.com>
2026-01-17 16:36:10 +0800

    f

--- packages/openimageio/build-openimageio.ps1
+++ packages/openimageio/build-openimageio.ps1
@@ -15,6 +15,8 @@ cmake -S . -B build `
     -DVERBOSE=ON `
     -DCMAKE_BUILD_TYPE=Release `
     -DBUILD_DOCS=0 `
+    -DCMAKE_C_FLAGS_MSVC_INIT="/utf-8" `
+    -DCMAKE_CXX_FLAGS_MSVC_INIT="/utf-8" `
     -DBUILD_SHARED_LIBS=1 `
     -DENABLE_DCMTK=0 `
     -DENABLE_FFmpeg=0 `
