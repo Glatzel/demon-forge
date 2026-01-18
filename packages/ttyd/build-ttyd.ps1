350d659f8968f6227cb47f6298fa0f6e4106f691
Glatzel <893016099@qq.com>
2026-01-19 00:06:50 +0800

    更新 build-ttyd.ps1

--- packages/ttyd/build-ttyd.ps1
+++ packages/ttyd/build-ttyd.ps1
@@ -2,39 +2,6 @@ $ROOT = git rev-parse --show-toplevel
 . $ROOT/scripts/util.ps1
 copy-item $PSScriptRoot/build/* ./ -recurse
 get-content ./index.scss >> ./html/src/style/index.scss
-function build_libwebsockets {
-    $repoUrl = 'https://github.com/msys2/MINGW-packages/trunk/mingw-w64-libwebsockets'
-    $pkgDir  = 'mingw-w64-libwebsockets'
-    $pkgbuild = Join-Path $pkgDir 'PKGBUILD'
-
-    # Checkout PKGBUILD
-    svn co $repoUrl
-
-    # Replace openssl -> mbedtls
-    (Get-Content $pkgbuild) `
-        -replace 'openssl', 'mbedtls' |
-        Set-Content $pkgbuild
-
-    # Insert CMake options after CMAKE_INSTALL_PREFIX line
-    $lines = Get-Content $pkgbuild
-    $out = foreach ($line in $lines) {
-        $line
-        if ($line -match '-DCMAKE_INSTALL_PREFIX=\$\{MINGW_PREFIX\}') {
-            '    -DLWS_WITH_MBEDTLS=ON \'
-            '    -DLWS_WITH_LIBUV=ON \'
-        }
-    }
-    $out | Set-Content $pkgbuild
-
-    Push-Location $pkgDir
-    try {
-        & C:/msys64/usr/bin/makepkg-mingw.exe --cleanbuild --syncdeps --force --noconfirm
-        & C:/msys64/usr/bin/pacman.exe -U *.pkg.tar.zst --noconfirm
-    }
-    finally {
-        Pop-Location
-    }
-}
 
 if ($IsWindows) {
     $env:NPM_CONFIG_PREFIX = "$env:BUILD_PREFIX"
