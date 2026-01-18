4a12975c5ae49ce09e5d7c5cd1ee2367c11c33fe
Glatzel <chenzelin9794@gmail.com>
2026-01-18 17:03:17 +0800

    Update build-termscp.ps1

--- packages/termscp/build-termscp.ps1
+++ packages/termscp/build-termscp.ps1
@@ -1,6 +1,7 @@
 $JITTER = Get-ChildItem "$env:BUILD_PREFIX\.cargo\registry\src\index.crates.io-*\" `
     -Recurse -Directory -Filter "jitterentropy-library" |
     Select-Object -First 1 -ExpandProperty FullName
+write-output $JITTER
 $env:CL = "/I`"$JITTER`""
 $ROOT = git rev-parse --show-toplevel
 . $ROOT/scripts/util.ps1
