e9a1eb1dc66322208fa070a8815474e20e3e27c4
Glatzel <chenzelin9794@gmail.com>
2026-01-18 16:32:12 +0800

    Update build-sony-camera-remote-sdk.ps1

--- packages/sony-camera-remote-sdk/build-sony-camera-remote-sdk.ps1
+++ packages/sony-camera-remote-sdk/build-sony-camera-remote-sdk.ps1
@@ -1,16 +1,29 @@
 $ROOT = git rev-parse --show-toplevel
 . $ROOT/scripts/util.ps1
-python $env:RECIPE_DIR/download.py
-$zipfile = (Get-ChildItem "./*.zip")[0]
-$zipfile.BaseName -match "CrSDK_v(\d+)\.(\d+)\.(\d+).+_(\S+)"
-$platform = $Matches[4]
-foreach ($f in Get-ChildItem "./*.zip") {
-    $f.BaseName -match "CrSDK_v(\d+)\.(\d+)\.(\d+).+_(\S+)"
-    $platform = $Matches[4]
-    7z x "$f" "-ounzip/$platform"
+if ($IsWindows) {
+    aria2c -c -x16 -s16 -d ./ `
+        https://support.d-imaging.sony.co.jp/disoft_DL/SDK_DL/win?fm=en-us `
+        -o "${env:PKG_NAME}.zip"
 }
+if ($IsMacOS) {
+    aria2c -c -x16 -s16 -d ./ `
+        https://support.d-imaging.sony.co.jp/disoft_DL/SDK_DL/mac?fm=en-us `
+        -o "${env:PKG_NAME}.zip"
+}
+if ($IsLinux -and $arch -eq "X64") {
+    aria2c -c -x16 -s16 -d ./ `
+        https://support.d-imaging.sony.co.jp/disoft_DL/SDK_DL/linux_x86?fm=en-us `
+        -o "${env:PKG_NAME}.zip"
+}
+if ($IsMacOS -and $arch -eq "Arm64") {
+    aria2c -c -x16 -s16 -d ./ `
+        https://support.d-imaging.sony.co.jp/disoft_DL/SDK_DL/linux_64?fm=en-us `
+        -o "${env:PKG_NAME}.zip"
+}
+7z x "$f" "-osdk"
+
 New-Item "$env:PREFIX/${env:PKG_NAME}" -ItemType Directory
-Copy-Item "./unzip/$platform/*" "$env:PREFIX/${env:PKG_NAME}" -Recurse
+Copy-Item "./sdk/*" "$env:PREFIX/${env:PKG_NAME}" -Recurse
 Remove-Item $env:PREFIX/${env:PKG_NAME}/app/*.h
 Remove-Item $env:PREFIX/${env:PKG_NAME}/app/*.cpp
 Remove-Item $env:PREFIX/${env:PKG_NAME}/*.zip
