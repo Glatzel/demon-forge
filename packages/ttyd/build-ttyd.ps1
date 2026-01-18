80873bedf4d47d056f216435286d6811caac20ab
Glatzel <893016099@qq.com>
2026-01-18 22:55:06 +0800

    更新 build-ttyd.ps1

--- packages/ttyd/build-ttyd.ps1
+++ packages/ttyd/build-ttyd.ps1
@@ -2,18 +2,22 @@ $ROOT = git rev-parse --show-toplevel
 . $ROOT/scripts/util.ps1
 copy-item $PSScriptRoot/build/* ./ -recurse
 get-content ./index.scss >> ./html/src/style/index.scss
-& ./download-font.ps1
 
-Set-Location ./html
 if ($IsWindows) {
     $env:NPM_CONFIG_PREFIX = "$env:BUILD_PREFIX"
+    $env:CMAKE_INSTALL_PREFIX="$env:BUILD_PREFIX/Library"
+    pacman -S mingw-w64-json-c
+    pacman -S mingw-w64-libwebsockets
     $env:CMAKE_INSTALL_PREFIX="$env:PREFIX/Library"
     pacman -S mingw-w64-json-c
     pacman -S mingw-w64-libwebsockets
 }
 else{
-$env:CMAKE_INSTALL_PREFIX="$env:PREFIX"
+    $env:CMAKE_INSTALL_PREFIX="$env:PREFIX"
 }
+
+& ./download-font.ps1
+Set-Location ./html
 npm install -g corepack
 corepack enable
 corepack prepare yarn@stable --activate
@@ -21,8 +25,5 @@ yarn install
 yarn run check
 yarn run build
 Set-Location ..
-mkdir build
-Set-Location build
-
 cmake -S . -B build -DCMAKE_BUILD_TYPE="RELEASE"
 cmake --build build --config Release --target install
