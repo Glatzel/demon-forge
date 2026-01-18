e94a2eabc1992c3811d8b401259c53fdfc31a01f
Glatzel <chenzelin9794@gmail.com>
2026-01-18 16:46:48 +0800

    f

--- packages/zed/build-zed.ps1
+++ packages/zed/build-zed.ps1
@@ -1,11 +1,25 @@
 $ROOT = git rev-parse --show-toplevel
 . $ROOT/scripts/util.ps1
-New-Item $env:PREFIX/bin -ItemType Directory
-Copy-Item "$ROOT/temp/$env:PKG_NAME/$env:PKG_NAME/target/release/zed.exe" "$env:PREFIX/bin/zed.exe"
-Copy-Item "$ROOT/temp/$env:PKG_NAME/$env:PKG_NAME/target/release/cli.exe" "$env:PREFIX/bin/zed-cli.exe"
-# shortcut
+
 New-Item $env:PREFIX/Menu -ItemType Directory
-Copy-Item "$env:RECIPE_DIR/$env:PKG_NAME.json" "$env:PREFIX/Menu"
+Copy-Item "$env:PREFIX/$name.json" "$env:PREFIX/Menu"
 if ($IsWindows) {
-    Copy-Item "$ROOT/temp/$env:PKG_NAME/$env:PKG_NAME.ico" "$env:PREFIX/Menu"
+    $version = get-current-version
+    aria2c -c -x16 -s16 -d "$env:PREFIX/Menu" `
+        "https://raw.githubusercontent.com/zed-industries/zed/refs/tags/v$version/crates/zed/resources/windows/app-icon.ico" `
+        -o "$name.ico"
 }
+# enable long path
+if ($IsWindows) {
+    git config --system core.longpaths true
+    New-ItemProperty `
+        -Path "HKLM:\SYSTEM\CurrentControlSet\Control\FileSystem" `
+        -Name "LongPathsEnabled" `
+        -PropertyType DWord `
+        -Value 1 `
+        -Force
+}
+$env:CARGO_TARGET_DIR = "$env:SRC_DIR/target"
+cargo install --root $env:PREFIX --path ./crates/zed
+cargo install --root $env:PREFIX --path ./crates/cli
+Rename-Item $env:PREFIX/bin/cli.exe $env:PREFIX/bin/zed-cli.exe
