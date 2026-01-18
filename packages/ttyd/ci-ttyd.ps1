1cedf31e79119fd7e6e166ff42029ccee0dac527
Glatzel <893016099@qq.com>
2026-01-18 22:59:25 +0800

    更新 ci-ttyd.ps1

--- packages/ttyd/ci-ttyd.ps1
+++ packages/ttyd/ci-ttyd.ps1
@@ -3,4 +3,5 @@ $ROOT = git rev-parse --show-toplevel
 . $ROOT/scripts/util.ps1
 $latest_version = get-version-github -repo "tsl0922/$name"
 update-recipe -version $latest_version
+which pacman
 build-pkg
