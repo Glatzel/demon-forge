adf22045d2df57edb88709accb6e4d6242580bc9
Glatzel <chenzelin9794@gmail.com>
2026-01-19 23:00:18 +0800

    chore: use ninja (#707)

--- packages/proj/build-proj.ps1
+++ packages/proj/build-proj.ps1
@@ -16,7 +16,7 @@ if ($IsLinux) {
     $env:CMAKE_INSTALL_PREFIX = "$env:PREFIX"
 }
 
-cmake -S . -B build `
+cmake -S . -B build -G Ninja `
     -DCMAKE_BUILD_TYPE="RELEASE" `
     -DBUILD_APPS=ON `
     -DBUILD_SHARED_LIBS=ON `
