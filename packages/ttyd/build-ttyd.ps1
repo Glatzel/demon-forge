f3580a6eeecd1f3d0fee1044485d5af684c91704
Glatzel <chenzelin9794@gmail.com>
2026-01-19 21:53:52 +0800

    f

--- packages/ttyd/build-ttyd.ps1
+++ packages/ttyd/build-ttyd.ps1
@@ -14,11 +14,11 @@ if ($IsWindows) {
     Set-Location ./external/libwebsockets
     Copy-Item $env:RECIPE_DIR/PKGBUILD ./
     Get-ChildItem -Recurse -File | ForEach-Object {
-    $c = Get-Content $_.FullName -Raw
-    if ($c -match "`r`n") {
-        $c -replace "`r`n", "`n" | Set-Content $_.FullName -NoNewline
+        $c = Get-Content $_.FullName -Raw
+        if ($c -match "`r`n") {
+            $c -replace "`r`n", "`n" | Set-Content $_.FullName -NoNewline
+        }
     }
-}
     & "C:\msys64\msys2_shell.cmd" -here -no-start -defterm -mingw64 -c "pacman -S --noconfirm mingw-w64-x86_64-json-c"
     & "C:\msys64\msys2_shell.cmd" -here -no-start -defterm -mingw64 -c "pacman -S --noconfirm binutils && makepkg --noconfirm -s && pacman --noconfirm -U *.pkg.tar.zst"
     Set-Location $env:SRC_DIR
@@ -28,9 +28,10 @@ if ($IsWindows) {
         "-G"
         "Ninja"
         "-DCMAKE_INSTALL_PREFIX=$env:PREFIX/Library"
-        "-DCMAKE_PREFIX_PATH=C:/msys64/mingw64;$env:CMAKE_PREFIX_PATH"
+        "-DCMAKE_PREFIX_PATH=$env:BUILD_PREFIX/Library;C:/msys64/mingw64;$env:CMAKE_PREFIX_PATH"
         "-DCMAKE_C_COMPILER=x86_64-w64-mingw32-gcc"
         "-DCMAKE_CXX_COMPILER=x86_64-w64-mingw32-g++"
+        "-DCMAKE_FIND_LIBRARY_SUFFIXES=.a"
     )
 }
 else {
