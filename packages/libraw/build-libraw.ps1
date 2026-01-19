523ec6850af9b56adc5837719bd34245cdbfa824
Glatzel <chenzelin9794@gmail.com>
2026-01-19 10:32:17 +0800

    Update build-libraw.ps1

--- packages/libraw/build-libraw.ps1
+++ packages/libraw/build-libraw.ps1
@@ -7,5 +7,6 @@ if ($IsMacOS) {
 if ($IsLinux) {
     $env:CMAKE_INSTALL_PREFIX = "$env:PREFIX"
 }
+Copy-Item ./LibRaw-cmake/* ./ -Recurse
 cmake -S . -B build -DCMAKE_BUILD_TYPE="RELEASE"
 cmake --build build --config Release --target install
