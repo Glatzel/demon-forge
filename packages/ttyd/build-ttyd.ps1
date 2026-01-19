01a4388c7d5dddfaffc36e8c15534c174d6d0487
Glatzel <chenzelin9794@gmail.com>
2026-01-19 11:09:11 +0800

    f

--- packages/ttyd/build-ttyd.ps1
+++ packages/ttyd/build-ttyd.ps1
@@ -6,7 +6,7 @@ get-content ./index.scss >> ./html/src/style/index.scss
 if ($IsWindows) {
     Set-Location ./external/libwebsockets
     & "C:\msys64\msys2_shell.cmd" -here -no-start -defterm -mingw64 -c "pacman -S --noconfirm mingw-w64-x86_64-json-c"
-    & "C:\msys64\msys2_shell.cmd" -here -no-start -defterm -mingw64 -c "pacman -S --noconfirm binutils && makepkg --noconfirm -s && pacman --noconfirm -U *.pkg.tar.zst"
+    & "C:\msys64\msys2_shell.cmd" -here -no-start -defterm -mingw64 -c "pacman -S --noconfirm binutils git && makepkg --noconfirm -s && pacman --noconfirm -U *.pkg.tar.zst"
     copy-item C:/msys64/mingw64/* $env:BUILD_PREFIX/Library -Recurse -force
     Set-Location $env:SRC_DIR
 
