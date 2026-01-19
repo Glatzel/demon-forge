cbac8c9462038735cbcb15e600ab1b1f4194f31e
Glatzel <chenzelin9794@gmail.com>
2026-01-19 10:51:23 +0800

    build: update libraw build script (#706)

--- packages/libraw/build-libraw.ps1
+++ packages/libraw/build-libraw.ps1
@@ -7,7 +7,6 @@ if ($IsMacOS) {
 if ($IsLinux) {
     $env:CMAKE_INSTALL_PREFIX = "$env:PREFIX"
 }
-git clone --depth 1 https://github.com/LibRaw/LibRaw-cmake.git
 Copy-Item ./LibRaw-cmake/* ./ -Recurse
 cmake -S . -B build -DCMAKE_BUILD_TYPE="RELEASE"
 cmake --build build --config Release --target install
