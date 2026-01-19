33ae6ccac4861f15761d243c234e4bbfa9beb16d
Glatzel <chenzelin9794@gmail.com>
2026-01-19 21:37:23 +0800

    f

--- packages/ttyd/build-ttyd.ps1
+++ packages/ttyd/build-ttyd.ps1
@@ -20,7 +20,7 @@ if ($IsWindows) {
     }
 }
     & "C:\msys64\msys2_shell.cmd" -here -no-start -defterm -mingw64 -c "pacman -S --noconfirm mingw-w64-x86_64-json-c"
-    & "C:\msys64\msys2_shell.cmd" -here -no-start -defterm -mingw64 -c "pacman -S --noconfirm binutils patch && makepkg --noconfirm -s && pacman --noconfirm -U *.pkg.tar.zst"
+    & "C:\msys64\msys2_shell.cmd" -here -no-start -defterm -mingw64 -c "makepkg --noconfirm -s && pacman --noconfirm -U *.pkg.tar.zst"
     Set-Location $env:SRC_DIR
     # Set up MinGW environment variables for Windows
     $env:NPM_CONFIG_PREFIX = "$env:BUILD_PREFIX"
@@ -28,9 +28,10 @@ if ($IsWindows) {
         "-G"
         "Ninja"
         "-DCMAKE_INSTALL_PREFIX=$env:PREFIX/Library"
-        "-DCMAKE_PREFIX_PATH=$env:BUILD_PREFIX/Library;C:/msys64/mingw64;$env:CMAKE_PREFIX_PATH"
+        "-DCMAKE_PREFIX_PATH=C:/msys64/mingw64;$env:CMAKE_PREFIX_PATH"
         "-DCMAKE_C_COMPILER=x86_64-w64-mingw32-gcc"
         "-DCMAKE_CXX_COMPILER=x86_64-w64-mingw32-g++"
+        "-DCMAKE_FIND_LIBRARY_SUFFIXES=.a"
     )
 }
 else {
