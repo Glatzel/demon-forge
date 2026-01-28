9b0e9b54b3795d66b05070dda8d0fd269f1b9614
Glatzel <chenzelin9794@gmail.com>
2026-01-28 19:34:17 +0800

    f

--- packages/easytier/build-easytier.ps1
+++ packages/easytier/build-easytier.ps1
@@ -1,21 +1,3 @@
 $ROOT = git rev-parse --show-toplevel
 . $ROOT/scripts/util.ps1
-if ($IsWindows) {
-    gh release download -R "EasyTier/EasyTier" -p "${env:PKG_NAME}-windows-x86_64-*.zip" `
-        -O  ./${env:PKG_NAME}.zip
-}
-if ($IsLinux -and ($arch -eq "X64")) {
-    gh release download -R "EasyTier/EasyTier" -p "${env:PKG_NAME}-linux-x86_64-*.zip" `
-        -O  ./${env:PKG_NAME}.zip
-}
-if ($IsLinux -and ($arch -eq "Arm64")) {
-    gh release download -R "EasyTier/EasyTier" -p "${env:PKG_NAME}-linux-aarch64-*.zip" `
-        -O  ./${env:PKG_NAME}.zip
-}
-if ($IsMacOS) {
-    gh release download -R "EasyTier/EasyTier" -p "${env:PKG_NAME}-macos-aarch64-*.zip" `
-        -O  ./${env:PKG_NAME}.zip
-}
-7z x "${env:PKG_NAME}.zip" "-o./${env:PKG_NAME}"
-New-Item $env:PREFIX/bin -ItemType Directory
-Copy-Item "./${env:PKG_NAME}/${env:PKG_NAME}*/*" "$env:PREFIX/bin/" -Recurse
+cargo install --path . @(Get-Cargo-Arg)
