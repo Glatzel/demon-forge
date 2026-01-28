7d3feb961991ecbb8a4fb05e3d395837a27381f6
Glatzel <chenzelin9794@gmail.com>
2026-01-28 19:13:54 +0800

    f

--- packages/edit/build-edit.ps1
+++ packages/edit/build-edit.ps1
@@ -1,27 +1,4 @@
 $ROOT = git rev-parse --show-toplevel
 . $ROOT/scripts/util.ps1
-if ($isWindows) {
-    gh release download -R "microsoft/${env:PKG_NAME}" -p "*x86_64-windows*" `
-        -O  ./${env:PKG_NAME}.zip
-    7z x "${env:PKG_NAME}.zip" "-o./${env:PKG_NAME}"
-}
-if ($IsLinux -and ($arch -eq 'X64')) {
-    gh release download -R "microsoft/${env:PKG_NAME}" -p "*x86_64-linux*" `
-        -O  ./${env:PKG_NAME}.tar.zst
-    tar  --zstd -xvf "${env:PKG_NAME}.tar.zst" -C "."
-    Remove-Item "./${env:PKG_NAME}.tar.zst"
-}
-if ($IsLinux -and ($arch -eq 'Arm64')) {
-    gh release download -R "microsoft/${env:PKG_NAME}" -p "*aarch64-linux*" `
-        -O  ./${env:PKG_NAME}.tar.zst
-    tar  --zstd -xvf "${env:PKG_NAME}.tar.zst" -C "."
-    Remove-Item "./${env:PKG_NAME}.tar.zst"
-}
-New-Item $env:PREFIX/bin -ItemType Directory
-if ($IsWindows) {
-    Copy-Item "./${env:PKG_NAME}/*/${env:PKG_NAME}.exe" "$env:PREFIX/bin/${env:PKG_NAME}.exe"
-}
-if ($IsLinux) {
-    Copy-Item "./${env:PKG_NAME}" "$env:PREFIX/bin/${env:PKG_NAME}"
-    chmod +rwx "$env:PREFIX/bin/${env:PKG_NAME}"
-}
+$env:RUSTC_BOOTSTRAP=1
+cargo install --path . --config .cargo/release-nightly.toml --release
