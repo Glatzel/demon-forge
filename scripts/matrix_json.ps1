dd5e3388a0a7f919fdae2f75d078af3ece27416d
Glatzel <chenzelin9794@gmail.com>
2026-01-31 17:04:16 +0800

    f

--- scripts/matrix_json.ps1
+++ scripts/matrix_json.ps1
@@ -3,29 +3,32 @@ $matrix = @()
 
 foreach ($row in $csvData) {
     $pkg = $row.pkg
-    foreach ($target_platform in "win-64", "osx-arm64", "linux-64", "linux-aarch64") {
-        if ($row.$target_platform) {
-            if (
-                (($env:GITHUB_EVENT_NAME -eq "pull_request") -or ($env:GITHUB_EVENT_NAME -eq "push")) `
-                    -and
-                $row.$target_platform -like "**+**"
-            ) {
-                $parts = $row.$target_platform -split '\+'
-                $matrix += @{
-                    pkg             = $pkg
-                    machine         = $parts[0]
-                    container       = $parts[1]
-                    target_platform = $target_platform
+    if (($env:GITHUB_EVENT_NAME -eq "pull_request") -or ($env:GITHUB_EVENT_NAME -eq "push")) {
+        foreach ($target_platform in "win-64", "osx-arm64", "linux-64", "linux-aarch64") {
+            if ($row.$target_platform) {
+                if ($row.$target_platform -like "**+**") {
+                    $parts = $row.$target_platform -split '\+'
+                    $matrix += @{
+                        pkg             = $pkg
+                        machine         = $parts[0]
+                        container       = $parts[1]
+                        target_platform = $target_platform
+                    }
+                }
+                else {
+                    $matrix += @{
+                        pkg             = $pkg
+                        machine         = $row.$target_platform
+                        target_platform = $target_platform
+                    }
                 }
             }
-            else {
-                $matrix += @{
-                    pkg             = $pkg
-                    machine         = $row.$target_platform
-                    target_platform = $target_platform
-                }
-
-            }
+        } 
+    }
+    else {
+        $matrix += @{
+            pkg     = $pkg
+            machine = "ubuntu-slim"
         }
     }
 }
@@ -43,9 +46,7 @@ switch ($env:GITHUB_EVENT_NAME) {
     "pull_request" {
         $matrix = $matrix | jq -c --argjson pkgs "${env:CHANGED_KEYS}" '{include: .include | map(select(.pkg as $p | $pkgs | index($p)))}'
     }
-    default {
-        $matrix = $matrix | jq -c '{include: .include | map(.machine = "ubuntu-slim") | group_by(.pkg) | map(.[0])}'
-    }
+    default {}
 }
 
 # Output matrix to GitHub Actions
