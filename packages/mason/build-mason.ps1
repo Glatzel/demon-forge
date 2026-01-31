dad1b644470dbe38b3275494b7e07f587d3e6613
Glatzel <chenzelin9794@gmail.com>
2026-01-31 20:15:06 +0800

    f

--- packages/mason/build-mason.ps1
+++ packages/mason/build-mason.ps1
@@ -1,5 +1,5 @@
 & ./scripts/build.ps1 -Release
 New-Item $env:PREFIX/bin/${env:PKG_NAME} -ItemType Directory
 Copy-Item "./bin/Mason/*" "$env:PREFIX/bin/${env:PKG_NAME}" -Recurse
-Copy-Item $env:RECIPE_DIR/post-link.bat "$PREFIX/bin/.mason-post-link.bat"
-Copy-Item $env:RECIPE_DIR/pre-unlink.bat "$PREFIX/bin/.mason-pre-unlink.bat"
+Copy-Item $env:RECIPE_DIR/post-link.bat $env:PREFIX/bin/.mason-post-link.bat
+Copy-Item $env:RECIPE_DIR/pre-unlink.bat $env:PREFIX/bin/.mason-pre-unlink.bat
