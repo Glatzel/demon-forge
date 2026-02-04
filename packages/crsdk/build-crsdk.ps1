7a72b53dcaa082cd22b8327cec8227d5f1debb06
Glatzel <chenzelin9794@gmail.com>
2026-02-04 11:28:36 +0800

    f

--- packages/crsdk/build-crsdk.ps1
+++ packages/crsdk/build-crsdk.ps1
@@ -3,14 +3,19 @@ $ROOT = git rev-parse --show-toplevel
 
 7z x "crsdk.zip" "-osdk"
 Set-Location sdk
-7z x "*.zip"
+foreach ($f in Get-ChildItem ./*.zip) {
+    $zip_name = $f.BaseName
+    7z x "$f" "-o$zip_name"
+}
+Remove-Item ./SourceCodeOfOpenSourceSoftware -Recurse -Force
+Remove-Item ./RemoteCli/external/opencv -Recurse -Force
 Remove-Item "*.zip" -Recurse -Force
 if ($IsWindows) {
     New-Item "$env:PREFIX/Library/bin" -ItemType Directory
     New-Item "$env:PREFIX/Library/include" -ItemType Directory
     New-Item "$env:PREFIX/Library/share/crsdk" -ItemType Directory
 
-    Copy-Item "./RemoteCli/app/crsdk/*" "$env:PREFIX/Library/include" -Recurse
+    Copy-Item "./RemoteCli/app/CRSDK/*" "$env:PREFIX/Library/include" -Recurse
     Copy-Item "./RemoteCli/external/crsdk/*" "$env:PREFIX/Library/bin" -Recurse
     Copy-Item "./*" "$env:PREFIX/Library/share/crsdk" -Recurse
 }
@@ -19,7 +24,7 @@ else {
     New-Item "$env:PREFIX/include" -ItemType Directory
     New-Item "$env:PREFIX/share/crsdk" -ItemType Directory
 
-    Copy-Item "./RemoteCli/app/crsdk/*" "$env:PREFIX/include" -Recurse
+    Copy-Item "./RemoteCli/app/CRSDK/*" "$env:PREFIX/include" -Recurse
     Copy-Item "./RemoteCli/external/crsdk/*" "$env:PREFIX/lib" -Recurse
     Copy-Item "./*" "$env:PREFIX/share/crsdk" -Recurse
 }
