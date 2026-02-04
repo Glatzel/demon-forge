a971a91b7b5c89faa9808908695bb91d34014870
Glatzel <893016099@qq.com>
2026-02-04 10:26:12 +0800

    更新 ci-sony-camera-remote-sdk.ps1

--- packages/sony-camera-remote-sdk/ci-sony-camera-remote-sdk.ps1
+++ packages/sony-camera-remote-sdk/ci-sony-camera-remote-sdk.ps1
@@ -2,6 +2,6 @@ Set-Location $PSScriptRoot
 $ROOT = git rev-parse --show-toplevel
 . $ROOT/scripts/util.ps1
 $latest_version = get-version-url -url "https://support.d-imaging.sony.co.jp/app/sdk/licenseagreement_d/en-US.html" -pattern 'CrSDK_v(\d+\.\d+\.\d+)_'
-$latest_version = "$latest_version".Replace("00", "0")
+$latest_version = [Version]"$latest_version"
 update-recipe -version $latest_version
 build-pkg
