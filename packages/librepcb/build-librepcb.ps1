ef6c3694498db1ac8f94508e5afb0fb32c15c9d6
Glatzel <chenzelin9794@gmail.com>
2026-02-04 16:16:24 +0800

    build: use source to download (#771)
    Co-authored-by: pre-commit-ci-lite[bot] <117423508+pre-commit-ci-lite[bot]@users.noreply.github.com>

--- packages/librepcb/build-librepcb.ps1
+++ packages/librepcb/build-librepcb.ps1
@@ -1,9 +1,5 @@
-aria2c -c -x16 -s16 -d ./ `
-    "https://download.librepcb.org/releases/${env:PKG_VERSION}/librepcb-${env:PKG_VERSION}-windows-x86_64.zip" `
-    -o "${env:PKG_NAME}.zip"
-7z x "${env:PKG_NAME}.zip" "-o./${env:PKG_NAME}"
 New-Item $env:PREFIX/bin/${env:PKG_NAME} -ItemType Directory
-Copy-Item "./${env:PKG_NAME}/*" "$env:PREFIX/bin/${env:PKG_NAME}" -Recurse
+Copy-Item "./*" "$env:PREFIX/bin/${env:PKG_NAME}" -Recurse
 # shortcut
 New-Item $env:PREFIX/Menu -ItemType Directory
 Copy-Item "${env:RECIPE_DIR}/${env:PKG_NAME}.json" "$env:PREFIX/Menu"
--- packages/nuget/build-nuget.ps1
+++ packages/nuget/build-nuget.ps1
@@ -1,5 +1,2 @@
-aria2c -c -x16 -s16 -d ./ `
-    "https://dist.${env:PKG_NAME}.org/win-x86-commandline/latest/${env:PKG_NAME}.exe" `
-    -o "${env:PKG_NAME}.exe"
 New-Item $env:PREFIX/bin -ItemType Directory
-Copy-Item "./${env:PKG_NAME}/${env:PKG_NAME}.exe" "$env:PREFIX/bin/${env:PKG_NAME}.exe"
+Copy-Item "./${env:PKG_NAME}.exe" "$env:PREFIX/bin/${env:PKG_NAME}.exe"
