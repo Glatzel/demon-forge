cafb78bcf3562d0529dd711d9657548bec5e7548
Glatzel <893016099@qq.com>
2026-02-05 17:32:44 +0800

    更新 build-fresh-editor.ps1

--- packages/fresh-editor/build-fresh-editor.ps1
+++ packages/fresh-editor/build-fresh-editor.ps1
@@ -1,4 +1,9 @@
 $ROOT = git rev-parse --show-toplevel
 . $ROOT/scripts/util.ps1
-if ($IsLinux) { $env:LIBCLANG_PATH = "$env:BUILD_PREFIX/lib" }
-cargo install $name @(Get-Cargo-Arg)
+if ($IsLinux) { 
+    $env:LIBCLANG_PATH = "$env:BUILD_PREFIX/lib" 
+    cargo install $name --root $env:PREFIX
+}
+else{
+    cargo install $name @(Get-Cargo-Arg)
+}
