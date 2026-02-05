db366da5f52f5234ea10bc685fbae5758ec671e5
Glatzel <chenzelin9794@gmail.com>
2026-02-06 00:15:21 +0800

    build: change rust build setting (#776)
    Co-authored-by: pre-commit-ci-lite[bot] <117423508+pre-commit-ci-lite[bot]@users.noreply.github.com>

--- packages/fresh-editor/build-fresh-editor.ps1
+++ packages/fresh-editor/build-fresh-editor.ps1
@@ -1,17 +1,4 @@
 $ROOT = git rev-parse --show-toplevel
 . $ROOT/scripts/util.ps1
-if ($IsLinux) {
-    $env:LIBCLANG_PATH = "$env:BUILD_PREFIX/lib"
-    cargo install $name `
-        --root $env:PREFIX `
-        --locked `
-        --force `
-        --config profile.release.debug=false `
-        --config profile.release.codegen-units=1 `
-        --config 'profile.release.lto="fat"' `
-        --config profile.release.opt-level=3 `
-        --config profile.release.strip=true
-}
-else{
-    cargo install $name @(Get-Cargo-Arg)
-}
+if ($IsLinux) { $env:LIBCLANG_PATH = "$env:BUILD_PREFIX/lib"}
+cargo install $name @(Get-Cargo-Arg)
