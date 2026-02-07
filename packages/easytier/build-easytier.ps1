d82d17713a6e8b0741a4bb14016edfd4e81316f8
Glatzel <chenzelin9794@gmail.com>
2026-02-07 19:01:36 +0800

    分

--- packages/easytier/build-easytier.ps1
+++ packages/easytier/build-easytier.ps1
@@ -3,6 +3,6 @@ $ROOT = git rev-parse --show-toplevel
 if ($IsLinux) { $env:LIBCLANG_PATH = "$env:BUILD_PREFIX/lib" }
 cargo install @(Get-Cargo-Arg) --path ./easytier
 if ($IsWindows) {
-    Copy-Item ./easytier/third_party/x86_64/Packet.dll $env:PREFIX/bin
-    Copy-Item ./easytier/third_party/x86_64/wintun.dll $env:PREFIX/bin
+    Copy-Item ./easytier/third_party/Packet.dll $env:PREFIX/bin
+    Copy-Item ./easytier/third_party/wintun.dll $env:PREFIX/bin
 }
