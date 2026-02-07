39491c5a848c30033f5c728dc907e9abd4ee6203
Glatzel <chenzelin9794@gmail.com>
2026-02-07 11:25:34 +0800

    f

--- packages/termscp/build-termscp.ps1
+++ packages/termscp/build-termscp.ps1
@@ -1,5 +1,5 @@
 $ROOT = git rev-parse --show-toplevel
 . $ROOT/scripts/util.ps1
 if($IsLinux){dnf install -y samba}
-if($IsMacOS){brew install -y samba}
+if($IsMacOS){brew install samba}
 cargo install --path . @(Get-Cargo-Arg)
