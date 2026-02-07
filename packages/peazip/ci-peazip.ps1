cdd84c0e73391de2011460f36b69ddd32b8aa158
Glatzel <chenzelin9794@gmail.com>
2026-02-07 13:25:14 +0800

    Update ci-peazip.ps1

--- packages/peazip/ci-peazip.ps1
+++ packages/peazip/ci-peazip.ps1
@@ -2,6 +2,7 @@ Set-Location $PSScriptRoot
 $ROOT = git rev-parse --show-toplevel
 . $ROOT/scripts/util.ps1
 
-$latest_version = get-version-github -repo "https://github.com/$name/PeaZip"
+$latest_version = get-version-github -repo "$name/PeaZip"
 update-recipe -version $latest_version
 build-pkg
+
