5f1972b6caa004d2fbc9de58df1b3d322cbaa37e
Glatzel <chenzelin9794@gmail.com>
2026-02-07 13:22:43 +0800

    f

--- packages/ueberzugpp/ci-ueberzugpp.ps1
+++ packages/ueberzugpp/ci-ueberzugpp.ps1
@@ -1,6 +1,6 @@
 Set-Location $PSScriptRoot
 $ROOT = git rev-parse --show-toplevel
 . $ROOT/scripts/util.ps1
-$latest_version = get-version-github -repo "tsl0922/$name"
+$latest_version = get-version-github -repo "jstkdng/ueberzugpp"
 update-recipe -version $latest_version
 build-pkg
