8a13469ec9b0f00a646ff5ebcc96bcb8fa8d7d90
Glatzel <chenzelin9794@gmail.com>
2026-02-07 14:47:54 +0800

    f

--- packages/ueberzugpp/build-ueberzugpp.ps1
+++ packages/ueberzugpp/build-ueberzugpp.ps1
@@ -1,6 +1,6 @@
 $ROOT = git rev-parse --show-toplevel
 . $ROOT/scripts/util.ps1
-
+if ($IsMacOS) { $env:CMAKE_CXX_COMPILER_CLANG_SCAN_DEPS = "$env:BUILD_PREFIX/bin/clang-scan-deps" }
 cmake -S . -B build `
     -G "Ninja" `
     -DVERBOSE=ON `
