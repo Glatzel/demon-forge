e8fe897bf6a75798e81e80137c18fd5b0556cf65
Glatzel <chenzelin9794@gmail.com>
2026-02-07 13:23:24 +0800

    fix: GitHub repo URL format in ci-yazi-full.ps1

--- packages/yazi-full/ci-yazi-full.ps1
+++ packages/yazi-full/ci-yazi-full.ps1
@@ -2,7 +2,7 @@ Set-Location $PSScriptRoot
 $ROOT = git rev-parse --show-toplevel
 . $ROOT/scripts/util.ps1
 
-$latest_version = get-version-github -repo "https://github.com/sxyazi/yazi"
+$latest_version = get-version-github -repo "sxyazi/yazi"
 update-recipe -version $latest_version
 
 build-pkg
