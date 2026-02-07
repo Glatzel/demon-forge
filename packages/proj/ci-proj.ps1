0742ef0804fbbec6e21119f4dd4abf4827e53378
Glatzel <chenzelin9794@gmail.com>
2026-02-07 13:53:55 +0800

    f

--- packages/proj/ci-proj.ps1
+++ packages/proj/ci-proj.ps1
@@ -1,6 +1,6 @@
 Set-Location $PSScriptRoot
 $ROOT = git rev-parse --show-toplevel
 . $ROOT/scripts/util.ps1
-$latest_version = get-version-github OSGeo/PROJ.git
+$latest_version = get-version-github OSGeo/PROJ
 update-recipe -version $latest_version
 build-pkg
