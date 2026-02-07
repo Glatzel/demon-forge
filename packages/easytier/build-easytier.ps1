c01714130a440b5a71c988079d61174a3f65db61
Glatzel <chenzelin9794@gmail.com>
2026-02-07 16:29:04 +0800

    f

--- packages/easytier/build-easytier.ps1
+++ packages/easytier/build-easytier.ps1
@@ -1,7 +1,10 @@
 $ROOT = git rev-parse --show-toplevel
 . $ROOT/scripts/util.ps1
-if($IsLinux){
-    $env:CFLAGS="-std=c11"
-    $env:CXXFLAGS="-std=c++11"
+if ($IsWindows) {
+    copy-item ./npcap/* $env:BUILD_PREFIX/Library -Recurse
 }
+# if ($IsLinux) {
+#     $env:CFLAGS = "-std=c11"
+#     $env:CXXFLAGS = "-std=c++11"
+# }
 cargo install @(Get-Cargo-Arg) easytier
