eb040913d9acb649c20beecb606de8e283782846
Glatzel <chenzelin9794@gmail.com>
2026-02-07 14:52:03 +0800

    f

--- packages/ueberzugpp/build-ueberzugpp.ps1
+++ packages/ueberzugpp/build-ueberzugpp.ps1
@@ -1,11 +1,14 @@
 $ROOT = git rev-parse --show-toplevel
 . $ROOT/scripts/util.ps1
-if ($IsMacOS) { $env:CMAKE_CXX_COMPILER_CLANG_SCAN_DEPS = "$env:BUILD_PREFIX/bin/clang-scan-deps" }
-cmake -S . -B build `
-    -G "Ninja" `
-    -DVERBOSE=ON `
-    -DCMAKE_BUILD_TYPE=Release `
-    -DCMAKE_INSTALL_PREFIX="$env:PREFIX" `
-    -DENABLE_X11=OFF `
-    -DENABLE_OPENCV=OFF
+$cmakeArgs = @(
+    "-G"
+    "Ninja"
+    "-DVERBOSE=ON"
+    "-DCMAKE_BUILD_TYPE=Release"
+    "-DCMAKE_INSTALL_PREFIX=$env:PREFIX"
+    "-DENABLE_X11=OFF"
+    "-DENABLE_OPENCV=OFF"
+)
+if ($IsMacOS) { $cmakeArgs += "-DCMAKE_CXX_COMPILER_CLANG_SCAN_DEPS=$env:BUILD_PREFIX/bin/clang-scan-deps" }
+cmake -S . -B build $cmakeArgs
 cmake --build build --config Release --target install
