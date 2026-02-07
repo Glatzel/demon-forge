ad1c80f061627fdaf581d0b9194f9bb98104b081
Glatzel <chenzelin9794@gmail.com>
2026-02-07 17:09:14 +0800

    f

--- packages/easytier/build-easytier.ps1
+++ packages/easytier/build-easytier.ps1
@@ -1,7 +1,3 @@
 $ROOT = git rev-parse --show-toplevel
 . $ROOT/scripts/util.ps1
-if ($IsWindows) {
-    $env:LIB = "$PWD/LIB/x64;$env:LIB"
-    $env:INCLUDE = "$PWD/Include;$env:INCLUDE"
-}
 cargo install @(Get-Cargo-Arg) --path ./easytier
