0ad88343b70670913e73804b829b4e6e9646f13e
Glatzel <chenzelin9794@gmail.com>
2026-02-07 14:31:37 +0800

    f

--- packages/ueberzugpp/build-ueberzugpp.ps1
+++ packages/ueberzugpp/build-ueberzugpp.ps1
@@ -5,7 +5,7 @@ cmake -S . -B build `
     -G "Ninja" `
     -DVERBOSE=ON `
     -DCMAKE_BUILD_TYPE=Release `
-    -DCMAKE_INSTALL_PREFIX=$env:PREFIX `
+    -DCMAKE_INSTALL_PREFIX="$env:PREFIX" `
     -DENABLE_X11=OFF `
     -DENABLE_OPENCV=OFF
 cmake --build build --config Release --target install
