6414740b9ab82bdf475941f0f663f772961bd394
Glatzel <chenzelin9794@gmail.com>
2026-02-07 10:50:39 +0800

    f

--- packages/easytier/build-easytier.ps1
+++ packages/easytier/build-easytier.ps1
@@ -1,3 +1,7 @@
 $ROOT = git rev-parse --show-toplevel
 . $ROOT/scripts/util.ps1
+if($IsLinux){
+    $env:CFLAGS="-std=c11"
+    $env:CXXFLAGS="-std=c++11"
+}
 cargo install @(Get-Cargo-Arg) easytier
