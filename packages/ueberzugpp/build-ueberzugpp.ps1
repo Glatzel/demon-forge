dcb150752d1c17966a65a19acf7c57401108b1ef
Glatzel <chenzelin9794@gmail.com>
2026-02-07 13:20:54 +0800

    f

---
+++ packages/ueberzugpp/build-ueberzugpp.ps1
@@ -0,0 +1,12 @@
+$ROOT = git rev-parse --show-toplevel
+. $ROOT/scripts/util.ps1
+
+cmake -S . -B build `
+    -G "Ninja" `
+    -DVERBOSE=ON `
+    -DCMAKE_BUILD_TYPE=Release `
+    -DCMAKE_INSTALL_PREFIX=$env:PREFIX `
+    -DENABLE_X11=OFF `
+    -DENABLE_OPENCV=OFF
+cmake --build build --config Release --target install
+
