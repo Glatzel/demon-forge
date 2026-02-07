6fb8a834b36e514e43d3e1c1b97f6cdb6c314660
Glatzel <chenzelin9794@gmail.com>
2026-02-07 14:53:49 +0800

    feat: add ueberzugpp (#790)
    Co-authored-by: pre-commit-ci-lite[bot] <117423508+pre-commit-ci-lite[bot]@users.noreply.github.com>

---
+++ packages/ueberzugpp/ci-ueberzugpp.ps1
@@ -0,0 +1,6 @@
+Set-Location $PSScriptRoot
+$ROOT = git rev-parse --show-toplevel
+. $ROOT/scripts/util.ps1
+$latest_version = get-version-github -repo "jstkdng/ueberzugpp"
+update-recipe -version $latest_version
+build-pkg
