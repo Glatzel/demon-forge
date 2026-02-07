ace35d8b9e8dbb8f0ec96e742a9e2f07ab4440a4
Glatzel <chenzelin9794@gmail.com>
2026-02-07 13:24:05 +0800

    Update ci-audacity.ps1

--- packages/audacity/ci-audacity.ps1
+++ packages/audacity/ci-audacity.ps1
@@ -1,7 +1,8 @@
 Set-Location $PSScriptRoot
 $ROOT = git rev-parse --show-toplevel
 . $ROOT/scripts/util.ps1
-$latest_version = get-version-github -repo "https://github.com/$name/$name"
+$latest_version = get-version-github -repo "$name/$name"
 $latest_version = "$latest_version".Replace("Audacity-", "")
 update-recipe -version $latest_version
 build-pkg
+
