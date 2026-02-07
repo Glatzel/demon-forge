cd2124c1482959b41ac85fe6cc8131684ffeb357
Glatzel <chenzelin9794@gmail.com>
2026-02-07 11:37:46 +0800

    f

--- packages/termscp/build-termscp.ps1
+++ packages/termscp/build-termscp.ps1
@@ -1,8 +1,9 @@
 $ROOT = git rev-parse --show-toplevel
 . $ROOT/scripts/util.ps1
 if ($IsLinux) {
+    dnf update -y
     dnf install -y samba-devel
-     $Env:PKG_CONFIG_PATH = "/usr/share/pkgconfig`:/usr/lib/pkgconfig`:/usr/lib64/pkgconfig`:$Env:PKG_CONFIG_PATH"
+    $Env:PKG_CONFIG_PATH = "/usr/share/pkgconfig`:/usr/lib/pkgconfig`:/usr/lib64/pkgconfig`:$Env:PKG_CONFIG_PATH"
 }
 if ($IsMacOS) {
     brew install samba
