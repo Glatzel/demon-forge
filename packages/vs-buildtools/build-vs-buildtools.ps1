572cd40a3ef45685fb7c2328dcb5b1fc180f4b92
Glatzel <chenzelin9794@gmail.com>
2026-02-08 12:17:17 +0800

    f

--- packages/vs-buildtools/build-vs-buildtools.ps1
+++ packages/vs-buildtools/build-vs-buildtools.ps1
@@ -1,9 +1,9 @@
 $ROOT = git rev-parse --show-toplevel
 . $ROOT/scripts/util.ps1
-
+$version=$([version]"$env:PKG_VERSION").Major
 aria2c -c -x16 -s16 -d "$env:PREFIX/bin" `
-    "https://aka.ms/vs/$env:PKG_VERSION/release/vs_BuildTools.exe" `
+    "https://aka.ms/vs/$version/release/vs_BuildTools.exe" `
     -o "vs_BuildTools.exe"
 # shortcut
 New-Item $env:PREFIX/Menu -ItemType Directory
-Copy-Item "$name.json" "$env:PREFIX/Menu"
+Copy-Item "$env:RECIPE_DIR/$name.json" "$env:PREFIX/Menu"
