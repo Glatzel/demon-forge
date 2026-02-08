0c6de210fcd19da8dfa3b1ab4a3c86b993765f93
Glatzel <chenzelin9794@gmail.com>
2026-02-08 23:51:00 +0800

    chore: Update recipe.yaml (#824)
    Co-authored-by: pre-commit-ci-lite[bot] <117423508+pre-commit-ci-lite[bot]@users.noreply.github.com>

--- packages/dism-plus-plus/build-dism-plus-plus.ps1
+++ packages/dism-plus-plus/build-dism-plus-plus.ps1
@@ -1,6 +1,6 @@
 gh release download -R Chuyu-Team/Dism-Multi-language -p "Dism*.zip" `
     -O  "./${env:PKG_NAME}.zip"
-7z x "${env:PKG_NAME}.zip"  "-o$env:PREFIX/bin/$env:PKG_NAME"
+7z x "${env:PKG_NAME}.zip"  "-o$env:PREFIX/bin"
 # shortcut
 New-Item $env:PREFIX/Menu -ItemType Directory
 Copy-Item "${env:RECIPE_DIR}/${env:PKG_NAME}.json" "$env:PREFIX/Menu"
