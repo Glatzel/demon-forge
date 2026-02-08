983a1c55f46a7243b7e819628f3894546b06619b
Glatzel <chenzelin9794@gmail.com>
2026-02-08 12:22:05 +0800

    build: Update download URL for BuildTools executable (#808)

--- packages/vs-buildtools/build-vs-buildtools.ps1
+++ packages/vs-buildtools/build-vs-buildtools.ps1
@@ -1,8 +1,7 @@
 $ROOT = git rev-parse --show-toplevel
 . $ROOT/scripts/util.ps1
-$version=$([version]"$env:PKG_VERSION").Major
 aria2c -c -x16 -s16 -d "$env:PREFIX/bin" `
-    "https://aka.ms/vs/$version/release/vs_BuildTools.exe" `
+    "https://aka.ms/vs/$env:PKG_VERSION/release/vs_BuildTools.exe" `
     -o "vs_BuildTools.exe"
 # shortcut
 New-Item $env:PREFIX/Menu -ItemType Directory
