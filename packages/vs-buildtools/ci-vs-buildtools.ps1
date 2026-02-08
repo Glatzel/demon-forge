3fce12a866190f0408873a6b9bb2b69b0f24b238
Glatzel <chenzelin9794@gmail.com>
2026-02-08 12:06:48 +0800

    f

--- packages/vs-buildtools/ci-vs-buildtools.ps1
+++ packages/vs-buildtools/ci-vs-buildtools.ps1
@@ -2,6 +2,6 @@ Set-Location $PSScriptRoot
 $ROOT = git rev-parse --show-toplevel
 . $ROOT/scripts/util.ps1
 
-$latest_version = get-version-url  "https://learn.microsoft.com/en-us/visualstudio/releases/" '(\d+)\.\d+\.\d+'
+$latest_version = get-version-url  "https://learn.microsoft.com/en-us/visualstudio/releases/" '#(\d+)\.\d+\.\d+' 
 update-recipe -version $latest_version
 # build-pkg
