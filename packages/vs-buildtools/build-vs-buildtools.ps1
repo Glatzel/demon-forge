87dc134ef9c36e7af8615160069481a909e12ca3
Glatzel <chenzelin9794@gmail.com>
2026-02-08 12:18:13 +0800

    feat: add vs-buildtools (#807)
    Co-authored-by: pre-commit-ci-lite[bot] <117423508+pre-commit-ci-lite[bot]@users.noreply.github.com>

---
+++ packages/vs-buildtools/build-vs-buildtools.ps1
@@ -0,0 +1,9 @@
+$ROOT = git rev-parse --show-toplevel
+. $ROOT/scripts/util.ps1
+$version=$([version]"$env:PKG_VERSION").Major
+aria2c -c -x16 -s16 -d "$env:PREFIX/bin" `
+    "https://aka.ms/vs/$version/release/vs_BuildTools.exe" `
+    -o "vs_BuildTools.exe"
+# shortcut
+New-Item $env:PREFIX/Menu -ItemType Directory
+Copy-Item "$env:RECIPE_DIR/$name.json" "$env:PREFIX/Menu"
