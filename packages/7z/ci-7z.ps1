566db2eacfcec4461bbeb6d4550092c0bfe2c6e0
Glatzel <chenzelin9794@gmail.com>
2026-02-08 20:08:53 +0800

    f

--- packages/7z/ci-7z.ps1
+++ packages/7z/ci-7z.ps1
@@ -1,5 +1,5 @@
 Set-Location $PSScriptRoot
 $ROOT = git rev-parse --show-toplevel
 . $ROOT/scripts/util.ps1
-$latest_version = get-version-text $(pixi search 7zip) 'Version\s+([0-9]+\.[0-9]+)'
+$latest_version = [Version](get-version-text $(pixi search 7zip) 'Version\s+([0-9]+\.[0-9]+)')
 dispatch-workflow $latest_version
