d374798b501660a3134ff63c271723c537c81085
Glatzel <chenzelin9794@gmail.com>
2026-02-08 12:30:28 +0800

    Update build-vs-buildtools.ps1

--- packages/vs-buildtools/build-vs-buildtools.ps1
+++ packages/vs-buildtools/build-vs-buildtools.ps1
@@ -1,7 +1,7 @@
 $ROOT = git rev-parse --show-toplevel
 . $ROOT/scripts/util.ps1
 aria2c -c -x16 -s16 -d "$env:PREFIX/bin" `
-    "https://aka.ms/vs/$env:PKG_VERSION/release/vs_BuildTools.exe" `
+    "https://aka.ms/vs/$env:PKG_VERSION/stable/vs_BuildTools.exe" `
     -o "vs_BuildTools.exe"
 # shortcut
 New-Item $env:PREFIX/Menu -ItemType Directory
