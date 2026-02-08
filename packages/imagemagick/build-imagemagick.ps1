c49fdf7e8feaf9ee2ca7cd6a43ff5b9fa7e3753a
Glatzel <chenzelin9794@gmail.com>
2026-02-08 23:37:47 +0800

    chore: Update build-imagemagick.ps1

--- packages/imagemagick/build-imagemagick.ps1
+++ packages/imagemagick/build-imagemagick.ps1
@@ -3,6 +3,4 @@ gh release download `
     -p "ImageMagick-*-portable-Q16-HDRI-x64.7z" `
     -O  ./${env:PKG_NAME}.7z `
 
-7z x "${env:PKG_NAME}.7z" "-o./${env:PKG_NAME}"
-New-Item $env:PREFIX/bin/${env:PKG_NAME} -ItemType Directory
-Copy-Item "./${env:PKG_NAME}/*" "$env:PREFIX/bin/${env:PKG_NAME}" -Recurse
+7z x "${env:PKG_NAME}.7z" "-o$env:PREFIX/bin"
