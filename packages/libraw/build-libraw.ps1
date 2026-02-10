a74ee717fd16d3943b02b493c6de91bf602df9a5
Glatzel <chenzelin9794@gmail.com>
2026-02-10 22:26:39 +0800

    Update build-libraw.ps1

--- packages/libraw/build-libraw.ps1
+++ packages/libraw/build-libraw.ps1
@@ -1,12 +1,7 @@
-$ROOT = git rev-parse --show-toplevel
-. $ROOT/scripts/util.ps1
 if ($IsWindows) {
     $env:CMAKE_INSTALL_PREFIX = "$env:PREFIX/Library"
 }
-if ($IsMacOS) {
-    $env:CMAKE_INSTALL_PREFIX = "$env:PREFIX"
-}
-if ($IsLinux) {
+else {
     $env:CMAKE_INSTALL_PREFIX = "$env:PREFIX"
 }
 Copy-Item ./LibRaw-cmake/* ./ -Recurse
