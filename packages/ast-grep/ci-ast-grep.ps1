e2817f878b9800f316adeff4cb8f28c4f7502065
Glatzel <chenzelin9794@gmail.com>
2026-02-10 10:24:52 +0800

    Update ci-ast-grep.ps1

--- packages/ast-grep/ci-ast-grep.ps1
+++ packages/ast-grep/ci-ast-grep.ps1
@@ -1,5 +1,6 @@
 Set-Location $PSScriptRoot
 $ROOT = git rev-parse --show-toplevel
 . $ROOT/scripts/util.ps1
+$env:CONDA_OVERRIDE_GLIBC='2.28'
 $latest_version = get-version-crateio -name $name
 dispatch-workflow $latest_version
