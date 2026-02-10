75434a489c7a50d0fb1a32a14158af8982678908
Glatzel <chenzelin9794@gmail.com>
2026-02-10 22:20:11 +0800

    Update util.ps1

--- scripts/util.ps1
+++ scripts/util.ps1
@@ -3,6 +3,7 @@ $ROOT = git rev-parse --show-toplevel
 $ErrorActionPreference = "Stop"
 $PSNativeCommandUseErrorActionPreference = $true
 Remove-Item Alias:curl -ErrorAction SilentlyContinue
+$env:CMAKE_GENERATOR='Ninja'
 if ($IsWindows) {
     $env:PYTHONPATH = "$ROOT;$env:PYTHONPATH"
     # avoid build error by long path
