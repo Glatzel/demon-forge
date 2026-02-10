2d3f3f92f3b6effda36afc413299c5938f46673a
Glatzel <chenzelin9794@gmail.com>
2026-02-10 22:12:10 +0800

    Update build-proj.ps1

--- packages/proj/build-proj.ps1
+++ packages/proj/build-proj.ps1
@@ -13,7 +13,7 @@ if ($IsLinux) {
     $env:CMAKE_INSTALL_PREFIX = "$env:PREFIX"
 }
 
-cmake -S . -B build -`
+cmake -S . -B build `
     -DCMAKE_BUILD_TYPE="RELEASE" `
     -DBUILD_APPS=ON `
     -DBUILD_SHARED_LIBS=ON `
