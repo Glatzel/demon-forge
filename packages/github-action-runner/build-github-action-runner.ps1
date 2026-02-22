fc065c9ba1db196910702fc5b89c0c2d6351bfc2
Glatzel <chenzelin9794@gmail.com>
2026-02-22 23:13:34 +0800

    Update build-github-action-runner.ps1

--- packages/github-action-runner/build-github-action-runner.ps1
+++ packages/github-action-runner/build-github-action-runner.ps1
@@ -1,19 +1,19 @@
 $ROOT = git rev-parse --show-toplevel
 . $ROOT/scripts/util.ps1
 if ($IsWindows) {
-    gh release download -R "github/action" -p "*win-x64*" `
+    gh release download -R "actions/runner" -p "*win-x64*" `
         -O "${env:PKG_NAME}.zip"
 }
 if ($IsMacos) {
-    gh release download -R "github/action" -p "*osx-arm64*" `
+    gh release download -R "actions/runner" -p "*osx-arm64*" `
         -O "${env:PKG_NAME}.tar.gz"
 }
 if ($env:TARGET_PLATFORM -eq 'linux-64') {
-    gh release download -R "github/action" -p "*linux-x64*" `
+    gh release download -R "actions/runner" -p "*linux-x64*" `
         -O "${env:PKG_NAME}.tar.gz"
 }
 if ($env:TARGET_PLATFORM -eq 'linux-aarch64') {
-    gh release download -R "github/action" -p "*linux-arm64*" `
+    gh release download -R "actions/runner" -p "*linux-arm64*" `
         -O "${env:PKG_NAME}.tar.gz"
 }
 if ($IsWindows) {
