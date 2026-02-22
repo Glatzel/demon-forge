1ac2ab2b5bf4197a78863f736e4e2f9699781b9f
Glatzel <chenzelin9794@gmail.com>
2026-02-22 23:12:37 +0800

    Update ci-github-action-runner.ps1

--- packages/github-action-runner/ci-github-action-runner.ps1
+++ packages/github-action-runner/ci-github-action-runner.ps1
@@ -1,5 +1,5 @@
 Set-Location $PSScriptRoot
 $ROOT = git rev-parse --show-toplevel
 . $ROOT/scripts/util.ps1
-$latest_version = get-version-github -repo "action/runner"
+$latest_version = get-version-github -repo "actions/runner"
 dispatch-workflow $latest_version
