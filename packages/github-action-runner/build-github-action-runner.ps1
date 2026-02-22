031fc1430ad5420b7a6d1375bf461a2e83cfedcb
Glatzel <chenzelin9794@gmail.com>
2026-02-22 23:11:20 +0800

    Update build-github-action-runner.ps1

--- packages/github-action-runner/build-github-action-runner.ps1
+++ packages/github-action-runner/build-github-action-runner.ps1
@@ -1,22 +1,25 @@
 $ROOT = git rev-parse --show-toplevel
 . $ROOT/scripts/util.ps1
 if ($IsWindows) {
-    gh release download -R "containerd/nerdctl" -p "nerdctl-*-windows-amd64.tar.gz" `
-        -O "${env:PKG_NAME}.tar.gz"
+    gh release download -R "github/action" -p "*win-x64*" `
+        -O "${env:PKG_NAME}.zip"
 }
 if ($IsMacos) {
-    gh release download -R "containerd/nerdctl" -p "nerdctl-*-windows-amd64.tar.gz" `
+    gh release download -R "github/action" -p "*osx-arm64*" `
         -O "${env:PKG_NAME}.tar.gz"
 }
 if ($env:TARGET_PLATFORM -eq 'linux-64') {
-    gh release download -R "containerd/nerdctl" -p "nerdctl-?.*.*-linux-amd64.tar.gz" `
+    gh release download -R "github/action" -p "*linux-x64*" `
         -O "${env:PKG_NAME}.tar.gz"
 }
 if ($env:TARGET_PLATFORM -eq 'linux-aarch64') {
-    gh release download -R "containerd/nerdctl" -p "nerdctl-?.*.*-linux-arm64.tar.gz" `
+    gh release download -R "github/action" -p "*linux-arm64*" `
         -O "${env:PKG_NAME}.tar.gz"
 }
+if ($IsWindows) {
+7z x "${env:PKG_NAME}.zip" "-o$env:PREFIX"
+}
+else{
 7z x "${env:PKG_NAME}.tar.gz"
-7z x "${env:PKG_NAME}.tar" "-o${env:PKG_NAME}"
-New-Item $env:PREFIX/bin -ItemType Directory
-Copy-Item "./${env:PKG_NAME}/*" "$env:PREFIX/bin/" -Recurse
+7z x "${env:PKG_NAME}.tar" "-o$env:PREFIX"
+}
