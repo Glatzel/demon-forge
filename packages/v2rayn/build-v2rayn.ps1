303b6a4a693fe1f57f5a9e1e668737046bd130f1
Glatzel <893016099@qq.com>
2026-02-23 17:28:40 +0800

    更新 build-v2rayn.ps1

--- packages/v2rayn/build-v2rayn.ps1
+++ packages/v2rayn/build-v2rayn.ps1
@@ -3,18 +3,10 @@ if ($IsWindows) {
     gh release download -R 2dust/v2rayN -p "v2rayN-windows-64-desktop.zip" `
         -O  ./${env:PKG_NAME}.zip
 }
-# if ($IsMacOS) {
-#     gh release download -R 2dust/v2rayN -p "v2rayN-macos-arm64.zip" `
-#         -O  ./${env:PKG_NAME}.zip
-# }
-# if ($env:TARGET_PLATFORM -eq 'linux-64') {
-#     gh release download -R 2dust/v2rayN -p "v2rayN-linux-64.zip" `
-#         -O  ./${env:PKG_NAME}.zip
-# }
-# if ($env:TARGET_PLATFORM -eq 'linux-aarch64') {
-#     gh release download -R 2dust/v2rayN -p "v2rayN-linux-arm64.zip" `
-#         -O  ./${env:PKG_NAME}.zip# }
-7z x "${env:PKG_NAME}.zip" "-o$env:PREFIX/bin/${env:PKG_NAME}"
+7z x "${env:PKG_NAME}.zip" "-o./${env:PKG_NAME}" 
+ if ($IsWindows) { 
+     Copy-Item "./${env:PKG_NAME}/v2rayN-windows-64/*" "$env:PREFIX/bin/${env:PKG_NAME}/" -Recurse 
+ }
 # shortcut
 New-Item $env:PREFIX/Menu -ItemType Directory
 Copy-Item "${env:RECIPE_DIR}/${env:PKG_NAME}.json" "$env:PREFIX/Menu"
