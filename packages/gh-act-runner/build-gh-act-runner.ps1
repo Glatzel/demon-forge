0b6b49bdce29dd36bd94dc14232673165cedce24
Glatzel <chenzelin9794@gmail.com>
2026-02-23 10:26:25 +0800

    feat: add gh-act-runner

---
+++ packages/gh-act-runner/build-gh-act-runner.ps1
@@ -0,0 +1,20 @@
+$ROOT = git rev-parse --show-toplevel
+. $ROOT/scripts/util.ps1
+if ($IsWindows) {
+    gh release download -R "ChristopherHX/github-act-runner" -p "binary-windows-amd64.tar.gz" `
+        -O "${env:PKG_NAME}.zip"
+}
+if ($IsMacos) {
+    gh release download -R "ChristopherHX/github-act-runner" -p "binary-darwin-arm64.tar.gz" `
+        -O "${env:PKG_NAME}.tar.gz"
+}
+if ($env:TARGET_PLATFORM -eq 'linux-64') {
+    gh release download -R "ChristopherHX/github-act-runner" -p "binary-linux-amd64.tar.gz" `
+        -O "${env:PKG_NAME}.tar.gz"
+}
+if ($env:TARGET_PLATFORM -eq 'linux-aarch64') {
+    gh release download -R "ChristopherHX/github-act-runner" -p "binary-linux-arm64.tar.gz" `
+        -O "${env:PKG_NAME}.tar.gz"
+}
+7z x "${env:PKG_NAME}.tar.gz"
+7z x "${env:PKG_NAME}.tar" "-o$env:PREFIX/bin"
