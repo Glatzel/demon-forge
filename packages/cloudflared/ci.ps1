65acd0ba712635dc645389dd42188e08d580d62b
Glatzel <chenzelin9794@gmail.com>
2026-02-26 09:24:44 +0800

    f

---
+++ packages/cloudflared/ci.ps1
@@ -0,0 +1,5 @@
+Set-Location $PSScriptRoot
+$ROOT = git rev-parse --show-toplevel
+. $ROOT/scripts/util.ps1
+$latest_version = get-version-github -repo "cloudflare/$name"
+dispatch-workflow $latest_version
---
+++ packages/imagemagick/ci.ps1
@@ -0,0 +1,6 @@
+Set-Location $PSScriptRoot
+$ROOT = git rev-parse --show-toplevel
+. $ROOT/scripts/util.ps1
+$latest_version = get-version-github -repo "ImageMagick/ImageMagick"
+$latest_version = "$latest_version".Replace("-", ".")
+dispatch-workflow $latest_version
