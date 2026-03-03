63df0d03156f1194894a59c267d1391605502c38
Glatzel <chenzelin9794@gmail.com>
2026-03-03 15:32:35 +0800

    Update PROJ_ROOT path resolution in build.ps1

--- packages/pyxis-cli/build.ps1
+++ packages/pyxis-cli/build.ps1
@@ -1,8 +1,8 @@
 Set-Location rust
 if ($IsWindows) {
-    $env:PROJ_ROOT = "$(Resolve-Path $env:BUILD_PREFIX/Library)"
+    $env:PROJ_ROOT = "$(Resolve-Path ../)"
 }
 else {
-    $env:PROJ_ROOT = "$(Resolve-Path $env:BUILD_PREFIX)"
+    $env:PROJ_ROOT = "$(Resolve-Path ../)"
 }
 cargo install --bin pyxis --root $env:PREFIX --path ./crates/pyxis-cli
