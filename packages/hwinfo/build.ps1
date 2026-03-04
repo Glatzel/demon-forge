1cb2a2342cfc1180d02863a5c7a3b2d3a5334f7e
Glatzel <chenzelin9794@gmail.com>
2026-03-04 19:21:32 +0800

    feat: modify hwinfo path (#927)

--- packages/hwinfo/build.ps1
+++ packages/hwinfo/build.ps1
@@ -4,7 +4,7 @@ $version="$env:PKG_VERSION".Replace(".","")
 aria2c -c -x16 -s16 -d ./ `
     "https://www.hwinfo.com/files/hwi_$version.zip" `
     -o "${env:PKG_NAME}.zip"
-7z x "${env:PKG_NAME}.zip" "-o$env:PREFIX/bin/${env:PKG_NAME}"
+7z x "${env:PKG_NAME}.zip" "-o$env:PREFIX/bin"
 # shortcut
 New-Item $env:PREFIX/Menu -ItemType Directory
 Copy-Item "${env:RECIPE_DIR}/${env:PKG_NAME}.json" "$env:PREFIX/Menu"
