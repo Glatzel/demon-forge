1bbf1b63693be09802911d944d71ccb0001a7945
Glatzel <chenzelin9794@gmail.com>
2026-03-04 19:21:00 +0800

    Remove unnecessary line in build.ps1

--- packages/blender-benchmark/build.ps1
+++ packages/blender-benchmark/build.ps1
@@ -3,5 +3,4 @@ $ROOT = git rev-parse --show-toplevel
 aria2c -c -x16 -s16 -d ./ `
     "https://download.blender.org/release/BlenderBenchmark2.0/launcher/benchmark-launcher-cli-${env:PKG_VERSION}-windows.zip" `
     -o "${env:PKG_NAME}.zip"
-
 7z x "$zipfile" "-o$env:PREFIX/bin"
