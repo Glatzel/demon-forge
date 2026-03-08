2d9e95feacf63f24baf53b51a2fcdf7c33b0b066
Glatzel <chenzelin9794@gmail.com>
2026-03-08 15:11:02 +0800

    f

--- packages/opencolorio/build-opencolorio.ps1
+++ packages/opencolorio/build-opencolorio.ps1
@@ -1,23 +1,37 @@
+$cmakeArgs = @(
+    "-B build -G Ninja"
+    "-DCMAKE_BUILD_TYPE=Release"
+    "-DOCIO_INSTALL_EXT_PACKAGES=MISSING"
+    "-DCMAKE_BUILD_TYPE=Release"
+    "-DBUILD_SHARED_LIBS=ON"
+    "-DOCIO_BUILD_PYTHON=OFF"
+    "-DOCIO_BUILD_OPENFX=OFF"
+    "-DOCIO_USE_SIMD=ON"
+    "-DOCIO_BUILD_TESTS=OFF"
+    "-DOCIO_BUILD_GPU_TESTS=OFF"
+    "-DOCIO_USE_HEADLESS=OFF"
+    "-DOCIO_WARNING_AS_ERROR=OFF"
+    "-DOCIO_BUILD_DOCS=OFF"
+)
 if ($IsWindows) {
-    $env:CMAKE_INSTALL_PREFIX = "$ENV:PREFIX/Library"
+    $cmakeArgs += @("-CMAKE_INSTALL_PREFIX=${ENV:PREFIX}/Library")
 }
 else {
-    $env:CMAKE_INSTALL_PREFIX = "$ENV:PREFIX"
+    $cmakeArgs += @("-CMAKE_INSTALL_PREFIX=${ENV:PREFIX}")
 }
 
-cmake -S . -B build -G Ninja `
-    -DCMAKE_BUILD_TYPE=Release `
-    -DOCIO_INSTALL_EXT_PACKAGES=MISSING `
-    -DCMAKE_BUILD_TYPE=Release `
-    -DBUILD_SHARED_LIBS=ON `
-    -DOCIO_BUILD_APPS=OFF `
-    -DOCIO_USE_OIIO_FOR_APPS=OFF `
-    -DOCIO_BUILD_PYTHON=OFF `
-    -DOCIO_BUILD_OPENFX=OFF `
-    -DOCIO_USE_SIMD=ON `
-    -DOCIO_BUILD_TESTS=OFF `
-    -DOCIO_BUILD_GPU_TESTS=OFF `
-    -DOCIO_USE_HEADLESS=OFF `
-    -DOCIO_WARNING_AS_ERROR=OFF `
-    -DOCIO_BUILD_DOCS=OFF
+
+if ($env:PKG_NAME -eq 'opencolorio-base') {
+    $cmakeArgs += @(
+        "-DOCIO_BUILD_APPS=OFF"
+        "-DOCIO_USE_OIIO_FOR_APPS=OFF"
+    )
+}
+if ($env:PKG_NAME -eq 'opencolorio-app') {
+    $cmakeArgs += @(
+        "-DOCIO_BUILD_APPS=ON"
+        "-DOCIO_USE_OIIO_FOR_APPS=ON"
+    )
+}
+cmake -S . @cmakeArgs
 cmake --build build --config Release --target install
