7c72739bb0cf53a3f7ad01946f93dda62394453b
Glatzel <chenzelin9794@gmail.com>
2026-03-08 21:48:40 +0800

    Update build-libraw.ps1

--- packages/libraw/build-libraw.ps1
+++ packages/libraw/build-libraw.ps1
@@ -7,3 +7,4 @@ else {
 Copy-Item ./LibRaw-cmake/* ./ -Recurse
 cmake -S . -G Ninja -B build -DCMAKE_BUILD_TYPE="RELEASE"
 cmake --build build --config Release --target install
+fd -t f . $env:PREFIX
