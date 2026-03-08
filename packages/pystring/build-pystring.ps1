d4e34079309ce785528ca0b1d1ec9544e5787258
Glatzel <chenzelin9794@gmail.com>
2026-03-08 16:25:37 +0800

    f

--- packages/pystring/build-pystring.ps1
+++ packages/pystring/build-pystring.ps1
@@ -1,11 +1,16 @@
 $ROOT = git rev-parse --show-toplevel
 . $ROOT/scripts/util.ps1
+if ($IsWindows) {
+    $env:CMAKE_INSTALL_PREFIX = "$ENV:PREFIX/Library"
+}
+else {
+    $env:CMAKE_INSTALL_PREFIX = "$ENV:PREFIX"
+}
 $cmakeArgs = @(
     "-G"
     "Ninja"
     "-DVERBOSE=ON"
     "-DCMAKE_BUILD_TYPE=Release"
-    "-DCMAKE_INSTALL_PREFIX=$env:PREFIX"
 )
 cmake -S . -B build @cmakeArgs
 cmake --build build --config Release --target install
