a4cde853640ea32740ecbfb6b19f1b7e00659c19
Glatzel <chenzelin9794@gmail.com>
2026-03-08 11:37:00 +0800

    f

--- packages/libraw/build-libraw.ps1
+++ packages/libraw/build-libraw.ps1
@@ -1,4 +1,9 @@
-$env:CMAKE_INSTALL_PREFIX = "$env:PREFIX"
+if ($IsWindows) {
+    $env:CMAKE_INSTALL_PREFIX = "$env:PREFIX/Library"
+}
+else {
+    $env:CMAKE_INSTALL_PREFIX = "$env:PREFIX"
+}
 Copy-Item ./LibRaw-cmake/* ./ -Recurse
 cmake -S . -G Ninja -B build -DCMAKE_BUILD_TYPE="RELEASE"
 cmake --build build --config Release --target install
