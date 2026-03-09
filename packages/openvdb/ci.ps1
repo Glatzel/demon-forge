5ff2755c9b16db2624b1d91b0aa02b01d55c8b64
Glatzel <chenzelin9794@gmail.com>
2026-03-09 15:23:11 +0800

    Update ci.ps1

--- packages/openvdb/ci.ps1
+++ packages/openvdb/ci.ps1
@@ -1,5 +1,5 @@
 Set-Location $PSScriptRoot
 $ROOT = git rev-parse --show-toplevel
 . $ROOT/scripts/util.ps1
-$latest_version = get-version-github -repo "imageworks/pystring"
+$latest_version = get-version-github -repo "AcademySoftwareFoundation/openvdb"
 dispatch-workflow $latest_version
