b8caf94307860c0b560ec4bd764413f9575c6c89
Glatzel <chenzelin9794@gmail.com>
2026-03-17 11:05:28 +0800

    chore: Update ci.ps1

--- packages/toolbox/ci.ps1
+++ packages/toolbox/ci.ps1
@@ -2,5 +2,5 @@ Set-Location $PSScriptRoot
 $ROOT = git rev-parse --show-toplevel
 . $ROOT/scripts/util.ps1
 
-$latest_version = get-version-github -repo "Glatzel/$name"
+$latest_version = get-version-text $(gh api repos/Glatzel/toolbox/contents/python/pyproject.toml  --header "Accept: application/vnd.github.raw") 'version\s?=\s?"(\d+\.\d+\.\d+)"'
 dispatch-workflow $latest_version
