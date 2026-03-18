2730b2cc39d4e0b334d37118475e0e1a819be1f2
Glatzel <chenzelin9794@gmail.com>
2026-03-18 23:00:23 +0800

    f

--- packages/rustup/build.ps1
+++ packages/rustup/build.ps1
@@ -3,9 +3,9 @@ $ROOT = git rev-parse --show-toplevel
 cargo install --path . @(Get-Cargo-Arg)
 if ($IsWindows) {
     Rename-Item $env:PREFIX/bin/rustup-init.exe $env:PREFIX/bin/rustup.exe
-    Copy-Item $env:RECIPE_DIR/win/etc $env:PREFIX/
+    Copy-Item $env:RECIPE_DIR/win/* $env:PREFIX -Recurse
 }
 else {
     Rename-Item $env:PREFIX/bin/rustup-init $env:PREFIX/bin/rustup
-    Copy-Item $env:RECIPE_DIR/unix/etc $env:PREFIX/
+    Copy-Item $env:RECIPE_DIR/unix/* $env:PREFIX -Recurse
 }
