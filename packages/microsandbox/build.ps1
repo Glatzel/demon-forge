299f3d200c6f17f6ee438dd4a45289ea44edf020
Glatzel <chenzelin9794@gmail.com>
2026-03-20 23:13:05 +0800

    f

---
+++ packages/microsandbox/build.ps1
@@ -0,0 +1,21 @@
+$ROOT = git rev-parse --show-toplevel
+. $ROOT/scripts/util.ps1
+if($env:TARGET_PLATFROM -eq 'linux-64')
+{
+    gh release download -R zerocore-ai/microsandbox `
+        -p "libkrunfw-linux-x86_64.tar.gz" `
+        -O libkrunfw.tar.gz
+}
+if($env:TARGET_PLATFROM -eq 'linux-aarch64')
+{
+    gh release download -R zerocore-ai/microsandbox `
+        -p "libkrunfw-linux-aarch.tar.gz" `
+        -O libkrunfw.tar.gz
+}
+if($env:TARGET_PLATFROM -eq 'osx-arm64')
+{
+    gh release download -R zerocore-ai/microsandbox `
+        -p "microsandbox-darwin-aarch64.tar.gz" `
+        -O libkrunfw.tar.gz
+}
+7z x libkrunfw.tar.gz -so | 7z x -si -ttar -o$env:PREFIX/bin
