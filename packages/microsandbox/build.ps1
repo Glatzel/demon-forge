1cb7a67f5a77698460271ea76aaee1fa59ec9c82
Glatzel <chenzelin9794@gmail.com>
2026-03-21 00:02:06 +0800

    Update build.ps1

--- packages/microsandbox/build.ps1
+++ packages/microsandbox/build.ps1
@@ -4,18 +4,18 @@ if($env:TARGET_PLATFORM -eq 'linux-64')
 {
     gh release download -R zerocore-ai/microsandbox `
         -p "microsandbox-linux-x86_64.tar.gz" `
-        -O libkrunfw.tar.gz
+        -O microsandbox.tar.gz
 }
 if($env:TARGET_PLATFORM -eq 'linux-aarch64')
 {
     gh release download -R zerocore-ai/microsandbox `
         -p "microsandbox-linux-aarch64.tar.gz" `
-        -O libkrunfw.tar.gz
+        -O microsandbox.tar.gz
 }
 if($env:TARGET_PLATFORM -eq 'osx-arm64')
 {
     gh release download -R zerocore-ai/microsandbox `
         -p "microsandbox-darwin-aarch64.tar.gz" `
-        -O libkrunfw.tar.gz
+        -O microsandbox.tar.gz
 }
-7z x libkrunfw.tar.gz -so | 7z x -si -ttar "-o$env:PREFIX/bin"
+7z x microsandbox.tar.gz -so | 7z x -si -ttar "-o$env:PREFIX/bin"
