9aad00f15fc663b71872275d0b6360d644ce6025
Glatzel <chenzelin9794@gmail.com>
2026-03-21 15:58:14 +0800

    f

--- packages/microsandbox/microsandbox-kvm.sh
+++ packages/microsandbox/microsandbox-kvm.sh
@@ -15,21 +15,16 @@ if [ -r /dev/kvm ] && [ -w /dev/kvm ]; then
     return 0
 fi
 
-# Try ACL method
-if command -v setfacl >/dev/null 2>&1; then
-    sudo setfacl -m u:${USER}:rw /dev/kvm || true
-fi
-
-# Re-check access
-if [ -r /dev/kvm ] && [ -w /dev/kvm ]; then
-    echo "Access granted using ACL"
-    return 0
-fi
-
 # Try kvm group method
 if [ "$(stat -c "%G" /dev/kvm)" = "kvm" ]; then
+    echo "Try add User to kvm group."
     sudo usermod -aG kvm "$USER"
-    echo "User added to kvm group. Log out and log in again."
+    newgrp kvm
+    echo "User added to kvm group."
+    if [ ! -e /dev/kvm ]; then
+        echo "/dev/kvm still not found."
+    exit 1
+    fi
     return 0
 fi
 
