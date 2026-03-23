f4bcd239d34ddd68c368c54eb93b3921b827fcec
Glatzel <chenzelin9794@gmail.com>
2026-03-23 12:26:00 +0800

    f

--- packages/dnc-init/build.ps1
+++ packages/dnc-init/build.ps1
@@ -1,4 +1,2 @@
-if ($IsWindows) {
-    new-item $env:PREFIX/etc/conda/activate.d -itemType Directory -Force -ErrorAction SilentlyContinue
-    copy-item $env:RECIPE_DIR/dnc-init.ps1 $env:PREFIX/etc/conda/activate.d/
-}
+new-item $env:PREFIX/etc/conda/activate.d -itemType Directory -Force -ErrorAction SilentlyContinue
+copy-item $env:RECIPE_DIR/dnc-init.ps1 $env:PREFIX/etc/conda/activate.d/
