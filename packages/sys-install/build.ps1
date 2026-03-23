49de5f0b8f5695947815056dc660fe872524bb50
Glatzel <chenzelin9794@gmail.com>
2026-03-23 22:39:51 +0800

    f

--- packages/sys-install/build.ps1
+++ packages/sys-install/build.ps1
@@ -1,3 +1,3 @@
 new-item $env:PREFIX/Scripts -itemType Directory -Force -ErrorAction SilentlyContinue
-Copy-Item $env:PREFIX/Scripts/sys-install.ps1 $env:PREFIX/Scripts/sys-install.ps1
-Copy-Item $env:PREFIX/Scripts/sys-install.bat $env:PREFIX/Scripts/sys-install.bat
+Copy-Item $env:RECIPE_DIR/sys-install.ps1 $env:PREFIX/Scripts/sys-install.ps1
+Copy-Item $env:RECIPE_DIR/sys-install.bat $env:PREFIX/Scripts/sys-install.bat
--- packages/sys-rhino/build.ps1
+++ packages/sys-rhino/build.ps1
@@ -1,3 +1,3 @@
 new-item $env:PREFIX/Scripts -itemType Directory -Force -ErrorAction SilentlyContinue
-(Get-Content $env:RECIPE_DIR/rhino-init.ps1) -replace '\$template_version', "`$version=`"$env:PKG_VERSION`"" | Set-Content $env:PREFIX/Scripts/rhino-init.ps1
-Write-Output (Get-Content $env:PREFIX/Scripts/rhino-init.ps1)
+(Get-Content $env:RECIPE_DIR/sys-install-rhino.ps1) -replace '\$template_version', "`$version=`"$env:PKG_VERSION`"" | Set-Content $env:PREFIX/Scripts/sys-install-rhino.ps1
+Write-Output (Get-Content $env:PREFIX/Scripts/sys-install-rhino.ps1)
