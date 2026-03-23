5243adca3e6afa2fe416bb9901f854a178186c1b
Glatzel <chenzelin9794@gmail.com>
2026-03-23 13:01:21 +0800

    f

--- packages/dnc-init/build.ps1
+++ packages/dnc-init/build.ps1
@@ -1,7 +1,5 @@
-$content = Get-Content $env:RECIPE_DIR/dnc-init.ps1
-$content = $content -replace '$version', "`$version=`"$env:PKG_VERSION`""
 Write-Output $content
 new-item $env:PREFIX/scripts -itemType Directory -Force -ErrorAction SilentlyContinue
-Set-Content $env:PREFIX/scripts/dnc-init.ps1 $content
+Get-Content $env:RECIPE_DIR/dnc-init.ps1 -replace '$version', "`$version=`"$env:PKG_VERSION`"" | Set-Content $env:PREFIX/scripts/dnc-init.ps1
 new-item $env:PREFIX/etc/conda/activate.d -itemType Directory -Force -ErrorAction SilentlyContinue
 Copy-Item $env:RECIPE_DIR/dnc-init.bat $env:PREFIX/etc/conda/activate.d/
