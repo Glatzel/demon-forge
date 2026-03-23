73e19ecb06e06dd74fa9020ecc03856c61eada86
Glatzel <chenzelin9794@gmail.com>
2026-03-23 15:49:39 +0800

    Update rhino-init.ps1

--- packages/rhino-init/rhino-init.ps1
+++ packages/rhino-init/rhino-init.ps1
@@ -15,7 +15,7 @@ if ( $need_install) {
             "https://www.rhino3d.com/download/rhino-for-windows/8/latest/direct?email=users.noreply.github.com" `
             -o "rhino-$version.exe"
     }
-    Write-Output "install dnc $version"
+    Write-Output "install rhino $version"
     Start-Process "$PSScriptRoot/../temp/rhino-$version.exe" -ArgumentList "/silent" -Wait
     Write-Output "rhino installed"
 }
