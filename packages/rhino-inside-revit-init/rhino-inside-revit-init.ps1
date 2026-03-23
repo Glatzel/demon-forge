25a816b45bf87025c06e9e5671cb67e9780e4a55
Glatzel <chenzelin9794@gmail.com>
2026-03-23 17:01:33 +0800

    feat: add rhino-inside-revit-init (#1040)

---
+++ packages/rhino-inside-revit-init/rhino-inside-revit-init.ps1
@@ -0,0 +1,21 @@
+$template_version
+if (-not (Test-Path "C:\ProgramData\Autodesk\Revit\Addins\*\RhinoInside.Revit\RhinoInside.Revit.Loader.dll")) {
+    $need_install = $true
+}
+else {
+    if ((Get-Item "c:\ProgramData\Autodesk\Revit\Addins\*\RhinoInside.Revit\RhinoInside.Revit.Loader.dll").VersionInfo.FileVersion[0] -ne "$version") {
+        $need_install = $true
+    }
+    else { Write-Output "rhino.inside-revit $version is already installed." }
+}
+if ( $need_install) {
+    if (-not (Test-Path "$PSScriptRoot/../temp/rhino-$version.exe")) {
+        & "$PSScriptRoot/../bin/aria2c.exe" -c -x16 -s16 `
+            -d "$PSScriptRoot/../temp" `
+            "https://files.mcneel.com/rhino.inside/revit/dujour/RhinoInside.Revit_$version.msi" `
+            -o "rhino-inside-revit-$version.msi"
+    }
+    Write-Output "install rhino.inside-revit $version"
+    msiexec /i "rhino-inside-revit-$version.msi" /qn
+    Write-Output "rhino.inside-revit installed"
+}
