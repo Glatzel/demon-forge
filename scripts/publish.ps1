284fe56bca30e714a800d3f040556beaa8480576
Glatzel <chenzelin9794@gmail.com>
2026-03-23 14:12:58 +0800

    f

--- scripts/publish.ps1
+++ scripts/publish.ps1
@@ -2,8 +2,8 @@ param($pkg)
 Set-Location $PSScriptRoot
 Set-Location ..
 
-if (-not (Test-Path "./output/$env:TARGET_PLATFORM/$pkg-*.conda" )) {
-    throw "The specified path was not found: ./output/*/*.conda"
+if (-not (Test-Path "./output/$env:TARGET_PLATFORM/*.conda" )) {
+    throw "The specified path was not found: ./output/$env:TARGET_PLATFORM/*.conda"
 }
 foreach ($pkg_file in Get-ChildItem "./output/$env:TARGET_PLATFORM/*.conda") {
     Write-Output "::group:: upload $pkg"
