4b9c7476d6292a5d55ec0b20bff812d430aea49a
Glatzel <chenzelin9794@gmail.com>
2026-03-23 14:07:55 +0800

    Update publish.ps1

--- scripts/publish.ps1
+++ scripts/publish.ps1
@@ -3,9 +3,9 @@ Set-Location $PSScriptRoot
 Set-Location ..
 
 if (-not (Test-Path "./output/$env:TARGET_PLATFORM/$pkg-*.conda" )) {
-    throw "The specified path was not found: ./output/*/$pkg-*.conda"
+    throw "The specified path was not found: ./output/*/*.conda"
 }
-foreach ($pkg_file in Get-ChildItem "./output/$env:TARGET_PLATFORM/$pkg-*.conda") {
+foreach ($pkg_file in Get-ChildItem "./output/$env:TARGET_PLATFORM/*.conda") {
     Write-Output "::group:: upload $pkg"
     pixi run rattler-build upload prefix -s -c glatzel --generate-attestation $pkg_file
     Write-Output "::endgroup::"
