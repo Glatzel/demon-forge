4c40b6831c4741d00a6530d51b102f8c99b96910
Glatzel <chenzelin9794@gmail.com>
2026-03-23 12:32:03 +0800

    f

--- packages/dnc-init/build.ps1
+++ packages/dnc-init/build.ps1
@@ -1,2 +1,4 @@
+$content=Get-Content $env:RECIPE_DIR/dnc-init.ps1
+$content="`$version=`"$env:PKG_VERSION`"`n"+$content
 new-item $env:PREFIX/etc/conda/activate.d -itemType Directory -Force -ErrorAction SilentlyContinue
-copy-item $env:RECIPE_DIR/dnc-init.ps1 $env:PREFIX/etc/conda/activate.d/
+Set-Content $env:PREFIX/etc/conda/activate.d/dnc-init.ps1 $content
