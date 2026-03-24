3a2d4e67f060210041ec981db8fbecc9453746ba
Glatzel <chenzelin9794@gmail.com>
2026-03-24 08:33:07 +0800

    f

--- packages/sys-install/install-sys.ps1
+++ packages/sys-install/install-sys.ps1
@@ -1,5 +1,5 @@
-foreach ($f in Get-ChildItem $PSScriptRoot/sys-install-*.ps1) {
-    $pkg_name = ([System.IO.Path]::GetFileNameWithoutExtension($f)).Replace("sys-install-", "")
+foreach ($f in Get-ChildItem $PSScriptRoot/install-sys-*.ps1) {
+    $pkg_name = ([System.IO.Path]::GetFileNameWithoutExtension($f)).Replace("install-sys-", "")
     Write-Output "Install $pkg_name"
     & $f
 }
