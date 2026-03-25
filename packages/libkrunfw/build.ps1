9d5de1c89312bcfaaf88b481e7bea0d513f0b7f5
Glatzel <893016099@qq.com>
2026-03-25 13:37:21 +0800

    更新 build.ps1

--- packages/libkrunfw/build.ps1
+++ packages/libkrunfw/build.ps1
@@ -1,4 +1,6 @@
 $ROOT = git rev-parse --show-toplevel
 . $ROOT/scripts/util.ps1
-make
+dnf install -y 'dnf-command(builddep)' python3-pyelftools curl
+dnf builddep -y kernel
+make -j$(nproc)
 make install PREFIX=$PREFIX
