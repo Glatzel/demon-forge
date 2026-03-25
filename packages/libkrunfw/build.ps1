931203a28a41dd6a98479e189003ab9348c8317d
Glatzel <chenzelin9794@gmail.com>
2026-03-25 14:39:09 +0800

    f

--- packages/libkrunfw/build.ps1
+++ packages/libkrunfw/build.ps1
@@ -4,5 +4,5 @@ if ($env:TARGET_PLATFORM -eq "linux-64") {
     $pattern = "aarch64"
 }
 gh release download -R "containers/${env:PKG_NAME}" -p "${env:PKG_NAME}-$pattern.tgz" `
-    -O  ${env:PKG_NAME}.tgz
+    -O  ./${env:PKG_NAME}.tgz
 7z x "${env:PKG_NAME}.tgz" "-o${env:PREFIX}/lib"
