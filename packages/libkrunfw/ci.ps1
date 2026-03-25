65ac2cf5adb6b57107722bf7c9555275390ab400
Glatzel <chenzelin9794@gmail.com>
2026-03-25 14:15:20 +0800

    f

--- packages/libkrunfw/ci.ps1
+++ packages/libkrunfw/ci.ps1
@@ -2,11 +2,4 @@ Set-Location $PSScriptRoot
 $ROOT = git rev-parse --show-toplevel
 . $ROOT/scripts/util.ps1
 $latest_version = get-version-github -repo "containers/libkrunfw"
-gh repo clone containers/libkrunfw
-cd libkrunfw
-sudo apt-get update
-sudo apt-get install -y make gcc bc bison flex elfutils python3-pyelftools curl patch libelf-dev
-make
-make install
-
-# dispatch-workflow $latest_version
+dispatch-workflow $latest_version
