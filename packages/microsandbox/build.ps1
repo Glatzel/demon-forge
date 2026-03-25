97aa68685d33c349c27e4856a36cbf6dd1b8fa70
Glatzel <chenzelin9794@gmail.com>
2026-03-25 15:09:59 +0800

    f

--- packages/microsandbox/build.ps1
+++ packages/microsandbox/build.ps1
@@ -1,28 +1,22 @@
 $ROOT = git rev-parse --show-toplevel
 . $ROOT/scripts/util.ps1
-if($env:TARGET_PLATFORM -eq 'linux-64')
-{
-    gh release download -R zerocore-ai/microsandbox `
-        -p "microsandbox-linux-x86_64.tar.gz" `
-        -O microsandbox.tar.gz
+if ($env:TARGET_PLATFORM -eq 'linux-64') {
+    $pattern = "linux-x86_64"
 }
-if($env:TARGET_PLATFORM -eq 'linux-aarch64')
-{
-    gh release download -R zerocore-ai/microsandbox `
-        -p "microsandbox-linux-aarch64.tar.gz" `
-        -O microsandbox.tar.gz
+if ($env:TARGET_PLATFORM -eq 'linux-aarch64') {
+    $pattern = "linux-aarch64"
 }
-if($env:TARGET_PLATFORM -eq 'osx-arm64')
-{
-    gh release download -R zerocore-ai/microsandbox `
-        -p "microsandbox-darwin-aarch64.tar.gz" `
-        -O microsandbox.tar.gz
+if ($env:TARGET_PLATFORM -eq 'osx-arm64') {
+    $pattern = "darwin-aarch64"
 }
+gh release download -R zerocore-ai/microsandbox `
+    -p "microsandbox-$pattern.tar.gz" `
+    -O microsandbox.tar.gz
 7z x microsandbox.tar.gz -so | 7z x -si -ttar "-o$env:PREFIX/bin"
 chmod +x "$env:PREFIX/bin/msb"
 chmod +x "$env:PREFIX/bin/msbnet"
-if($IsLinux)
-{
+if ($IsLinux) {
+    Remove-Item $env:PREFIX/bin/libkrun*
     New-Item -Path $env:PREFIX/etc/conda/activate.d -ItemType Directory -Force -ErrorAction SilentlyContinue
     Copy-Item -Path $env:RECIPE_DIR/microsandbox-kvm.sh -Destination $env:PREFIX/etc/conda/activate.d
 }
