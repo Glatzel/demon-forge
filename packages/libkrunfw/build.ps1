ae169a99004a78a490e60554a2ab4d0dbd14e640
Glatzel <chenzelin9794@gmail.com>
2026-03-25 15:02:24 +0800

    feat: add libkrunfw (#1048)
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

---
+++ packages/libkrunfw/build.ps1
@@ -0,0 +1,11 @@
+if ($env:TARGET_PLATFORM -eq "linux-64") {
+    $pattern = "x86_64"
+}if ($env:TARGET_PLATFORM -eq "linux-aarch64") {
+    $pattern = "aarch64"
+}
+gh release download -R "containers/${env:PKG_NAME}" -p "${env:PKG_NAME}-$pattern.tgz" `
+    -O  ./${env:PKG_NAME}.tgz
+7z x "${env:PKG_NAME}.tgz"
+7z x "${env:PKG_NAME}.tar"
+New-Item $env:PREFIX/lib -ItemType Directory -ErrorAction SilentlyContinue
+Copy-Item ./lib64/* $env:PREFIX/lib/
