5a757e540dc377c42dc425fa3124db5c8300daac
Glatzel <chenzelin9794@gmail.com>
2026-03-26 00:20:28 +0800

    fix: build number (#1055)

--- scripts/util.ps1
+++ scripts/util.ps1
@@ -116,7 +116,7 @@ function dispatch-workflow {
         Write-Output "New version found."
         # Update version number and reset build number
         (Get-Content -Path "./recipe.yaml") -replace '^  version: .*', "  version: ""$version""" | Set-Content -Path "./recipe.yaml"
-        (Get-Content -Path "./recipe.yaml") -replace '^\s+number: .*', "  number: 0" | Set-Content -Path "./recipe.yaml"
+        (Get-Content -Path "./recipe.yaml") -replace '  number: .*', "  number: 0" | Set-Content -Path "./recipe.yaml"
         Write-Output "::endgroup::"
     }
 
