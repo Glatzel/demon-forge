8b8ec258d72ac6b3627e79bdc6d43ccc2b33122d
Glatzel <chenzelin9794@gmail.com>
2026-03-27 11:20:07 +0800

    d

---
+++ scripts/update.ps1
@@ -0,0 +1,14 @@
+# Input CSV
+$csvFile = "$PSScriptRoot/../packages.csv"
+# Output YAML
+$yamlFile = "$PSScriptRoot/../packages.yaml"
+Set-Content -Path $yamlFile -Value ""
+# Read CSV
+$csvData = Import-Csv $csvFile
+ForEach ($Row in $csvData) {
+    $name=$Row.pkg
+    Write-Output "::group::update $name"
+    Set-Location "$PSScriptRoot/../packages/$name"
+    & ci.ps1
+    Write-Output "::endgroup::"
+}
