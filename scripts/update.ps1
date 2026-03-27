1a9b6968bde7b99f4c9f6ca4744089916ed15afe
Glatzel <chenzelin9794@gmail.com>
2026-03-27 11:23:47 +0800

    f

--- scripts/update.ps1
+++ scripts/update.ps1
@@ -9,6 +9,6 @@ ForEach ($Row in $csvData) {
     $name=$Row.pkg
     Write-Output "::group::update $name"
     Set-Location "$PSScriptRoot/../packages/$name"
-    & ci.ps1
+    ./ci.ps1
     Write-Output "::endgroup::"
 }
