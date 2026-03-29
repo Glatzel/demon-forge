747eb780f7584eff74d15e23c2663cda6e3fbae8
Glatzel <893016099@qq.com>
2026-03-29 08:05:27 +0800

    更新 ci.ps1

--- packages/7-zip/ci.ps1
+++ packages/7-zip/ci.ps1
@@ -1,5 +1,5 @@
 Set-Location $PSScriptRoot
 $ROOT = git rev-parse --show-toplevel
 . $ROOT/scripts/util.ps1
-$latest_version = get-version-conda-forge 7zip
+$latest_version = [Version]get-version-conda-forge 7zip
 dispatch-workflow $latest_version
