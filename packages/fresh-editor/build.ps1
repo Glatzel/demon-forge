0973f2d17a13c5dafd20968365586802d55df737
Glatzel <chenzelin9794@gmail.com>
2026-03-31 18:47:13 +0800

    f

--- packages/fresh-editor/build.ps1
+++ packages/fresh-editor/build.ps1
@@ -1,8 +1,3 @@
 $ROOT = git rev-parse --show-toplevel
 . $ROOT/scripts/util.ps1
-
 cargo install $name @(Get-Cargo-Arg)
-Rename-Item $env:PREFIX/bin/unused-features.exe $env:PREFIX/bin/${env:PKG_NAME}.exe
-
-
-    Rename-Item $env:PREFIX/bin/unused-features $env:PREFIX/bin/${env:PKG_NAME}
