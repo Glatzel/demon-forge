e849278f1854a8a30eff18602741fe0affbe66df
Glatzel <chenzelin9794@gmail.com>
2026-03-31 20:49:16 +0800

    f

--- packages/pystring/build-pystring.sh
+++ packages/pystring/build-pystring.sh
@@ -2,5 +2,5 @@ cmake -S . -B build \
     -G Ninja \
     -DVERBOSE=ON \
     -DCMAKE_BUILD_TYPE=Release \
-    -DCMAKE_INSTALL_PREFIX="$PREFIX/Library"
+    -DCMAKE_INSTALL_PREFIX="$PREFIX"
 cmake --build build --config Release --target install
