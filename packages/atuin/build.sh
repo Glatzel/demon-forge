34da879b614b20e6410caca860db5d8ed83d60d4
Glatzel <chenzelin9794@gmail.com>
2026-03-31 18:15:16 +0800

    f

--- packages/atuin/build.sh
+++ packages/atuin/build.sh
@@ -1,4 +1,9 @@
 ROOT=$(git rev-parse --show-toplevel)
 . "$ROOT/scripts/util.sh"
+if [ "$(uname)" = "Linux" ]; then
+    OPENSSL_DIR="$BUILD_PREFIX"
+    export OPENSSL_DIR
+fi
 set -- $(get_cargo_arg)
-cargo install "$(get_name)" "$@"
+cargo install "$PKG_NAME" "$@"
+mv -f "$PREFIX/bin/unused-features" "$PREFIX/bin/$PKG_NAME"
