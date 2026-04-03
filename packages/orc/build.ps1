9c8833c54c95a5688b0da58a7fdf79462bd7ea25
Glatzel <chenzelin9794@gmail.com>
2026-04-03 11:55:04 +0800

    Update build.ps1

--- packages/orc/build.ps1
+++ packages/orc/build.ps1
@@ -1,4 +1,4 @@
 $ROOT = git rev-parse --show-toplevel
 . $ROOT/scripts/util.ps1
 Set-Location ./tools/vinaya
-cargo install --path . @(Get-Cargo-Arg)
+cargo install --path ./tools/orc @(Get-Cargo-Arg)
