00f08c1b26720a956fa58dfbe565bc2004e7c494
Glatzel <893016099@qq.com>
2026-04-09 14:15:29 +0800

    更新 build.sh

--- packages/shook/build.sh
+++ packages/shook/build.sh
@@ -2,5 +2,6 @@ cd $RECIPE_DIR
 ROOT=$(git rev-parse --show-toplevel)
 . "$ROOT/scripts/util.sh"
 cd $SRC_DIR
+dnf install -y libcap-ng-devel
 set -- $(get_cargo_arg)
 cargo install --path ./tools/shook "$@"
