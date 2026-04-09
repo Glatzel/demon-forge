166b95518a006a4e3524e72b19dc25b5914cbca1
Glatzel <893016099@qq.com>
2026-04-09 14:05:58 +0800

    更新 build.sh

--- packages/shook/build.sh
+++ packages/shook/build.sh
@@ -2,6 +2,5 @@ cd $RECIPE_DIR
 ROOT=$(git rev-parse --show-toplevel)
 . "$ROOT/scripts/util.sh"
 cd $SRC_DIR
-dnf install -y dbus-devel
 set -- $(get_cargo_arg)
 cargo install --path ./tools/shook "$@"
