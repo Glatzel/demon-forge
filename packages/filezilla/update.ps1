a6bd56a25d0df2461701f5fbf2fbbfb872cd1138
Glatzel <chenzelin9794@gmail.com>
2026-04-11 01:06:23 +0800

    Delete packages/filezilla directory

--- packages/filezilla/update.ps1
+++
@@ -1,5 +0,0 @@
-Set-Location $PSScriptRoot
-$ROOT = git rev-parse --show-toplevel
-. $ROOT/scripts/util.ps1
-$latest_version = get-version-url -url "https://filezilla-project.org/newsfeed.php" -pattern 'FileZilla Client (\d+\.\d+\.\d+) released'
-update-recipe $latest_version
