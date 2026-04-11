011d3da00edb060178832a571f313385f89a815d
Glatzel <chenzelin9794@gmail.com>
2026-04-11 08:40:40 +0800

    build: Update recipe.yaml (#1140)

--- packages/pyxis-cli/build.ps1
+++ packages/pyxis-cli/build.ps1
@@ -1,3 +1,3 @@
 Set-Location rust
-$env:PROJ_ROOT = "$(Resolve-Path $env:BUILD_PREFIX/Library)"
+$env:PROJ_ROOT = "$(Resolve-Path $env:PREFIX/Library)"
 cargo install --bin pyxis --root $env:PREFIX --path ./crates/pyxis-cli
