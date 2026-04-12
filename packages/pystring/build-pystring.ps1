e1dde3e61d3420b08c45f936faa60438b2b52265
Glatzel <chenzelin9794@gmail.com>
2026-04-12 18:53:51 +0800

    f

--- packages/pystring/build-pystring.ps1
+++ packages/pystring/build-pystring.ps1
@@ -2,5 +2,6 @@ cmake -S . -B build "-G" `
     "Ninja" `
     "-DVERBOSE=ON" `
     "-DCMAKE_BUILD_TYPE=Release" `
-    "-DCMAKE_INSTALL_PREFIX=$ENV:PREFIX/Library"
+    "-DCMAKE_INSTALL_PREFIX=$ENV:PREFIX/Library" `
+    "-DCMAKE_WINDOWS_EXPORT_ALL_SYMBOLS=ON"
 cmake --build build --config Release --target install
