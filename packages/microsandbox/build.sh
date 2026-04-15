eeb977e52126c5251a4ceeaed1581efb664b0ed4
Glatzel <893016099@qq.com>
2026-04-15 16:58:36 +0800

    更新 build.sh

--- packages/microsandbox/build.sh
+++ packages/microsandbox/build.sh
@@ -1,5 +1,5 @@
 ROOT=$(git rev-parse --show-toplevel)
 . "$ROOT/scripts/util.sh"
-export LIBRARY_PATH="$(realpath $PREFIX/lib)"
+export LIBRARY_PATH="$(realpath $PREFIX/lib):$LIBRARY_PATH"
 set -- $(get_cargo_arg)
 cargo install --path ./crates/cli "$@"
