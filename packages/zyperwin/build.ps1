de3d6ab9df669e33f1befa43327814b636d10041
Glatzel <chenzelin9794@gmail.com>
2026-04-17 12:06:10 +0800

    Delete packages/zyperwin directory

--- packages/zyperwin/build.ps1
+++
@@ -1,8 +0,0 @@
-gh release download -R ZyperWave/ZyperWinOptimize -p "ZyperWin*.zip" `
-    -O  ./${env:PKG_NAME}.zip
-7z x "${env:PKG_NAME}.zip" "-o./${env:PKG_NAME}"
-New-Item $env:PREFIX/bin -ItemType Directory
-Copy-Item "./${env:PKG_NAME}/Release/*" "$env:PREFIX/bin" -Recurse
-# shortcut
-New-Item $env:PREFIX/Menu -ItemType Directory
-Copy-Item "${env:RECIPE_DIR}/${env:PKG_NAME}.json" "$env:PREFIX/Menu"
