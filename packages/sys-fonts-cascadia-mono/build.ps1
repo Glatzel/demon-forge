60b2fccbbb0d5b8bca60f920610cf4be4be03adf
Glatzel <chenzelin9794@gmail.com>
2026-04-21 17:39:58 +0800

    Update build.ps1

--- packages/sys-fonts-cascadia-mono/build.ps1
+++ packages/sys-fonts-cascadia-mono/build.ps1
@@ -1,5 +1,5 @@
-gh release download -R "ryanoasis/nerd-fonts" -p "CascadiaMono.zip" -O ./font.zip --clobber
-7z x "./font.zip" "-o$env:PREFIX/fonts"
+gh release download -R "ryanoasis/nerd-fonts" -p "CascadiaMono.zip" -O ./fonts.zip --clobber
+7z x "./fonts.zip" "-o$env:PREFIX/fonts"
 foreach( $f in Get-ChildItem "$env:PREFIX/fonts/*.ttf")
 {
     pyftsubset $f `
