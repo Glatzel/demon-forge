2f9377718d18019a83786bb7034dfe541bc4b4fb
Glatzel <chenzelin9794@gmail.com>
2026-04-21 18:07:46 +0800

    feat: add nerd fonts (#1204)
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

---
+++ packages/sys-fonts-hack/build.ps1
@@ -0,0 +1,10 @@
+gh release download -R "ryanoasis/nerd-fonts" -p "Hack.zip" -O ./fonts.zip --clobber
+7z x "./fonts.zip" "-o$env:PREFIX/fonts"
+foreach( $f in Get-ChildItem "$env:PREFIX/fonts/*.ttf")
+{
+    pyftsubset $f `
+        --output-file="$env:PREFIX/fonts/$($f.BaseName).woff2" `
+        --flavor=woff2 `
+        --layout-features='*' `
+        --glyphs='*'
+}
