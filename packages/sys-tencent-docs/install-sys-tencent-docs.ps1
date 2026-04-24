deacbec4b0eb1be425b0ff936528445c4b861ee2
Glatzel <chenzelin9794@gmail.com>
2026-04-24 16:44:02 +0800

    fix: tencent doc (#1225)

--- packages/sys-tencent-docs/install-sys-tencent-docs.ps1
+++ packages/sys-tencent-docs/install-sys-tencent-docs.ps1
@@ -1,7 +1,1 @@
-if (-not (Test-Path "C:\Program Files\TencentDocs\TencentDocs.exe"))
-{
-    winget install --source winget -i --id Tencent.QQ.NT
-} else
-{
-    write-host "Tencent Docs is already installed."
-}
+winget install --source winget -i --id Tencent.TencentDocs
