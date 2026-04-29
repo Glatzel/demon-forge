c1a4be7b7d66dd48644c188cbee3b2b9258c2295
Glatzel <chenzelin9794@gmail.com>
2026-04-29 12:41:24 +0800

    f

---
+++ packages/ttyd/build.ps1
@@ -0,0 +1,32 @@
+# Append SCSS
+Get-Content "$env:RECIPE_DIR/index.scss" | Add-Content "./html/src/style/index.scss"
+
+# fonts
+New-Item -ItemType Directory -Force -Path "./html/src/style/webfont" | Out-Null
+
+# Copy fonts (PowerShell handles globbing differently)
+Copy-Item "$env:BUILD_PREFIX/fonts/*" -Destination "./html/src/style/webfont/" -ErrorAction Stop
+
+# Frontend build
+Set-Location ./html
+$env:NPM_CONFIG_PREFIX="$env:BUILD_PREFIX"
+npm install -g corepack
+corepack enable
+corepack prepare yarn@stable --activate
+
+yarn install
+yarn run check
+yarn run build
+
+Pop-Location
+
+# CMake args
+$cmake_args = @(
+    "-G", "Ninja",
+    "-DVERBOSE=ON",
+    "-DCMAKE_BUILD_TYPE=Release",
+    "-DCMAKE_INSTALL_PREFIX=$env:PREFIX"
+)
+
+cmake -S . -B build @cmake_args
+cmake --build build --config Release --target install
