e2a11693bfbc3c8dcf93152737f91ac3b66aabfe
Glatzel <893016099@qq.com>
2026-04-29 10:39:38 +0800

    build: manually build msedit (#1239)

---
+++ packages/msedit/build.ps1
@@ -0,0 +1,5 @@
+$ROOT = git rev-parse --show-toplevel
+. $ROOT/scripts/util.ps1
+$env:RUSTC_BOOTSTRAP = "1"
+cargo test -- --ignored
+cargo install --path ./crates/edit --config .cargo/release.toml @(Get-Cargo-Arg)
