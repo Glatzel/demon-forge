c7eac5f30448e6085cbb2abbcc72b598b3e9f29f
Glatzel <893016099@qq.com>
2026-04-29 18:46:31 +0800

    fix: msedit update (#1242)

--- packages/msedit/update.ps1
+++ packages/msedit/update.ps1
@@ -1,5 +1,5 @@
 Set-Location $PSScriptRoot
 $ROOT = git rev-parse --show-toplevel
 . $ROOT/scripts/util.ps1
-$latest_version = get-version-github -repo "microsoft/$name"
+$latest_version = get-version-github -repo "microsoft/edit"
 update-recipe $latest_version
