c87c0b834f6e080deee228280bb830bc64404d8d
Glatzel <chenzelin9794@gmail.com>
2026-04-29 10:33:05 +0800

    f

--- packages/msedit/build.sh
+++ packages/msedit/build.sh
@@ -2,4 +2,5 @@ ROOT=$(git rev-parse --show-toplevel)
 . "$ROOT/scripts/util.sh"
 set -- $(get_cargo_arg)
 export RUSTC_BOOTSTRAP=1
+cargo test -- --ignored
 cargo install --path ./crates/edit --config .cargo/release.toml "$@"
