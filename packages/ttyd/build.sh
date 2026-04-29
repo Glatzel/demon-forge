8470dd3f38f733926fe5a1d9086b96a60fcae5ca
Glatzel <chenzelin9794@gmail.com>
2026-04-29 12:25:36 +0800

    Update build.sh

--- packages/ttyd/build.sh
+++ packages/ttyd/build.sh
@@ -2,6 +2,7 @@ cat "${RECIPE_DIR}/index.scss" >> ./html/src/style/index.scss
 
 # fonts
 mkdir ./html/src/style/webfont
+ls "${BUILD_PREFIX}/fonts"
 cp "${BUILD_PREFIX}/fonts/*" ./html/src/style/webfont/
 
 # Handle npm and yarn tasks for front-end
