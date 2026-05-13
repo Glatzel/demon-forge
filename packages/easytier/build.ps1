94a6a3fb15433f81e99b8d5d95477c5822bc5a76
Glatzel <chenzelin9794@gmail.com>
2026-05-13 09:25:36 +0800

    Update build.ps1

--- packages/easytier/build.ps1
+++ packages/easytier/build.ps1
@@ -1,5 +1,5 @@
 $ROOT = git rev-parse --show-toplevel
 . $ROOT/scripts/util.ps1
 cargo install @(Get-Cargo-Arg) --path ./easytier
-Copy-Item ./easytier/third_party/Packet.dll $env:PREFIX/bin
-Copy-Item ./easytier/third_party/wintun.dll $env:PREFIX/bin
+Copy-Item ./easytier/third_party/x86_64/Packet.dll $env:PREFIX/bin
+Copy-Item ./easytier/third_party/x86_64/wintun.dll $env:PREFIX/bin
