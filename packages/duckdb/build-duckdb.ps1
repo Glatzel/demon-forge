e6cfa7234e86ae4cde3c6e6605e83bb98ff2b8e1
Glatzel <chenzelin9794@gmail.com>
2026-05-15 18:06:30 +0800

    f

--- packages/duckdb/build-duckdb.ps1
+++ packages/duckdb/build-duckdb.ps1
@@ -1,6 +1,6 @@
 cmake -S . -B build -G Ninja `
     -DFORCE_COLORED_OUTPUT=1 `
-    -DCMAKE_INSTALL_PREFIX="$env:PREFIX" `
+    -DCMAKE_INSTALL_PREFIX="$env:PREFIX/Library" `
     -DCMAKE_BUILD_TYPE=Release `
     -DOVERRIDE_GIT_DESCRIBE=""
 
