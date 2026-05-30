9114ef535f6a47fa9e65b23a2cf0cb29f4f6cb53
Glatzel <893016099@qq.com>
2026-05-30 10:05:54 +0800

    Update publish.ps1

--- scripts/publish.ps1
+++ scripts/publish.ps1
@@ -7,6 +7,6 @@ if (-not (Test-Path "./output/$env:TARGET_PLATFORM/*.conda" )) {
 }
 foreach ($pkg_file in Get-ChildItem "./output/$env:TARGET_PLATFORM/*.conda") {
     Write-Output "::group:: upload $pkg"
-    pixi run rattler-build -v upload prefix -s -c glatzel --generate-attestation $pkg_file
+    pixi run rattler-build -vv upload prefix -s -c glatzel --generate-attestation $pkg_file
     Write-Output "::endgroup::"
 }
