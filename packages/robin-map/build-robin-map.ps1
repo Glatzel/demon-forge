3740e2e0e6ec3ad19cb855e35667f3daa8dd3841
Glatzel <chenzelin9794@gmail.com>
2026-06-07 15:39:17 +0800

    feat: add robin-map

---
+++ packages/robin-map/build-robin-map.ps1
@@ -0,0 +1,8 @@
+$cmakeArgs = @(
+    "-G"
+    "Ninja"
+    "-DCMAKE_BUILD_TYPE=Release"
+    "-DCMAKE_INSTALL_PREFIX=$ENV:PREFIX/Library"
+)
+cmake -S . -B build @cmakeArgs
+cmake --build build --config Release --target install
