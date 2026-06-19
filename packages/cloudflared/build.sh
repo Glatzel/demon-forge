8e0b9ed85538b837f2987a68bccaa89f130de986
Glatzel <chenzelin9794@gmail.com>
2026-06-19 10:57:11 +0800

    fix: cloudflare

---
+++ packages/cloudflared/build.sh
@@ -0,0 +1,11 @@
+if ${TARGET_PLATFORM} -eq 'linux-64'; then
+    gh release download -R "cloudflare/${PKG_NAME}" -p "${PKG_NAME}-linux-amd64" \
+        -O  $PREFIX/bin/${PKG_NAME}
+fi
+
+if ${TARGET_PLATFORM} -eq 'linux-aarch64'; then
+    gh release download -R "cloudflare/${PKG_NAME}" -p "${PKG_NAME}-linux-arm64" \
+        -O  ${PREFIX}/bin/${PKG_NAME}
+fi
+
+chmod +rwx "${PREFIX}/bin/${PKG_NAME}"
