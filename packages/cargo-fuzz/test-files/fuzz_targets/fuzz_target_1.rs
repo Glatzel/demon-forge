42bd4e43c28070b8cc169bb683f225a24f9679b2
Glatzel <chenzelin9794@gmail.com>
2026-07-09 22:29:37 +0800

    ff

--- packages/cargo-fuzz/test-files/fuzz_targets/fuzz_target_1.rs
+++ packages/cargo-fuzz/test-files/fuzz_targets/fuzz_target_1.rs
@@ -2,13 +2,12 @@
 
 use libfuzzer_sys::fuzz_target;
 
-fn buggy_parse(data: &[u8]) -> u32 {
-    // BUG: no length check
-    let value = u32::from_le_bytes([data[0], data[1], data[2], data[3]]);
-
-    value
-}
-
 fuzz_target!(|data: &[u8]| {
-    buggy_parse(data);
+    let _len = data.len();
+
+    if data.is_empty() {
+        return;
+    }
+
+    let _first = data[0];
 });
