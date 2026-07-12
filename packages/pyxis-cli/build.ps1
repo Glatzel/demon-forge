767e95252eccc8bdc68894a7b6762cb6d575f57e
Glatzel <893016099@qq.com>
2026-07-12 08:42:49 +0800

    Update build.ps1

--- packages/pyxis-cli/build.ps1
+++ packages/pyxis-cli/build.ps1
@@ -1,4 +1,4 @@
 $ROOT = git rev-parse --show-toplevel
 . $ROOT/scripts/util.ps1
 Set-Location rust
-cargo install --bin pyxis --path ./crates/pyxis-cli @(Get-Cargo-Arg)
+cargo install --path ./tools/pyxis-cli @(Get-Cargo-Arg)
