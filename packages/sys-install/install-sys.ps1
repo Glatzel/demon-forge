744bcf3e6ec3ad68aa4fc90f68f491352984fca4
Glatzel <893016099@qq.com>
2026-07-12 15:55:04 +0800

    chore: Update install-sys.ps1 (#1598)

--- packages/sys-install/install-sys.ps1
+++ packages/sys-install/install-sys.ps1
@@ -1,6 +1,9 @@
+$ErrorActionPreference = "Stop"
+$PSNativeCommandUseErrorActionPreference = $true
 foreach ($f in Get-ChildItem $PSScriptRoot/install-sys-*.ps1)
 {
     $pkg_name = ([System.IO.Path]::GetFileNameWithoutExtension($f)).Replace("install-sys-", "")
     Write-Output "===================Install $pkg_name=========================="
     & $f
 }
+remove-item $PSScriptRoot/../temp/ -recurse -force
