1b04a5d5ce5661b97154e0b01cad7d0f3d0b7a38
Glatzel <chenzelin9794@gmail.com>
2026-07-14 11:07:33 +0800

    Update build.sh

--- packages/csharpier/build.sh
+++ packages/csharpier/build.sh
@@ -3,6 +3,6 @@ dotnet tool install csharpier --tool-path "$PREFIX/csharpier"
 
 tee ${PREFIX}/bin/csharpier << EOF
 #!/bin/sh
-exec \${DOTNET_ROOT}/dotnet exec \${CONDA_PREFIX}/libexec/csharpier/CSharpier.dll "\$@"
+exec \${DOTNET_ROOT}/dotnet exec \${CONDA_PREFIX}/csharpier/CSharpier.dll "\$@"
 EOF
 chmod +x ${PREFIX}/bin/csharpier
