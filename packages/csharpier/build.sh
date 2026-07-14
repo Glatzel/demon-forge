0bfd5f4ea539325f609ffdc16126dd1080ef20ae
Glatzel <893016099@qq.com>
2026-07-14 11:42:55 +0800

    feat: add csharpier (#1605)

---
+++ packages/csharpier/build.sh
@@ -0,0 +1,19 @@
+mkdir -p ${PREFIX}/bin
+mkdir -p ${PREFIX}/${PKG_NAME}
+
+rm -rf global.json
+rm -rf .config/dotnet-tools.json
+framework_version="$(dotnet --version | sed -e 's/\..*//g').0"
+dotnet publish --no-self-contained Src/CSharpier.Cli/CSharpier.Cli.csproj --output ${PREFIX}/${PKG_NAME} \
+    --framework net${framework_version} -p:TreatWarningAsErrors=false
+rm ${PREFIX}/${PKG_NAME}/CSharpier
+
+tee ${PREFIX}/bin/csharpier << EOF
+#!/bin/sh
+exec \${DOTNET_ROOT}/dotnet exec \${CONDA_PREFIX}/csharpier/CSharpier.dll "\$@"
+EOF
+chmod +x ${PREFIX}/bin/csharpier
+
+tee ${PREFIX}/bin/dotnet-csharpier.cmd << EOF
+call %DOTNET_ROOT%\dotnet exec %CONDA_PREFIX\csharpier\CSharpier.dll %*
+EOF
