bce6f0433ae6bdf0a78969fc18ce97ca82cb571d
Glatzel <893016099@qq.com>
2026-07-15 11:36:03 +0800

    chore: update blender (#1608)

--- packages/blender/build.ps1
+++ packages/blender/build.ps1
@@ -5,8 +5,8 @@ aria2c -c -x16 -s16 -d ./ `
     https://download.blender.org/release/Blender${major}.${minor}/blender-${version}-windows-x64.zip `
     -o "${env:PKG_NAME}.zip"
 7z x "${env:PKG_NAME}.zip" "-o./${env:PKG_NAME}"
-New-Item $env:PREFIX/bin/${env:PKG_NAME} -ItemType Directory
-Copy-Item "./${env:PKG_NAME}/${env:PKG_NAME}*/*" "$env:PREFIX/bin/${env:PKG_NAME}" -Recurse
+New-Item $env:PREFIX/${env:PKG_NAME} -ItemType Directory
+Copy-Item "./${env:PKG_NAME}/${env:PKG_NAME}*/*" "$env:PREFIX/${env:PKG_NAME}" -Recurse
 # shortcut
 New-Item $env:PREFIX/Menu -ItemType Directory
 Copy-Item "${env:RECIPE_DIR}/${env:PKG_NAME}.json" "$env:PREFIX/Menu"
