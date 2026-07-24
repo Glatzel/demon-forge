45616def603c7ff92cbb5eae0d69b545f885a6c0
Glatzel <chenzelin9794@gmail.com>
2026-07-24 11:14:36 +0800

    f

--- packages/easytier/build.ps1
+++ packages/easytier/build.ps1
@@ -1,5 +1,5 @@
 $ROOT = git rev-parse --show-toplevel
 . $ROOT/scripts/util.ps1
-cargo install @(Get-Cargo-Arg) --path ./easytier
+cargo install --path ./easytier @(Get-Cargo-Arg) 
 Copy-Item ./easytier/third_party/x86_64/Packet.dll $env:PREFIX/bin
 Copy-Item ./easytier/third_party/x86_64/wintun.dll $env:PREFIX/bin
