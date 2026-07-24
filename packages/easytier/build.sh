e561303626c858937c404f74f510144095ababcd
Glatzel <chenzelin9794@gmail.com>
2026-07-24 11:45:45 +0800

    Update build.sh

--- packages/easytier/build.sh
+++ packages/easytier/build.sh
@@ -3,5 +3,7 @@ ROOT=$(git rev-parse --show-toplevel)
 if [ "$(uname)" = "Linux" ]; then
     export LIBCLANG_PATH="$BUILD_PREFIX/lib"
 fi
+sed -i '/target\.x86_64-unknown-linux-gnu/{n;/rustflags/d;}' .cargo/config.toml
+sed -i '/target\.aarch64-unknown-linux-gnu/{n;/rustflags/d;}' .cargo/config.toml
 set -- $(get_cargo_arg)
 cargo install --path ./easytier "$@"
