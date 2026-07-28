fbf42cac26f0d8f770de60fd4957f411abfa1f8d
Glatzel <893016099@qq.com>
2026-07-29 00:04:25 +0800

    chore: 更新 util.ps1

--- scripts/util.ps1
+++ scripts/util.ps1
@@ -23,12 +23,12 @@ function Get-Cargo-Arg
         '--locked'
         '--force'
         '--config', 'profile.release.debug=false'
-        '--config', 'profile.release.codegen-units=1'
         '--config', 'profile.release.lto="fat"'
         '--config', 'profile.release.opt-level=3'
         '--config', 'profile.release.strip=true'
         '--config', 'build.rustflags=["-C","target-cpu=x86-64-v3"]'
     )
+    if ($env:GITHUB_EVENT_NAME -eq "workflow_dispatch"){$cargo_arg+=(       '--config', 'profile.release.codegen-units=1')}
 
     return $cargo_arg
 }
