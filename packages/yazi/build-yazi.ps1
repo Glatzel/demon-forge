70cbe3c662f9a07fffe54c76c97fc3f785068ee6
Glatzel <chenzelin9794@gmail.com>
2026-07-28 21:09:20 +0800

    f

--- packages/yazi/build-yazi.ps1
+++ packages/yazi/build-yazi.ps1
@@ -1,4 +1,4 @@
 $ROOT = git rev-parse --show-toplevel
 . $ROOT/scripts/util.ps1
-cargo install --path ./yazi-fm @(Get-Cargo-Arg)
-cargo install --path ./yazi-cli @(Get-Cargo-Arg)
+cargo install --path ./yazi-fm --profile release-windows @(Get-Cargo-Arg)
+cargo install --path ./yazi-cli --profile release-windows @(Get-Cargo-Arg)
