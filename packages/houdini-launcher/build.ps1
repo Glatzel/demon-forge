55a0c2ca18768d0cccbb28c039af9c64dd058c78
Glatzel <chenzelin9794@gmail.com>
2026-08-06 09:14:31 +0800

    Update build.ps1

--- packages/houdini-launcher/build.ps1
+++ packages/houdini-launcher/build.ps1
@@ -6,7 +6,7 @@ $url = &vinaya.exe sidefx `
     --minor $version.Minor `
     --build production `
     --platform win64 `
-    --jq '.download_url' | ConvertFrom-Json
+    | jq '.download_url'
 aria2c -c -x16 -s16 "$url" -o houdini-launcher.exe
 new-item -itemtype directory -path $env:PREFIX/houdini-launcher
 & ./houdini-launcher.exe /S /D=$(Resolve-Path $env:PREFIX/houdini-launcher)
