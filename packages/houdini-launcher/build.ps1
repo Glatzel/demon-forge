72a055452d1ba6d8764dcdaa6243f93f7c3d3309
Glatzel <chenzelin9794@gmail.com>
2026-08-06 09:10:42 +0800

    Update build.ps1

--- packages/houdini-launcher/build.ps1
+++ packages/houdini-launcher/build.ps1
@@ -1,7 +1,9 @@
+$version=[version]"$env:PKG_VERSION"
 $url = &vinaya.exe sidefx `
     download.get-daily-build-download `
     --product houdini-launcher `
-    --version 20.5 `
+    --major $version.Major `
+    --minor $version.Minor `
     --build production `
     --platform win64 `
     --jq '.download_url'
