2aa26f8499364cab5afa5ee82b0fd9fdc02b0b92
Glatzel <chenzelin9794@gmail.com>
2026-08-06 08:57:59 +0800

    f

--- packages/vinaya-cli/build.sh
+++ packages/vinaya-cli/build.sh
@@ -2,6 +2,4 @@ ROOT=$(git rev-parse --show-toplevel)
 . "$ROOT/scripts/util.sh"
 set -- $(get_cargo_arg)
 cd ./tools/vinaya/crates/vinaya-cli
-pixi run install
-pixi run build
 cargo install --path . "$@"
