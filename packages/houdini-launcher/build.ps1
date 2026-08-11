dccc70411f65c566e1ecffd3dc3b89de3d78cb8e
Glatzel <chenzelin9794@gmail.com>
2026-08-11 18:09:38 +0800

    Update build.ps1

--- packages/houdini-launcher/build.ps1
+++ packages/houdini-launcher/build.ps1
@@ -1,11 +1,9 @@
-$version=[version]"$env:PKG_VERSION"
 $url = &vinaya.exe sidefx `
     download.get-daily-build-download `
-    --product houdini-launcher `
-    --major $version.Major `
-    --minor $version.Minor `
-    --build production `
-    --platform win64 `
+    houdini-launcher `
+    "$env:PKG_VERSION" `
+    production `
+    win64 `
     | jq -r '.download_url'
 Write-Output $url
 aria2c -c -x16 -s16 "$url" -o houdini-launcher.exe
