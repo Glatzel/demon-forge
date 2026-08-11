bfb19b49770e6b778e30a62f2e642fe5ca0991f9
Glatzel <chenzelin9794@gmail.com>
2026-08-11 17:58:47 +0800

    Update build.ps1

--- packages/houdini-launcher/build.ps1
+++ packages/houdini-launcher/build.ps1
@@ -9,7 +9,12 @@ $url = &vinaya.exe sidefx `
     | jq -r '.download_url'
 Write-Output $url
 aria2c -c -x16 -s16 "$url" -o houdini-launcher.exe
-new-item -itemtype directory -path $env:PREFIX/bin
 new-item -itemtype directory -path $env:PREFIX/houdini-launcher
 & ./houdini-launcher.exe /S /D=$(Resolve-Path $env:PREFIX/houdini-launcher)
+
+new-item -itemtype directory -path $env:PREFIX/bin
 new-item -Path $env:PREFIX/bin/houdini_launcher.exe -Target "../houdini-launcher/bin/houdini_launcher.exe" -ItemType SymbolicLink
+
+# shortcut
+New-Item $env:PREFIX/Menu -ItemType Directory
+Copy-Item "${env:RECIPE_DIR}/${env:PKG_NAME}.json" "$env:PREFIX/Menu"
