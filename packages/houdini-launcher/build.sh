946e09ba5d1e78e7f5d294f21de6f2f29b8756bd
Glatzel <chenzelin9794@gmail.com>
2026-08-11 17:53:42 +0800

    f

--- packages/houdini-launcher/build.sh
+++ packages/houdini-launcher/build.sh
@@ -1,13 +1,10 @@
-IFS='.' read -r major minor <<EOF
-$PKG_VERSION
-EOF
 url=$(vinaya sidefx \
     download.get-daily-build-download \
-    --product houdini-launcher \
-    --major $major \
-    --minor $minor \
-    --build production \
-    --platform linux \
+    houdini-launcher \
+    $PKG_VERSION \
+    $minor \
+    production \
+    linux \
     | jq -r '.download_url')
 aria2c -c -x16 -s16 "$url" -o install_houdini_launcher.sh
 mkdir -p $PREFIX/bin
